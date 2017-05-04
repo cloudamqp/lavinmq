73f4b56a0f6e75bd259a8ea12a843c4508f2d123
Carl Hörberg <carl.hoerberg@gmail.com>
2017-05-04 20:59:32 +0100

    write publishes to queue files

--- src/amqpserver/client.cr
+++ src/amqpserver/client.cr
@@ -2,9 +2,8 @@ require "socket"
 
 module AMQPServer
   class Client
-    def initialize(@socket : TCPSocket)
-      @socket.sync = false
-      @open_channels = Set(UInt16).new
+    def initialize(@socket : TCPSocket, @server_state : Server::State)
+      @channels = Hash(UInt16, Client::Channel).new
       negotiate_client
     end
 
@@ -14,16 +13,24 @@ module AMQPServer
         puts "=> #{frame.inspect}"
         case frame
         when AMQP::Channel::Open
-          @open_channels.add frame.channel
+          @channels[frame.channel] = Client::Channel.new(@server_state)
           send AMQP::Channel::OpenOk.new(frame.channel)
         when AMQP::Channel::Close
-          @open_channels.delete frame.channel
+          @channels.delete frame.channel
           send AMQP::Channel::CloseOk.new(frame.channel)
         when AMQP::Exchange::Declare
           send AMQP::Exchange::DeclareOk.new(frame.channel)
         when AMQP::Connection::Close
           send AMQP::Connection::CloseOk.new
           break
+        when AMQP::Basic::Publish
+          ex = frame.exchange
+          rk = frame.routing_key
+          @channels[frame.channel].start_publish(ex, rk)
+        when AMQP::HeaderFrame
+          @channels[frame.channel].write_body_size(frame.body_size)
+        when AMQP::BodyFrame
+          @channels[frame.channel].write_content(frame.body)
         end
       end
     ensure
@@ -56,6 +63,45 @@ module AMQPServer
       open = AMQP::Frame.decode @socket
       open_ok = AMQP::Connection::OpenOk.new
       send open_ok
+    end
+
+    class Channel
+      def initialize(@state : Server::State)
+        @publish_queues = Array(Server::Queue).new
+      end
+
+      def start_publish(exchange_name, routing_key)
+        #make sure exchange is declared
+        ex = @state.exchanges[exchange_name]
+        if ex.nil?
+          raise "Exchange not declared"
+        end
+        case ex[:type]
+        when "direct"
+          queues = ex[:bindings][routing_key]
+          if queues.nil? || queues.empty?
+            puts "No queue to publish to"
+          else
+            queues.each do |q|
+              q.write_head(exchange_name, routing_key)
+            end
+            @publish_queues = queues
+          end
+        else raise "Exchange type #{ex[:type]} not implemented"
+        end
+      end
+
+      def write_body_size(size)
+        @publish_queues.each do |q|
+          q.write_body_size size
+        end
+      end
+
+      def write_content(bytes)
+        @publish_queues.each do |q|
+          q.write_content bytes
+        end
+      end
     end
   end
 end
--- src/amqpserver/server.cr
+++ src/amqpserver/server.cr
@@ -6,6 +6,10 @@ require "./backend"
 
 module AMQPServer
   class Server
+    def initialize
+      @state = State.new
+    end
+
     def listen(port : Int)
       server = TCPServer.new("localhost", port)
       puts "Server listening on #{server.local_address}"
@@ -19,8 +23,60 @@ module AMQPServer
     end
 
     def handle_connection(socket)
-      client = Client.new(socket)
+      client = Client.new(socket, @state)
       client.run_loop
+    end
+
+    class State
+      getter exchanges
+
+      def initialize
+        @exchanges = {
+          "amq.direct" => {
+            type: "direct",
+            durable: true,
+            bindings: {
+              "rk" => [
+                Queue.new("q1")
+              ]
+            }
+          }
+        }
+      end
+    end
+
+    class Queue
+      class QueueFile < File
+        include AMQP::IO
+
+        def finalize
+          flush
+          close
+          super
+        end
+      end
+
+      def initialize(@name : String)
+        @file = QueueFile.open("/tmp/#{@name}.q", "a")
+        @next_msg_size = 0_u64
+        @msg_bytes_left = 0_u64
+      end
+      
+      def write_head(exchange_name : String, routing_key : String)
+        @file.write_short_string exchange_name
+        @file.write_short_string routing_key
+      end
+
+      def write_body_size(size : UInt64)
+        @file.write_int size
+        @msg_bytes_left = size
+      end
+
+      def write_content(bytes : Bytes)
+        @file.write bytes
+        @msg_bytes_left -= bytes.size
+        @file.flush if @msg_bytes_left == 0
+      end
     end
   end
 end
