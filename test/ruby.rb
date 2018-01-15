1b058abb6ebafbbe854b7b589c16ec8ceaea21e9
Carl Hörberg <carl.hoerberg@gmail.com>
2018-01-15 15:27:14 +0100

    print throughput every 1s in ruby test

--- test/ruby.rb
+++ test/ruby.rb
@@ -1,15 +1,29 @@
 require 'bunny'
 
+
+i = j = 0
+Thread.new do
+  loop do
+    i = j = 0
+    sleep 1
+    puts "Consumed: #{i} msgs/s"
+    puts "Published: #{j} msgs/s"
+  end
+end
+
 b = Bunny.new("amqp://guest:guest@localhost/default").start
 ch = b.create_channel
-q = ch.queue "q2", durable: true
-q.subscribe do |d, h, p|
-  puts d, h, p
-end
+q1 = ch.queue "q1", durable: true, auto_delete: false
+q1.bind "amq.fanout"
+#q1.subscribe do |d, h, p|
+  #puts "q1: #{p}"
+#  i += 1
+#end
 
-10.times do
-  q.publish "hej"
+ch2 = b.create_channel
+e = ch2.exchange("amq.fanout", type: :fanout)
+loop do
+  e.publish "hej"
+  j += 1
 end
-
 sleep
-b.close
