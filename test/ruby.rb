207ebb863e6416f32f32280b15d3b6d3d4172605
Carl Hörberg <carl.hoerberg@gmail.com>
2018-01-23 17:00:45 +0100

    pub/sub at the same time in ruby test script

--- test/ruby.rb
+++ test/ruby.rb
@@ -15,15 +15,15 @@ b = Bunny.new("amqp://guest:guest@localhost/default").start
 ch = b.create_channel
 q1 = ch.queue "q1", durable: true, auto_delete: false
 q1.bind "amq.fanout"
-#q1.subscribe do |d, h, p|
-  #puts "q1: #{p}"
-#  i += 1
-#end
+q1.subscribe do |d, h, p|
+  i += 1
+end
 
 ch2 = b.create_channel
 e = ch2.exchange("amq.fanout", type: :fanout)
+msg = " "
 loop do
-  e.publish "hej"
+  e.publish msg
   j += 1
 end
 sleep
