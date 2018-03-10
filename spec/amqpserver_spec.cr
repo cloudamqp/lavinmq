a540c09bdd2bf218f995be37bf0852b109b1b8df
Carl Hörberg <carl.hoerberg@gmail.com>
2018-03-10 09:31:28 +0100

    fix the specs

--- spec/amqpserver_spec.cr
+++ spec/amqpserver_spec.cr
@@ -3,18 +3,16 @@ require "amqp"
 
 describe AMQPServer::Server do
   it "accepts connections" do
-    s = AMQPServer::Server.new("/tmp/spec3", Logger::DEBUG)
+    s = AMQPServer::Server.new("/tmp/spec4", Logger::DEBUG)
     spawn { s.listen(5674) }
     sleep 0.001
-    AMQP::Connection.start(AMQP::Config.new(port: 5672, vhost: "/")) do |conn|
+    AMQP::Connection.start(AMQP::Config.new(port: 5674, vhost: "default")) do |conn|
       ch = conn.channel
       x = ch.exchange("amq.topic", "topic", auto_delete: false, durable: true, internal: true, passive: true)
       q = ch.queue("q3", auto_delete: false, durable: true, exclusive: false)
       q.bind(x, "#")
       pmsg = AMQP::Message.new("test message")
       x.publish pmsg, q.name
-      x.publish pmsg, q.name
-      sleep 0.001
       msg = q.get(no_ack: true)
       msg.to_s.should eq("test message")
     end
