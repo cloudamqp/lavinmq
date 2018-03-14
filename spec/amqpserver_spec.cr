e1da294dc5b8c7e719d29f7db2d99d0c840d592a
Carl Hörberg <carl.hoerberg@gmail.com>
2018-03-15 00:06:56 +0100

    separate specs a bit

--- spec/amqpserver_spec.cr
+++ spec/amqpserver_spec.cr
@@ -3,7 +3,7 @@ require "amqp"
 
 describe AMQPServer::Server do
   it "accepts connections" do
-    s = AMQPServer::Server.new("/tmp/spec", Logger::ERROR)
+    s = AMQPServer::Server.new("/tmp/spec", Logger::DEBUG)
     spawn { s.listen(5674) }
     sleep 0.001
     AMQP::Connection.start(AMQP::Config.new(port: 5674, vhost: "default")) do |conn|
@@ -20,10 +20,10 @@ describe AMQPServer::Server do
   end
 
   it "can delete queue" do
-    s = AMQPServer::Server.new("/tmp/spec", Logger::DEBUG)
-    spawn { s.listen(5674) }
+    s = AMQPServer::Server.new("/tmp/spec2", Logger::ERROR)
+    spawn { s.listen(5675) }
     sleep 0.001
-    AMQP::Connection.start(AMQP::Config.new(port: 5674, vhost: "default")) do |conn|
+    AMQP::Connection.start(AMQP::Config.new(port: 5675, vhost: "default")) do |conn|
       ch = conn.channel
       pmsg = AMQP::Message.new("m1")
       q = ch.queue("q3", auto_delete: false, durable: true, exclusive: false)
