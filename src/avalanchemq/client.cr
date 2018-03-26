1756bb3f6eafeb3e0305d4709392a139dd517114
Carl Hörberg <carl.hoerberg@gmail.com>
2018-03-26 13:36:29 +0200

    enable yielding every 1000 frame

--- src/avalanchemq/client.cr
+++ src/avalanchemq/client.cr
@@ -94,7 +94,7 @@ module AvalancheMQ
           cleanup
           break
         end
-        if false && (i += 1) % 1000 == 0
+        if (i += 1) % 1000 == 0
           @log.debug "send_loop yielding"
           Fiber.yield
         end
@@ -303,7 +303,7 @@ module AvalancheMQ
           send AMQP::HeartbeatFrame.new
         else @log.error "Unhandled frame #{frame.inspect}"
         end
-        if false && (i += 1) % 1000 == 0
+        if (i += 1) % 1000 == 0
           @log.debug "read_loop yielding"
           Fiber.yield
         end
