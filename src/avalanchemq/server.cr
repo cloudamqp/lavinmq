c314c1eea4be9486e64a206116700a510fe089f8
Carl Hörberg <carl.hoerberg@gmail.com>
2018-03-28 01:27:08 +0200

    make socket unsynced but flush after each frame

--- src/avalanchemq/server.cr
+++ src/avalanchemq/server.cr
@@ -53,14 +53,14 @@ module AvalancheMQ
     end
 
     private def handle_connection(socket)
-      socket.sync = true
+      socket.sync = false
       socket.keepalive = true
       socket.tcp_nodelay = true
       socket.tcp_keepalive_idle = 60
       socket.tcp_keepalive_count = 3
       socket.tcp_keepalive_interval = 10
       socket.linger = 0
-      socket.write_timeout = 5
+      # socket.write_timeout = 5
       if client = Client.start(socket, @vhosts, @log)
         @connection_events.send({ client, :connected })
         client.on_close do |c|
