3df1e2cc55b0726e49149f604f0ac357391fdf88
Carl Hörberg <carl.hoerberg@gmail.com>
2018-03-29 00:58:39 +0200

    wip: segment queue index files

--- src/avalanchemq/durable_queue.cr
+++ src/avalanchemq/durable_queue.cr
@@ -1,6 +1,7 @@
 require "./queue"
 module AvalancheMQ
   class DurableQueue < Queue
+    MAX_SEGMENT_SIZE = 16 * 1024**2
     @ack : QueueFile
     @enq : QueueFile
     @durable = true
@@ -11,11 +12,11 @@ module AvalancheMQ
       super
       @index_dir = File.join(@vhost.data_dir, Digest::SHA1.hexdigest @name)
       Dir.mkdir_p @index_dir
-      @enq_seg = 0_u32
-      @ack_seg = 0_u32
-      @enq = QueueFile.open(File.join(@index_dir, "#{@enq_seg}.enq"), "a+")
-      @ack = QueueFile.open(File.join(@index_dir, "#{@ack_seg}.ack"), "a+")
       restore_index
+      @enq_seg = last_segment "enq"
+      @ack_seg = last_segment "ack"
+      @enq = QueueFile.open(File.join(@index_dir, "enq.#{@enq_seg}"), "a")
+      @ack = QueueFile.open(File.join(@index_dir, "ack.#{@ack_seg}"), "a")
     end
 
     def close(deleting = false)
@@ -33,6 +34,11 @@ module AvalancheMQ
     def publish(sp : SegmentPosition, flush = false)
       @enq.write_bytes sp
       @enq.flush if flush
+      if @enq.pos >= MAX_SEGMENT_SIZE
+        @enq.close
+        @enq_seg += 1
+        @enq = QueueFile.open(File.join(@index_dir, "enq.#{@enq_seg}"), "a")
+      end
       super
     end
 
@@ -40,6 +46,12 @@ module AvalancheMQ
       super.tap do |env|
         if no_ack && env
           @ack.write_bytes env.segment_position
+          @ack.flush
+          if @ack.pos >= MAX_SEGMENT_SIZE
+            @ack.close
+            @ack_seg += 1
+            @ack = QueueFile.open(File.join(@index_dir, "ack.#{@ack_seg}"), "a")
+          end
         end
       end
     end
@@ -47,28 +59,54 @@ module AvalancheMQ
     def ack(sp : SegmentPosition)
       @ack.write_bytes sp
       @ack.flush
+      if @ack.pos >= MAX_SEGMENT_SIZE
+        @ack.close
+        @ack_seg += 1
+        @ack = QueueFile.open(File.join(@index_dir, "ack.#{@ack_seg}"), "a")
+      end
       super
     end
 
     def purge
-      @enq.truncate
-      @ack.truncate
+      @enq.close
+      @ack.close
+      Dir.children(@index_dir).each { |f| File.delete f }
+      @enq_seg = 0_u32
+      @ack_seg = 0_u32
+      @enq = QueueFile.open(File.join(@index_dir, "enq.#{@enq_seg}"), "a")
+      @ack = QueueFile.open(File.join(@index_dir, "ack.#{@ack_seg}"), "a")
       super
+    end
+
+    private def last_segment(prefix) : UInt32
+      segments = Dir.glob(File.join(@index_dir, "#{prefix}.*")).sort
+      last_file = segments.last? || return 0_u32
+      last_file[/\d+$/].to_u32
     end
 
     private def restore_index
       @log.info "Restoring index"
-      @ack.seek(0, IO::Seek::Set)
-      acked = Set(SegmentPosition).new(@ack.size / sizeof(SegmentPosition))
-      loop do
-        break if @ack.pos == @ack.size
-        acked << SegmentPosition.decode @ack
+      acks = Dir.glob(File.join(@index_dir, "ack.*")).sort
+      ack_sizes = acks.map { |f| File.size f }.sum
+      acked = Set(SegmentPosition).new(ack_sizes / sizeof(SegmentPosition))
+      acks.each do |path|
+        File.open(path) do |ack|
+          loop do
+            break if ack.pos == ack.size
+            acked << SegmentPosition.decode ack
+          end
+        end
       end
-      @enq.seek(0, IO::Seek::Set)
-      loop do
-        break if @enq.pos == @enq.size
-        sp = SegmentPosition.decode @enq
-        @ready << sp unless acked.includes? sp
+
+      enqs = Dir.glob(File.join(@index_dir, "enq.*")).sort
+      enqs.each do |path|
+        File.open(path) do |enq|
+          loop do
+            break if enq.pos == enq.size
+            sp = SegmentPosition.decode enq
+            @ready << sp unless acked.includes? sp
+          end
+        end
       end
       @log.info "#{message_count} messages"
     rescue Errno
