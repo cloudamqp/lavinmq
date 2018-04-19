7fba4dbdf9d4c1b8e185dac5696d7ddb0eee5a2a
Carl Hörberg <carl.hoerberg@gmail.com>
2018-04-19 14:30:59 +0200

    prettier formatting of spec json payload

--- spec/definitions_spec.cr
+++ spec/definitions_spec.cr
@@ -18,14 +18,16 @@ describe AvalancheMQ::HTTPServer do
     spawn { h.listen }
     Fiber.yield
     s.users["bunny_gem"]?.should be_nil
-    response =
-      HTTP::Client.post("http://localhost:8080/api/definitions",
-                        headers: HTTP::Headers{"Content-Type" => "application/json"},
-                        body: %({"rabbit_version":"3.7.4", "users":[
-                        {"name":"bunny_gem",
-                        "password_hash":"rynF7rEzCknjtWTiPKuobNIVNjy/0CpOnaAoI6XFImG4RBJd",
-                        "hashing_algorithm":"rabbit_password_hashing_sha256","tags":""}
-                        ]}))
+    body = %({
+      "rabbit_version":"3.7.4", "users":[{
+        "name":"bunny_gem",
+        "password_hash":"rynF7rEzCknjtWTiPKuobNIVNjy/0CpOnaAoI6XFImG4RBJd",
+        "hashing_algorithm":"rabbit_password_hashing_sha256","tags":""
+      }]
+    })
+    response = HTTP::Client.post("http://localhost:8080/api/definitions",
+                                 headers: HTTP::Headers{"Content-Type" => "application/json"},
+                                 body: body)
     response.status_code.should eq 200
     s.users["bunny_gem"]?.should be_truthy
   end
