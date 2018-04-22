5f2c7f024413c400c4b3dd74b1340ea8f091a310
Carl Hörberg <carl.hoerberg@gmail.com>
2018-04-22 10:29:54 +0200

    spec for shoveling large messages (>frame max)

--- spec/shovel_spec.cr
+++ spec/shovel_spec.cr
@@ -30,4 +30,32 @@ describe AvalancheMQ::Shovel do
     end
     s.close
   end
+
+  it "can shovel large messages" do
+    s = AvalancheMQ::Server.new("/tmp/spec", Logger::ERROR)
+    spawn { s.listen(5672) }
+    Fiber.yield
+    AMQP::Connection.start do |conn|
+      ch = conn.channel
+      x = ch.exchange("", "direct", passive: true)
+      q1 = ch.queue("q1")
+      q2 = ch.queue("q2")
+      pmsg = AMQP::Message.new("a" * 10_000)
+      x.publish pmsg, "q1"
+
+      source = AvalancheMQ::Shovel::Source.new(
+        "amqp://guest:guest@localhost",
+        "q1",
+        delete_after: AvalancheMQ::Shovel::DeleteAfter::QueueLength
+      )
+      dest = AvalancheMQ::Shovel::Destination.new(
+        "amqp://guest:guest@localhost",
+        "q2"
+      )
+      shovel = AvalancheMQ::Shovel.new(source, dest)
+      shovel.run
+      q2.get.to_s.bytesize.should eq 10_000
+    end
+    s.close
+  end
 end
