49a674f3fc94d1aaf9e35819fd79b32c9633b997
Carl Hörberg <carl.hoerberg@gmail.com>
2018-04-26 10:13:26 +0200

    prefix custom html attributes with data-

--- static/table.js
+++ static/table.js
@@ -1,50 +1,54 @@
 (function () {
   function renderTable(url, keyColumns, renderRow) {
-    var sortColumns = keyColumns;
+    var sortKey = "";
     var reverseOrder = false;
     makeHeadersSortable();
     var raw = localStorage.getItem(url);
-    if (raw) {
-      updateTable(raw);
-    }
+    if (raw) updateTable(raw);
     fetchAndUpdate();
     var updateTimer = setInterval(fetchAndUpdate, 5000);
 
     function makeHeadersSortable() {
-      document.querySelectorAll("#table th[sort-columns]").forEach(function (cell) {
+      document.querySelectorAll("#table th[data-sort-key]").forEach(function (cell) {
         cell.addEventListener("click", function (e) {
           // var column = e.target.cellIndex;
-          var newSortColumns = e.target.getAttribute("sort-columns").split(",");
           // var newSortColumn = e.target.textContent.toLowerCase();
-          if (sortColumns.toString() === newSortColumns.toString()) {
+          var newSortKey = e.target.getAttribute("data-sort-key");
+          if (newSortKey === newSortKey) {
             reverseOrder = !reverseOrder;
           } else {
-            sortColumns = newSortColumns;
+            sortKey = newSortKey;
+            reverseOrder = false;
           }
           clearInterval(updateTimer);
           var t = document.getElementById("table").tBodies[0];
-          while (t.rows.length) {
-            t.deleteRow(0);
-          }
+          clearRows(t);
           var raw = localStorage.getItem(url);
           updateTable(raw);
         });
       });
     }
 
+    function clearRows(t) {
+      while (t.rows.length) t.deleteRow(-1);
+    }
+
     function fetchAndUpdate() {
+      var tableError = document.getElementById("table-error");
       var xhr = new XMLHttpRequest();
       xhr.open('GET', url);
       xhr.onload = function () {
-        document.getElementById("xhr-error").textContent = "";
+        tableError.textContent = "";
         localStorage.setItem(url, this.response);
         updateTable(this.response);
       };
       xhr.onerror = function (e) {
-        document.getElementById("xhr-error").textContent = "Error fetching data";
+        tableError.textContent = "Error fetching data";
+        console.error(e);
       };
       xhr.ontimeout = function (e) {
-        document.getElementById("xhr-error").textContent = "Error fetching data";
+        tableError.textContent = "Error fetching data";
+        console.error(e);
       };
       xhr.timeout = 5000;
       xhr.send();
@@ -52,17 +56,7 @@
 
     function updateTable(raw) {
       var data = JSON.parse(raw);
-      data.sort(function (a, b) {
-        for (var i = 0; i < sortColumns.length; i++) {
-          if (a[sortColumns[i]] > b[sortColumns[i]]) {
-            return 1 * (reverseOrder ? -1 : 1);
-          }
-          if (a[sortColumns[i]] < b[sortColumns[i]]) {
-            return -1 * (reverseOrder ? -1 : 1);
-          }
-        }
-        return 0;
-      });
+      data.sort(byColumn);
       document.getElementById("table-count").textContent = data.length;
       var t = document.getElementById("table").tBodies[0];
       for (var i = 0; i < data.length; i++) {
@@ -80,15 +74,25 @@
       }
 
       var rowsToDelete = t.rows.length - data.length;
-      while (0 < rowsToDelete--) {
+      while (0 < rowsToDelete--)
         t.deleteRow(t.rows.length - 1);
+    }
+
+    function byColumn(a, b) {
+      var sortColumns = sortKey === "" ? keyColumns : [sortKey].concat(keyColumns);
+      for (var i = 0; i < sortColumns.length; i++) {
+        if (a[sortColumns[i]] > b[sortColumns[i]])
+          return 1 * (reverseOrder ? -1 : 1);
+        if (a[sortColumns[i]] < b[sortColumns[i]])
+          return -1 * (reverseOrder ? -1 : 1);
       }
+      return 0;
     }
 
     function findIndex(rows, start, item) {
       for (var i = start; i < rows.length; i++) {
         for (var k = 0; k < keyColumns.length; k++) {
-          if (rows[i].getAttribute(keyColumns[k]) !== item[keyColumns[k]]) {
+          if (rows[i].getAttribute("data-" + keyColumns[k]) !== item[keyColumns[k]]) {
             break;
           } else if (k === keyColumns.length - 1) {
             return i;
@@ -100,7 +104,7 @@
 
     function setKeyAttributes(tr, item) {
       keyColumns.forEach(function (key) {
-        tr.setAttribute(key, item[key]);
+        tr.setAttribute("data-" + key, item[key]);
       });
     }
   }
