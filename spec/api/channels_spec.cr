328333a531e113e92a3067204607b03a5596c138
Anders Bälter <anders@84codes.com>
2018-04-28 10:32:48 +0200

    list channels for a vhost

--- spec/api/channels_spec.cr
+++ spec/api/channels_spec.cr
@@ -1,7 +1,7 @@
 require "../spec_helper"
 require "uri"
 
-describe AvalancheMQ::ConnectionsController do
+describe AvalancheMQ::ChannelsController do
   describe "GET /api/channels" do
     it "should return all channels" do
       s = AvalancheMQ::Server.new("/tmp/spec", Logger::ERROR)
@@ -23,98 +23,25 @@ describe AvalancheMQ::ConnectionsController do
       h.try &.close
       s.try &.close
     end
-  end
 
-  describe "GET /api/vhosts/vhost/connections" do
-    it "should return network connections" do
+    it "should return empty array if no connections" do
       s = AvalancheMQ::Server.new("/tmp/spec", Logger::ERROR)
       h = AvalancheMQ::HTTPServer.new(s, 8080)
       spawn { s.try &.listen(5672) }
       spawn { h.try &.listen }
       Fiber.yield
-      AMQP::Connection.start do |conn|
-        response = HTTP::Client.get("http://localhost:8080/api/vhosts/%2f/connections")
-        response.status_code.should eq 200
-        body = JSON.parse(response.body)
-        body.as_a.empty?.should be_false
-        keys = ["channels", "connected_at", "type", "channel_max", "timeout", "client_properties",
-                "vhost", "user", "protocol", "auth_mechanism", "host", "port", "name", "ssl",
-                "state"]
-        body.each { |v| keys.each { |k| v.as_h.keys.should contain(k) } }
-      end
-    ensure
-      h.try &.close
-      s.try &.close
-    end
-
-    it "should return 404 if vhosts does not exist" do
-      s = AvalancheMQ::Server.new("/tmp/spec", Logger::ERROR)
-      h = AvalancheMQ::HTTPServer.new(s, 8080)
-      spawn { h.try &.listen }
-      Fiber.yield
-      response = HTTP::Client.get("http://localhost:8080/api/vhosts/vhost/connections")
-      response.status_code.should eq 404
-    ensure
-      h.try &.close
-    end
-  end
-
-  describe "GET /api/connections/name" do
-    it "should return connection" do
-      s = AvalancheMQ::Server.new("/tmp/spec", Logger::ERROR)
-      h = AvalancheMQ::HTTPServer.new(s, 8080)
-      spawn { s.try &.listen(5672) }
-      spawn { h.try &.listen }
-      Fiber.yield
-      AMQP::Connection.start do |conn|
-        response = HTTP::Client.get("http://localhost:8080/api/vhosts/%2f/connections")
-        response.status_code.should eq 200
-        body = JSON.parse(response.body)
-        name = URI.escape(body[0]["name"].as_s)
-        response = HTTP::Client.get("http://localhost:8080/api/connections/#{name}")
-        response.status_code.should eq 200
-      end
-    ensure
-      h.try &.close
-      s.try &.close
-    end
-
-    it "should return 404 if connection does not exist" do
-      s = AvalancheMQ::Server.new("/tmp/spec", Logger::ERROR)
-      h = AvalancheMQ::HTTPServer.new(s, 8080)
-      spawn { h.try &.listen }
-      Fiber.yield
-      response = HTTP::Client.get("http://localhost:8080/api/connections/name")
-      response.status_code.should eq 404
-    ensure
-      h.try &.close
-    end
-  end
-
-  describe "DELETE /api/connections/name" do
-    it "should close connection" do
-      s = AvalancheMQ::Server.new("/tmp/spec", Logger::ERROR)
-      h = AvalancheMQ::HTTPServer.new(s, 8080)
-      spawn { s.try &.listen(5672) }
-      spawn { h.try &.listen }
-      Fiber.yield
-      AMQP::Connection.start do |conn|
-        response = HTTP::Client.get("http://localhost:8080/api/vhosts/%2f/connections")
-        response.status_code.should eq 200
-        body = JSON.parse(response.body)
-        name = URI.escape(body[0]["name"].as_s)
-        response = HTTP::Client.delete("http://localhost:8080/api/connections/#{name}")
-      ensure
-        response.try &.status_code.should eq 200
-      end
+      response = HTTP::Client.get("http://localhost:8080/api/channels")
+      response.status_code.should eq 200
+      body = JSON.parse(response.body)
+      body.as_a.empty?.should be_true
     ensure
       h.try &.close
       s.try &.close
     end
   end
 
-  describe "GET /api/connections/name/channels" do
-    it "should return channels for a connection" do
+  describe "GET /api/vhosts/vhost/channels" do
+    it "should return all channels for a vhost" do
       s = AvalancheMQ::Server.new("/tmp/spec", Logger::ERROR)
       h = AvalancheMQ::HTTPServer.new(s, 8080)
       spawn { s.try &.listen(5672) }
@@ -122,15 +49,26 @@ describe AvalancheMQ::ConnectionsController do
       Fiber.yield
       AMQP::Connection.start do |conn|
         ch = conn.channel
-        response = HTTP::Client.get("http://localhost:8080/api/vhosts/%2f/connections")
-        response.status_code.should eq 200
-        body = JSON.parse(response.body)
-        name = URI.escape(body[0]["name"].as_s)
-        response = HTTP::Client.get("http://localhost:8080/api/connections/#{name}/channels")
+        response = HTTP::Client.get("http://localhost:8080/api/vhosts/%2f/channels")
         response.status_code.should eq 200
         body = JSON.parse(response.body)
         body.as_a.size.should eq 1
       end
+    ensure
+      h.try &.close
+      s.try &.close
+    end
+
+    it "should return empty array if no connections" do
+      s = AvalancheMQ::Server.new("/tmp/spec", Logger::ERROR)
+      h = AvalancheMQ::HTTPServer.new(s, 8080)
+      spawn { s.try &.listen(5672) }
+      spawn { h.try &.listen }
+      Fiber.yield
+      response = HTTP::Client.get("http://localhost:8080/api/vhosts/%2f/channels")
+      response.status_code.should eq 200
+      body = JSON.parse(response.body)
+      body.as_a.empty?.should be_true
     ensure
       h.try &.close
       s.try &.close
--- src/avalanchemq/http/controller/channels.cr
+++ src/avalanchemq/http/controller/channels.cr
@@ -8,6 +8,17 @@ module AvalancheMQ
         @amqp_server.connections.flat_map { |c| c.channels.values }.to_json(context.response)
         context
       end
+
+      get "/api/vhosts/:vhost/channels" do |context, params|
+        with_vhost(context, params) do |vhost|
+          c = @amqp_server.connections.find { |c| c.vhost.name == vhost }
+          if c
+            c.channels.values.to_json(context.response)
+          else
+            context.response.print("[]")
+          end
+        end
+      end
     end
   end
 end
