658fd429abf1cca5578841b64cc9f611421b8244
Magnus Hörberg <magnus.hoerberg@gmail.com>
2019-02-08 11:12:09 +0100

    refine test

--- test/basic_recover.rb
+++ test/basic_recover.rb
@@ -8,23 +8,28 @@ def publish_100_msgs(qname)
 end
 def get(qname, ch)
   delivery, _headers, body = ch.basic_get(qname, manual_ack: true)
-  yield body
-  ch.acknowledge(delivery.delivery_tag, false)
+  ack = yield delivery.delivery_tag, body
+  ch.acknowledge(delivery.delivery_tag, false) if ack
 end
 count = 0
 ch = B.create_channel(nil, 1)
 ch.queue('test_get1', durable: false, auto_delete: true)
 publish_100_msgs('test_get1')
 ch.prefetch 100
+ack = false
 begin
   150.times do |i|
-    get('test_get1', ch) do |msg|
-      puts "queue=test_get1 msg=#{msg}"
-      ch.basic_recover(true) if count == 50
+    if i == 50
+      ch.basic_recover(true)
+      ack = true
+    end
+    get('test_get1', ch) do |dt, msg|
+      puts "queue=test_get1 dt=#{dt.to_i} msg=#{msg}"
+      ack
     end
     count += 1
   end
 ensure
   puts "Published 150 msgs; Consumed #{count} msgs"
+  B.close
 end
-
