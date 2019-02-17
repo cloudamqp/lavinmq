f56348e2dbd688f4298a68726c461742aaf42016
Carl Hörberg <carl.hoerberg@gmail.com>
2019-02-17 09:12:56 +0100

    shift before push into stats log dequeues

--- src/avalanchemq/stats.cr
+++ src/avalanchemq/stats.cr
@@ -26,14 +26,14 @@ module AvalancheMQ
 
       def update_rates
         {% for name in stats_keys %}
-          @{{name.id}}_log << @{{name.id}}_rate
           @{{name.id}}_log.shift if @{{name.id}}_log.size > Config.instance.stats_log_size
+          @{{name.id}}_log.push @{{name.id}}_rate
           @{{name.id}}_rate = @{{name.id}}_count.to_f32 / (Config.instance.stats_interval / 1000)
           @{{name.id}}_count = 0
         {% end %}
         {% for name in log_keys %}
-          @{{name.id}}_log << {{name.id}}
-          @{{name.id}}_log.shift if @{{name.id}}_log.size > Config.instance.stats_log_size
+          @{{name.id}}_log.shift if @{{name.id}}_log.size >= Config.instance.stats_log_size
+          @{{name.id}}_log.push {{name.id}}
         {% end %}
       end
     end
