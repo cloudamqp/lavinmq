72e4559dd9ef93396d48cc9e23cd168dd0468152
Carl Hörberg <carl.hoerberg@gmail.com>
2019-06-06 12:37:20 +0200

    require crystal 0.29

--- src/avalanchemq/stdlib_fixes.cr
+++ src/avalanchemq/stdlib_fixes.cr
@@ -5,27 +5,9 @@ class Fiber
 end
 
 require "openssl"
-
-lib LibSSL
-  type SSLCipher = Void*
-  fun ssl_get_current_cipher = SSL_get_current_cipher(ssl : SSL) : SSLCipher
-  fun ssl_cipher_get_name = SSL_CIPHER_get_name(cipher : SSLCipher) : UInt8*
-  fun ssl_get_version = SSL_get_version(ssl : SSL) : UInt8*
-end
-
 require "io"
 
 abstract class OpenSSL::SSL::Socket
-  # Returns the cipher currently in use
-  def cipher : String
-    String.new(LibSSL.ssl_cipher_get_name(LibSSL.ssl_get_current_cipher(@ssl)))
-  end
-
-  # Returns the TLS version currently in use
-  def tls_version : String
-    String.new(LibSSL.ssl_get_version(@ssl))
-  end
-
   def read_timeout=(read_timeout)
     io = @bio.io
     if io.responds_to? :read_timeout
