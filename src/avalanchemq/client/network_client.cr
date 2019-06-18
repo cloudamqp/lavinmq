0d21fb5a8025de3861c9a80fbe37eb5e99b3681e
Carl Hörberg <carl.hoerberg@gmail.com>
2019-06-18 23:57:59 +0200

    dont yield

--- src/avalanchemq/client/network_client.cr
+++ src/avalanchemq/client/network_client.cr
@@ -72,7 +72,6 @@ module AvalancheMQ
     end
 
     private def read_loop
-      i = 0
       loop do
         AMQP::Frame.from_io(@socket) do |frame|
           @log.debug { "Read #{frame.inspect}" }
@@ -86,7 +85,6 @@ module AvalancheMQ
           end
           process_frame(frame)
         end || break
-        Fiber.yield if (i += 1) % 1000 == 0
       end
     rescue ex : AMQP::Error::NotImplemented
       @log.error { "#{ex} when reading from socket" }
