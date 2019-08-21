1b01f77cae44894d67ca1e324b76076761a58850
Anders Bälter <anders@84codes.com>
2019-08-21 17:14:10 +0200

    lint

--- src/stdlib/event_loop.cr
+++ src/stdlib/event_loop.cr
@@ -1,6 +1,6 @@
 module Crystal::EventLoop
   def self.create_timeout_event(fiber)
-    @@eb.new_event(-1, LibEvent2::EventFlags::None, fiber) do |s, flags, data|
+    @@eb.new_event(-1, LibEvent2::EventFlags::None, fiber) do |_s, _flags, data|
       f = data.as(Fiber)
       f.timed_out = true
       f.resume
