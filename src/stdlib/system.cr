8d3d6985ba101704ee8fb2fa41cc8146f6ed4493
Carl Hörberg <carl.hoerberg@gmail.com>
2019-09-24 15:56:25 +0200

    fix overflow in system memeory on linux

--- src/stdlib/system.cr
+++ src/stdlib/system.cr
@@ -1,7 +1,7 @@
 module System
   def self.physical_memory
     {% if flag?(:linux) %}
-      LibC.get_phys_pages * LibC.getpagesize
+      LibC.get_phys_pages.to_u64 * LibC.getpagesize
     {% elsif flag?(:darwin) %}
       LibC.sysconf(LibC::SC_PHYS_PAGES) * LibC.sysconf(LibC::SC_PAGESIZE)
     {% else %}
