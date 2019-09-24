701e81ff55cefa1ccfd44c94559f3ebdfa369529
Carl Hörberg <carl.hoerberg@gmail.com>
2019-09-24 15:18:46 +0200

    re-implement Channel#empty? and Channel#full?

--- src/stdlib/channel.cr
+++ src/stdlib/channel.cr
@@ -1,32 +1,17 @@
-# https://github.com/crystal-lang/crystal/pull/8006
-class Channel
-  struct TimeoutAction
-    include SelectAction
-
-    @timeout_at : Time::Span
-
-    def initialize(timeout : Time::Span)
-      @timeout_at = Time.monotonic + timeout
+class Channel(T)
+  def full?
+    if queue = @queue
+      return queue.size >= @capacity
     end
 
-    def ready?
-      Fiber.current.timed_out?
-    end
-
-    def execute : Nil
-      Fiber.current.timed_out = false
-    end
-
-    def wait
-      Fiber.timeout @timeout_at - Time.monotonic
-    end
-
-    def unwait
-      Fiber.cancel_timeout
-    end
+    !@senders.empty?
   end
-end
 
-def timeout_select_action(timeout)
-  Channel::TimeoutAction.new(timeout)
+  def empty?
+    if queue = @queue
+      return queue.empty?
+    end
+
+    @senders.empty?
+  end
 end
