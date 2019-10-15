df8db7f6b3922db1af0ed685ed01eb1f133b5543
Carl Hörberg <carl.hoerberg@gmail.com>
2019-10-15 23:56:17 +0200

    fix unix server spec in MT

--- spec/unix_server_spec.cr
+++ spec/unix_server_spec.cr
@@ -4,7 +4,7 @@ describe AvalancheMQ::Server do
   describe "UNIX Sockets" do
     it "can accept UNIX socket connections" do
       spawn { s.listen_unix("/tmp/spec/avalanchemq.sock", 0) }
-      Fiber.yield
+      sleep 0.1
       with_channel(host: "/tmp/spec/avalanchemq.sock") do |ch|
         ch.should_not be_nil
       end
