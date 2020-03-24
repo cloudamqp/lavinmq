101b91804f89f7cddecbc87bc4fab4f37406a728
Carl Hörberg <carl.hoerberg@gmail.com>
2020-03-24 13:19:40 +0100

    rename File#hint_target_size to File#allocate

--- src/stdlib/file.cr
+++ src/stdlib/file.cr
@@ -1,9 +1,12 @@
 # No PR yet
 class File
-  def hint_target_size(size)
+  def allocate(size, offset = 0, keep_size = false)
     {% if flag?(:linux) %}
-      if LibC.fallocate(fd, LibC::FALLOC_FL_KEEP_SIZE, 0, size) != 0
-        raise Errno.new("fallocate failed")
+      flags = case
+              when keep_size then LibC::FALLOC_FL_KEEP_SIZE
+              end
+      if LibC.fallocate(fd, flags, offset, size) != 0
+        raise Errno.new("fallocate")
       end
     {% end %}
   end
@@ -11,7 +14,7 @@ class File
   def advise(advice)
     {% if flag?(:linux) %}
       if LibC.posix_fadvise(fd, 0, 0, advice) != 0
-        raise Errno.new("fadvise failed")
+        raise Errno.new("fadvise")
       end
     {% end %}
   end
