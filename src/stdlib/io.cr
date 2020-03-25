175f0040204a73a1b68339bfead0a698b56e1d02
Carl Hörberg <carl.hoerberg@gmail.com>
2020-03-25 17:01:51 +0100

    larger buffer in IO.copy(src, dst)

--- src/stdlib/io.cr
+++ src/stdlib/io.cr
@@ -1,4 +1,14 @@
 class IO
+  def self.copy(src, dst) : UInt64
+    buffer = uninitialized UInt8[131_072]
+    count = 0_u64
+    while (len = src.read(buffer.to_slice).to_i32) > 0
+      dst.write buffer.to_slice[0, len]
+      count += len
+    end
+    count
+  end
+
   def self.copy(src, dst, limit : Int) : UInt64
     return 0_u64 if limit.zero?
     raise ArgumentError.new("Negative limit") if limit < 0
