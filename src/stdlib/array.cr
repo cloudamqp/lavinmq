c5cdb05b8539ced9cca63cfe3b05ab78595ca9e6
Carl Hörberg <carl.hoerberg@gmail.com>
2020-04-25 11:09:43 +0200

    SortedSet class

--- src/stdlib/array.cr
+++ src/stdlib/array.cr
@@ -2,4 +2,50 @@ class Array(T)
   def capacity
     @capacity
   end
+
+  def insert_sorted(v : T)
+    i = bsearch_index { |x| x >= v }
+    if i
+      insert(i, v) unless self.[i] == v
+    else
+      push(v)
+    end
+  end
+end
+
+class SortedSet(T)
+  def initialize
+    @array = Array(T).new
+  end
+
+  def push(v : T)
+    i = @array.bsearch_index { |x| x >= v }
+    if i
+      if @array[i] != v
+        @array.insert(i, v)
+      end
+    else
+      @array.push(v)
+    end
+  end
+
+  def <<(v : T)
+    push(v)
+  end
+
+  def each
+    @array.each
+  end
+
+  def clear
+    @array.clear
+  end
+
+  def size
+    @array.size
+  end
+
+  def capacity
+    @array.capacity
+  end
 end
