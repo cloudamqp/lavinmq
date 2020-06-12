03bb8142fce0bce12411d699f50c97a3d2c8b2c4
Carl Hörberg <carl.hoerberg@gmail.com>
2020-06-12 12:44:09 +0200

    MFile improvements/refactoring

--- src/avalanchemq/mfile.cr
+++ src/avalanchemq/mfile.cr
@@ -1,15 +1,24 @@
-require "../stdlib/libc"
+lib LibC
+  MS_ASYNC = 0x0001
+  MS_SYNC = 0x0004
+  fun munmap(addr : Void*, len : SizeT) : Int
+  fun msync(addr : Void*, len : SizeT, flags : Int) : Int
+  fun truncate(path : Char*, len : OffT) : Int
+end
+
 # Memory mapped file
+# Max 2GB files (Slice is currently limited to Int32)
+# Beware, the file will be truncated to the position you're at when closing
 class MFile < IO
   property pos = 0
   getter? closed = false
   getter capacity = 0
   @buffer : Bytes
 
-  def initialize(@path : String, capacity)
+  def initialize(@path : String, capacity : Int, @readonly = false)
     @capacity = capacity.to_i32
     fd = open_fd
-    truncate(fd, @capacity)
+    truncate(fd, @capacity) unless @readonly
     @buffer = mmap(fd)
     close_fd(fd)
   end
@@ -17,72 +26,84 @@ class MFile < IO
   # mmaps an existing file
   def self.open(path)
     if info = File.info? path
-      self.new(path, info.size)
+      self.new(path, info.size, readonly: true)
     else
-      raise File::Error.new "File not found", file: path
+      raise File::Error.new("File not found", file: path)
     end
   end
 
   private def open_fd
     flags = LibC::O_CREAT | LibC::O_RDWR
     fd = LibC.open(@path.check_no_null_byte, flags, 0o644)
-    if fd < 0
-      raise ::File::Error.from_errno("Error opening file", file: @path)
-    end
+    raise File::Error.from_errno("Error opening file", file: @path) if fd < 0
     fd
   end
 
   private def truncate(fd, size)
     code = LibC.ftruncate(fd, size)
-    if code != 0
-      raise ::File::Error.from_errno("Error truncating file", file: @path)
-    end
+    raise File::Error.from_errno("Error truncating file", file: @path) if code < 0
   end
 
   private def mmap(fd) : Bytes
-    protection = LibC::PROT_READ | LibC::PROT_WRITE
+    protection = LibC::PROT_READ
+    protection |= LibC::PROT_WRITE unless @readonly
     flags = LibC::MAP_SHARED
     buffer = LibC.mmap(nil, @capacity, protection, flags, fd, 0).as(UInt8*)
     raise RuntimeError.from_errno("mmap") if buffer == LibC::MAP_FAILED
-    buffer.to_slice(@capacity)
+    Bytes.new(buffer, @capacity, read_only: @readonly)
   end
 
   private def close_fd(fd)
-    res = LibC.close(fd)
-    raise ::File::Error.from_errno("Error closing file", file: @path) unless res.zero?
+    code = LibC.close(fd)
+    raise File::Error.from_errno("Error closing file", file: @path) if code < 0
   end
+
+  @deleted = false
 
   def delete
-    err = LibC.unlink(@path.check_no_null_byte)
-    if err == -1
-      raise ::File::Error.from_errno("Error deleting file", file: path)
-    end
+    code = LibC.unlink(@path.check_no_null_byte)
+    raise File::Error.from_errno("Error deleting file", file: @path) if code < 0
+    @deleted = true
   end
 
+  # Unmapping the file
+  # The file will be truncated to the current position unless readonly or deleted
   def close : Nil
     return if @closed
     @closed = true
-    res = LibC.munmap(@buffer, @capacity)
-    raise RuntimeError.from_errno("munmap") if res == -1
-    #res = LibC.truncate(@path.check_no_null_byte, @pos)
-    #raise RuntimeError.from_errno("truncate") if res == -1
+    code = LibC.munmap(@buffer, @capacity)
+    raise RuntimeError.from_errno("Error unmapping file") if code == -1
+    return if @readonly || @deleted
+    code = LibC.truncate(@path.check_no_null_byte, @pos)
+    raise File::Error.from_errno("Error truncating file", file: @path) if code < 0
   end
 
-  def closed?
-    @closed
+  @synced_pos = 0
+
+  def flush(from = @synced_pos, to = @pos)
+    async(from, to)
   end
 
-  def flush
-    msync(@buffer.to_unsafe, @pos, LibC::MS_ASYNC)
+  def fsync(from = @synced_pos, to = @pos)
+    sync(from, to)
   end
 
-  def fsync
-    msync(@buffer.to_unsafe, @pos, LibC::MS_SYNC)
+  def async(from = @synced_pos, to = @pos)
+    addr = @buffer + from
+    len = to - from
+    msync(addr.to_unsafe, len, LibC::MS_ASYNC)
+  end
+
+  def sync(from = @synced_pos, to = @pos)
+    addr = @buffer + from
+    len = to - from
+    msync(addr.to_unsafe, len, LibC::MS_SYNC)
+    @synced_pos = @pos
   end
 
   private def msync(addr, len, flag) : Nil
-    res = LibC.msync(addr, len, flag)
-    raise RuntimeError.from_errno("msync") if res == -1
+    code = LibC.msync(addr, len, flag)
+    raise RuntimeError.from_errno("msync") if code < 0
   end
 
   def finalize
