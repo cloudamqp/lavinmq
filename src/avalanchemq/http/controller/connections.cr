df2965528f6811673400ff0b52888a41a12036c9
Carl Hörberg <carl.hoerberg@gmail.com>
2020-08-24 23:15:00 +0200

    fixing bug introduced in last commit in connections controller

--- src/avalanchemq/http/controller/connections.cr
+++ src/avalanchemq/http/controller/connections.cr
@@ -24,7 +24,7 @@ module AvalancheMQ
         get "/api/vhosts/:vhost/connections" do |context, params|
           with_vhost(context, params) do |vhost|
             refuse_unless_management(context, user(context), vhost)
-            page(context, @amqp_server.vhost_connections(vhost))
+            page(context, @amqp_server.vhost_connections(vhost).each)
           end
         end
 
