f5c7a1bcc52069e05b06daab04e82f21d07e7759
Anton Dalgren <anton@84codes.com>
2020-09-21 16:33:49 +0200

    Fix failing specs

--- spec/api/node_spec.cr
+++ spec/api/node_spec.cr
@@ -9,11 +9,21 @@ describe AvalancheMQ::HTTP::NodesController do
       body = JSON.parse(response.body)
       body.as_a.empty?.should be_false
       data = body.as_a.first.as_h
-      data["connection_created"].as_i.should eq 0.0
-      data["connection_closed"].as_i.should eq 0.0
+      keys = ["connection_created", "connection_closed"]
+      keys.each do |key|
+        data.has_key?(key).should be_true
+      end
     end
 
     it "should update queue data" do
+      sleep 5.1 # Let stats_loop run once
+      response = get("/api/nodes")
+
+      body = JSON.parse(response.body)
+      data = body.as_a.first.as_h
+      declared_queues = data["queue_declared"].as_i
+      deleted_queues = data["queue_deleted"].as_i
+
       s.vhosts["/"].declare_queue("q0", false, false)
       sleep 5.1 # Let stats_loop run once
       response = get("/api/nodes")
@@ -21,8 +31,8 @@ describe AvalancheMQ::HTTP::NodesController do
       body = JSON.parse(response.body)
       body.as_a.empty?.should be_false
       data = body.as_a.first.as_h
-      data["queue_declared"].as_i.should eq 1
-      data["queue_deleted"].as_i.should eq 0
+      data["queue_declared"].as_i.should eq (declared_queues + 1)
+      data["queue_deleted"].as_i.should eq deleted_queues
       s.vhosts["/"].delete_queue("q0")
       sleep 5.1
       response = get("/api/nodes")
@@ -30,8 +40,8 @@ describe AvalancheMQ::HTTP::NodesController do
       body = JSON.parse(response.body)
       body.as_a.empty?.should be_false
       data = body.as_a.first.as_h
-      data["queue_declared"].as_i.should eq 1
-      data["queue_deleted"].as_i.should eq 1
+      data["queue_declared"].as_i.should eq (declared_queues + 1)
+      data["queue_deleted"].as_i.should eq (deleted_queues + 1)
     end
   end
 end
