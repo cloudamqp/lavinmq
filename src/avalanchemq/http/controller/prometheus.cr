9e4d90a985ed938ec23a0af6552d932ec633cee4
Magnus Landerblom <magnus@landerblom.se>
2020-09-25 09:45:11 +0200

    optimisations

--- src/avalanchemq/http/controller/prometheus.cr
+++ src/avalanchemq/http/controller/prometheus.cr
@@ -7,62 +7,64 @@ require "../binding_helpers"
 module AvalancheMQ
   module HTTP
     class PrometheusController < Controller
-      alias MetricValue = UInt64 | Int64 | Int32
-      struct Metric
-        def initialize(@name : String, @value : MetricValue, @labels : Hash(String, String)?)
+      private def append_labels(io, labels)
+        key = true
+        first = true
+        io << "{"
+        labels.each do |v|
+          io << ", " if !first && key
+          io << v << "=" if key
+          io << "\"" << v << "\"" unless key
+          key = !key
+          first = false
         end
-        def initialize(@name : String, @value : MetricValue)
-        end
-        def to_s(io : IO)
+        io << "}"
+      end
+
+      private def append(io, name, value, labels = Tuple.new)
           io << "avalanchemq_"
-          io << @name
-          if l = @labels
-            first = true
-            io << "{"
-            l.each do |k, v|
-              io << "," unless first
-              io << k << "=" << v
-              first = false
-            end
-            io << "}"
+          case name
+          when Tuple
+            io << name.join("_")
+          else
+            io << name
           end
-          io << " " << @value << "\n"
-        end
+          append_labels(io, labels) unless labels.empty?
+          io << " " << value << "\n"
       end
 
       private def register_routes
         get "/metrics" do |context, _|
           o = context.response
           u = user(context)
-          info = { "avalanchemq_version" => AvalancheMQ::VERSION }
-          Metric.new("server_info", 1, info).to_s(o)
-          Metric.new("server_info_uptime", @amqp_server.uptime.to_i).to_s(o)
-          Metric.new("server_cpu_system_time", @amqp_server.sys_time).to_s(o)
-          Metric.new("server_cpu_user_time", @amqp_server.user_time).to_s(o)
-          Metric.new("server_cpu_system_time", @amqp_server.sys_time).to_s(o)
-          Metric.new("server_rss_bytes", @amqp_server.rss).to_s(o)
-          Metric.new("server_disk_total_bytes", @amqp_server.disk_total).to_s(o)
-          Metric.new("server_disk_free_bytes", @amqp_server.disk_free).to_s(o)
+          append(o, "server_info", 1, { "avalanchemq_version", AvalancheMQ::VERSION })
+          append(o, "server_uptime_seconds", @amqp_server.uptime.to_i)
+          append(o, "server_cpu_system_time", @amqp_server.sys_time)
+          append(o, "server_cpu_system_time", @amqp_server.sys_time)
+          append(o, "server_cpu_user_time", @amqp_server.user_time)
+          append(o, "server_cpu_system_time", @amqp_server.sys_time)
+          append(o, "server_rss_bytes", @amqp_server.rss)
+          append(o, "server_disk_total_bytes", @amqp_server.disk_total)
+          append(o, "server_disk_free_bytes", @amqp_server.disk_free)
           vhosts(u).each do |vhost|
             vhost.message_details.each do |k, v|
-              l = { "name" => vhost.name }
-              Metric.new("vhost_#{k}", v, l).to_s(o)
+              append(o, {"vhost", k}, v, { "name", vhost.name })
             end
             vhost.exchanges.each_value do |e|
-              l = { "name" => e.name, "vhost" => vhost.name }
-              Metric.new("exchange_publish_in", e.publish_in_count, l).to_s(o)
-              Metric.new("exchange_publish_out", e.publish_out_count, l).to_s(o)
-              Metric.new("exchange_unroutable", e.unroutable_count, l).to_s(o)
-            end
-            vhost.queues.each_value do |q|
-              l = { "name" => q.name, "vhost" => vhost.name }
-              Metric.new("queue_ack", q.ack_count, l).to_s(o)
-              Metric.new("queue_deliver", q.deliver_count, l).to_s(o)
-              Metric.new("queue_get", q.get_count, l).to_s(o)
-              Metric.new("queue_publish", q.publish_count, l).to_s(o)
-              Metric.new("queue_redeliver", q.redeliver_count, l).to_s(o)
-              Metric.new("queue_reject", q.reject_count, l).to_s(o)
-            end
+               l = { "name", e.name, "vhost", vhost.name }
+               append(o, "exchange_publish_in", e.publish_in_count, l)
+               append(o, "exchange_publish_out", e.publish_out_count, l)
+               append(o, "exchange_unroutable", e.unroutable_count, l)
+             end
+             vhost.queues.each_value do |q|
+               l = { "name", q.name, "vhost", vhost.name }
+               append(o, "queue_ack", q.ack_count, l)
+               append(o, "queue_deliver", q.deliver_count, l)
+               append(o, "queue_get", q.get_count, l)
+               append(o, "queue_publish", q.publish_count, l)
+               append(o, "queue_redeliver", q.redeliver_count, l)
+               append(o, "queue_reject", q.reject_count, l)
+             end
           end
           context
         end
