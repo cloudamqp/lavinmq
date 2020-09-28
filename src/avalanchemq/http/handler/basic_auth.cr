3d9cdcea060588ce55c59256d23e3f6d62af3f31
Carl Hörberg <carl.hoerberg@gmail.com>
2020-09-28 14:13:05 +0200

    refactor basic_auth for clarity

--- src/avalanchemq/http/handler/basic_auth.cr
+++ src/avalanchemq/http/handler/basic_auth.cr
@@ -10,49 +10,53 @@ module AvalancheMQ
       def initialize(@user_store : UserStore, @log : Logger)
       end
 
-      def require_auth(context)
-        context.request.path.starts_with?("/api/") ||
-        context.request.path = "/metrics"
+      def call(context)
+        if public_path?(context.request.path)
+          return call_next(context)
+        end
+
+        if internal_unix_socket?(context)
+          context.authenticated_username = UserStore::DIRECT_USER
+          return call_next(context)
+        end
+
+        if auth = context.request.headers.fetch("Authorization", nil)
+          if auth.starts_with? "Basic "
+            base64 = auth[6..-1]
+            begin
+              username, password = Base64.decode_string(base64).split(":")
+              if user = @user_store[username]?
+                if valid_auth?(user, password)
+                  context.authenticated_username = username
+                  return call_next(context)
+                end
+              end
+            rescue Base64::Error
+            end
+          end
+        end
+        forbidden(context)
       end
 
-      def internal_unix_socket?(context)
+      private def public_path?(request_path) : Bool
+        !request_path.starts_with?("/api/") && !request_path == "/metrics"
+      end
+
+      private def internal_unix_socket?(context) : Bool
         context.request.remote_address.to_s == HTTP::INTERNAL_UNIX_SOCKET
       end
 
-      def forbidden(context)
+      private def forbidden(context)
         context.response.status_code = 401
         unless context.request.path.starts_with?("/api/whoami")
           context.response.headers["WWW-Authenticate"] = %(Basic realm="Login Required")
         end
       end
 
-      def valid_auth?(user, password)
-        !user.tags.empty? && user.password.to_s.size > 0 &&
-          user.password && user.password.not_nil!.verify(password)
-      end
-
-      def call(context)
-        unless require_auth(context)
-          return call_next(context)
-        end
-        auth = context.request.headers.fetch("Authorization", nil)
-        if auth && auth.starts_with? "Basic "
-          base64 = auth[6..-1]
-          begin
-            username, password = Base64.decode_string(base64).split(":")
-            if user = @user_store[username]?
-              if valid_auth?(user, password)
-                context.authenticated_username = username
-                return call_next(context)
-              end
-            end
-          rescue Base64::Error
-          end
-        elsif internal_unix_socket?(context)
-          context.authenticated_username = UserStore::DIRECT_USER
-          return call_next(context)
-        end
-        forbidden(context)
+      private def valid_auth?(user, password) : Bool
+        return false if user.tags.empty?
+        return false if user.password.to_s.empty?
+        user.password.not_nil!.verify(password)
       end
     end
   end
