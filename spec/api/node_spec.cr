fa4eb97c9b3118e9835fc9bb2ea44f121e15cc15
Anton Dalgren <anton@84codes.com>
2020-10-20 11:08:00 +0200

    Use manual stats update function

--- spec/api/node_spec.cr
+++ spec/api/node_spec.cr
@@ -16,16 +16,16 @@ describe AvalancheMQ::HTTP::NodesController do
     end
 
     it "should update queue data" do
-      sleep 5.1 # Let stats_loop run once
-      response = get("/api/nodes")
+      s.update_stats_rates()
 
+      response = get("/api/nodes")
       body = JSON.parse(response.body)
       data = body.as_a.first.as_h
       declared_queues = data["queue_declared"].as_i
       deleted_queues = data["queue_deleted"].as_i
-
       s.vhosts["/"].declare_queue("q0", false, false)
-      sleep 5.1 # Let stats_loop run once
+      s.update_stats_rates()
+
       response = get("/api/nodes")
       response.status_code.should eq 200
       body = JSON.parse(response.body)
@@ -34,7 +34,8 @@ describe AvalancheMQ::HTTP::NodesController do
       data["queue_declared"].as_i.should eq (declared_queues + 1)
       data["queue_deleted"].as_i.should eq deleted_queues
       s.vhosts["/"].delete_queue("q0")
-      sleep 5.1
+      s.update_stats_rates()
+
       response = get("/api/nodes")
       response.status_code.should eq 200
       body = JSON.parse(response.body)
@@ -45,22 +46,32 @@ describe AvalancheMQ::HTTP::NodesController do
     end
 
     it "should not delete stats when connection is closed" do
-      with_channel do
-        sleep 5.1 # Let stats_loop run once
-        response = get("/api/nodes")
-        response.status_code.should eq 200
-        body = JSON.parse(response.body)
-        data = body.as_a.first.as_h
-        data["channel_created"].as_i.should eq 1
-        data["channel_closed"].as_i.should eq 0
-      end
-      sleep 5.1 # Let stats_loop run once
+      s.update_stats_rates()
+
       response = get("/api/nodes")
       response.status_code.should eq 200
       body = JSON.parse(response.body)
       data = body.as_a.first.as_h
-      data["channel_created"].as_i.should eq 1
-      data["channel_closed"].as_i.should eq 1
+      channels_created = data["channel_created"].as_i
+      channels_closed = data["channel_closed"].as_i
+      with_channel do
+        s.update_stats_rates()
+
+        response = get("/api/nodes")
+        response.status_code.should eq 200
+        body = JSON.parse(response.body)
+        data = body.as_a.first.as_h
+        data["channel_created"].as_i.should eq (channels_created + 1)
+        data["channel_closed"].as_i.should eq (channels_closed)
+      end
+      s.update_stats_rates()
+
+      response = get("/api/nodes")
+      response.status_code.should eq 200
+      body = JSON.parse(response.body)
+      data = body.as_a.first.as_h
+      data["channel_created"].as_i.should eq (channels_created + 1)
+      data["channel_closed"].as_i.should eq (channels_closed + 1)
     end
   end
 end
