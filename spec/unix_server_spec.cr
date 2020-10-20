d4ff4d0b3a0751f7d7cbcf39266920ee8ca96f11
Patrik Ragnarsson <patrik@starkast.net>
2020-10-20 10:49:20 +0200

    Update unix server spec
    The method signature changed in 65d17deac51ac8c24c8d8621af7c128571876023

--- spec/unix_server_spec.cr
+++ spec/unix_server_spec.cr
@@ -3,7 +3,7 @@ require "./spec_helper"
 describe AvalancheMQ::Server do
   describe "UNIX Sockets" do
     it "can accept UNIX socket connections" do
-      spawn { s.listen_unix("/tmp/spec/avalanchemq.sock", 0) }
+      spawn { s.listen_unix("/tmp/spec/avalanchemq.sock") }
       sleep 0.1
       with_channel(host: "/tmp/spec/avalanchemq.sock") do |ch|
         ch.should_not be_nil
