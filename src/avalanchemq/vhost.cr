1c459a43d075e5583f62469a0d776034a799f2c3
Anton Dalgren <anton@84codes.com>
2020-10-20 11:10:30 +0200

    Do not count on loading

--- src/avalanchemq/vhost.cr
+++ src/avalanchemq/vhost.cr
@@ -360,7 +360,7 @@ module AvalancheMQ
       when AMQP::Frame::Queue::Declare
         return false if @queues.has_key? f.queue_name
         q = @queues[f.queue_name] = QueueFactory.make(self, f)
-        @churn_events.send({:queue_declared, 1_u32})
+        @churn_events.send({:queue_declared, 1_u32}) unless loading
         apply_policies([q] of Queue) unless loading
       when AMQP::Frame::Queue::Delete
         if q = @queues.delete(f.queue_name)
@@ -369,7 +369,7 @@ module AvalancheMQ
               ex.unbind(q, *binding_args) if destinations.includes?(q)
             end
           end
-          @churn_events.send({:queue_deleted, 1_u32})
+          @churn_events.send({:queue_deleted, 1_u32}) unless loading
           q.delete
         else
           return false
