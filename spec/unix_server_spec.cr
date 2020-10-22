4d2282e9f638efcae7c3d8ff2c525e9c3672545e
Patrik Ragnarsson <patrik@starkast.net>
2020-10-22 10:03:38 +0200

    Mark "UNIX Sockets" spec as pending for now

--- spec/unix_server_spec.cr
+++ spec/unix_server_spec.cr
@@ -2,7 +2,7 @@ require "./spec_helper"
 
 describe AvalancheMQ::Server do
   describe "UNIX Sockets" do
-    it "can accept UNIX socket connections" do
+    pending "can accept UNIX socket connections" do
       spawn { s.listen_unix("/tmp/spec/avalanchemq.sock") }
       sleep 0.1
       with_channel(host: "/tmp/spec/avalanchemq.sock") do |ch|
