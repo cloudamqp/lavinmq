4d5f26584bd6aa3250135eca212259abb6f0dbb9
Patrik Ragnarsson <patrik@starkast.net>
2020-10-29 12:15:20 +0100

    Let the static_requests.sh test all static files
    As suggested in #140

--- extras/static_requests.sh
+++ extras/static_requests.sh
@@ -6,17 +6,19 @@ PORT=${PORT:-15672}
 FAILURES=0
 
 request() {
-  local path=$1
-  local body_size=$(curl --silent --output /dev/null --write-out "%{size_download}\n" "http://127.0.0.1:${PORT}${path}")
+  local path=${1}
+  local body_size=$(curl --silent --output /dev/null --write-out \
+  "%{size_download}\n" "http://127.0.0.1:${PORT}/${path}")
 
   [[ $body_size -eq 0 ]] && echo "FAIL: $path" && ((FAILURES += 1)) || :
 }
 
-request /
-request /robots.txt
-request /img/favicon.png
-request /test/
-request /test
-request /test/index.html
+request # request to /
+
+# test all static files, except empty ones like .gitkeep
+for path in $(find static -type f ! -path '*/.*' | sed 's/static\///')
+do
+    request $path
+done
 
 [[ $FAILURES -gt 0 ]] && exit 1 || true
