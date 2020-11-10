f778abdccea61ed46e419883a23ee7d8f153b96a
Magnus Landerblom <magnus@landerblom.se>
2020-11-10 13:41:37 +0100

    Webhook shovel
    HTTP shovel: Allow setting URI path using headers
    
    Setting the header "uri_path" on a message will make the value
    of that header be used as path for the HTTP request
    
    This is useful when someone need dynamic routing for each message

--- src/avalanchemq/shovel/shovel.cr
+++ src/avalanchemq/shovel/shovel.cr
@@ -1,140 +1,14 @@
 require "logger"
 require "../sortable_json"
 require "amqp-client"
+require "http/client"
 
 module AvalancheMQ
-  class Shovel
-    include SortableJSON
-    @log : Logger
-    @state = State::Terminated
-    @stop = ::Channel(Exception).new
-
-    getter name, vhost
-
+  module Shovel
     DEFAULT_ACK_MODE        = AckMode::OnConfirm
-    DEFUALT_RECONNECT_DELAY = 5
-    DEFUALT_DELETE_AFTER    = DeleteAfter::Never
+    DEFAULT_DELETE_AFTER    = DeleteAfter::Never
     DEFAULT_PREFETCH        = 1000_u16
-
-    def initialize(@source : Source, @destination : Destination, @name : String, @vhost : VHost,
-                   @ack_mode = DEFAULT_ACK_MODE, @reconnect_delay = DEFUALT_RECONNECT_DELAY)
-      @log = @vhost.log.dup
-      @log.progname += " shovel=#{@name}"
-    end
-
-    def state
-      @state.to_s
-    end
-
-    def run
-      @log.info { "Created and Starting" }
-      @state = State::Starting
-      spawn(run_loop, name: "Shovel #{@vhost.name}/#{@name}")
-      Fiber.yield
-    end
-
-    def details_tuple
-      {
-        name:  @name,
-        vhost: @vhost.name,
-        state: @state.to_s,
-      }
-    end
-
-    private def run_loop
-      loop do
-        @state = State::Starting
-        ::AMQP::Client.start(@source.uri) do |c|
-          ::AMQP::Client.start(@destination.uri) do |p|
-            cch, q = setup_queue(c)
-            cch.prefetch @source.prefetch
-            return if @source.delete_after == DeleteAfter::QueueLength && q[:message_count].zero?
-
-            pch = p.channel
-            pch.confirm_select if @ack_mode == AckMode::OnConfirm
-            no_ack = @ack_mode == AckMode::NoAck
-            @state = State::Running
-
-            cch.basic_consume(q[:queue_name], no_ack: no_ack, tag: "Shovel") do |msg|
-              shovel(msg, pch, q[:message_count])
-            end
-
-            if ex = @stop.receive?
-              raise ex
-            end
-            return
-          end
-        end
-      rescue ex
-        @state = State::Stopped
-        @log.error { "Shovel error: #{ex.message}" }
-        select
-        when @stop.receive?
-          break
-        when timeout @reconnect_delay.seconds
-          @log.info { "Shovel try reconnect" }
-        end
-      end
-    ensure
-      @state = State::Terminated
-    end
-
-    private def setup_queue(c)
-      cch = c.channel
-      name = @source.queue || ""
-      q = begin
-        cch.queue_declare(name, passive: true)
-      rescue ::AMQP::Client::Channel::ClosedException
-        cch = c.channel
-        cch.queue_declare(name, passive: false)
-      end
-      if @source.exchange || @source.exchange_key
-        cch.queue_bind(q[:queue_name], @source.exchange || "", @source.exchange_key || "")
-      end
-      return {cch, q}
-    end
-
-    private def shovel(msg, pch, queue_length)
-      ex = @destination.exchange || msg.exchange
-      rk = @destination.exchange_key || msg.routing_key
-      msgid = pch.basic_publish(msg.body_io, ex, rk)
-      delete_after_this =
-        @source.delete_after == DeleteAfter::QueueLength &&
-          msg.delivery_tag == queue_length
-      should_multi_ack = msgid % (@source.prefetch / 2).ceil.to_i == 0
-      if delete_after_this || should_multi_ack
-        case @ack_mode
-        when AckMode::OnConfirm
-          pch.wait_for_confirm(msgid)
-          msg.ack(multiple: true)
-        when AckMode::OnPublish
-          msg.ack(multiple: true)
-        when AckMode::NoAck
-          nil
-        end
-      end
-      if delete_after_this
-        @stop.close
-      end
-    rescue ex
-      @stop.send ex unless @stop.closed?
-    end
-
-    # Does not trigger reconnect, but a graceful close
-    def terminate
-      return if terminated?
-      @stop.close
-      @log.info { "Terminated" }
-    end
-
-    def delete
-      terminate
-      @vhost.delete_parameter("shovel", @name)
-    end
-
-    def terminated?
-      @state == State::Terminated
-    end
+    DEFAULT_RECONNECT_DELAY = 5
 
     enum State
       Starting
@@ -154,13 +28,18 @@ module AvalancheMQ
       NoAck
     end
 
-    struct Source
-      getter uri, queue, exchange, exchange_key, delete_after, prefetch
+    class FailedDeliveryError <  Exception
+    end
 
-      def initialize(raw_uri : String, @queue : String?, @exchange : String? = nil,
-                     @exchange_key : String? = nil,
-                     @delete_after = DEFUALT_DELETE_AFTER, @prefetch = DEFAULT_PREFETCH)
-        @uri = URI.parse(raw_uri)
+    class AMQPSource
+      @conn : ::AMQP::Client::Connection?
+      @ch : ::AMQP::Client::Channel?
+      @q : NamedTuple(queue_name: String, message_count: UInt32, consumer_count: UInt32)?
+      @last_unacked : UInt64?
+
+      def initialize(@name : String, @uri : URI, @queue : String?, @exchange : String? = nil,
+          @exchange_key : String? = nil,
+          @delete_after = DEFAULT_DELETE_AFTER, @prefetch = DEFAULT_PREFETCH, @ack_mode = DEFAULT_ACK_MODE)
         cfg = Config.instance
         @uri.host ||= "#{cfg.amqp_bind}:#{cfg.amqp_port}"
         unless @uri.user
@@ -172,14 +51,82 @@ module AvalancheMQ
           raise ArgumentError.new("Shovel source requires a queue or an exchange")
         end
       end
+
+      def start
+        @conn = conn = ::AMQP::Client.new(@uri).connect
+        @ch = ch = conn.channel
+        ch.prefetch @prefetch
+        q_name = @queue || ""
+        @q = q = begin
+              ch.queue_declare(q_name, passive: true)
+            rescue ::AMQP::Client::Channel::ClosedException
+              ch = conn.channel
+              ch.queue_declare(q_name, passive: false)
+            end
+        if @exchange || @exchange_key
+          ch.queue_bind(q[:queue_name], @exchange || "", @exchange_key || "")
+        end
+      end
+
+      def stop
+        # If we have any outstanding messages when closing, ack them first.
+        @ch.try do |ch|
+          # Might end up with channel closed
+          next if ch.closed?
+          @last_unacked.try { |t| ch.basic_ack(t, multiple: true) }
+        end
+        @conn.try &.close
+      end
+
+      def each(&blk : ::AMQP::Client::DeliverMessage -> Nil)
+        raise "Not started" unless @q
+        q = @q.not_nil!
+        ch = @ch.not_nil!
+        queue_length = q[:message_count]
+        limited = @delete_after == DeleteAfter::QueueLength
+        should_ack = @ack_mode != AckMode::NoAck
+        return if limited && queue_length.zero?
+        tag = "Shovel[#{@name}]"
+        ch.basic_consume(q[:queue_name],
+                         no_ack: !should_ack,
+                         block: true,
+                         tag: tag) do |msg|
+          blk.call(msg)
+
+          # We batch ack for faster shovel
+          batch_full = msg.delivery_tag % (@prefetch / 2).ceil.to_i == 0
+          at_end = limited && msg.delivery_tag == queue_length
+          if (batch_full || at_end) && should_ack
+            msg.ack(multiple: true)
+          else
+            @last_unacked = msg.delivery_tag
+          end
+
+          # Reached end, cancel consumer
+          ch.not_nil!.basic_cancel(tag) if at_end
+        rescue e : FailedDeliveryError
+          msg.reject
+        end
+      ensure
+        ch.try &.close
+      end
     end
 
-    struct Destination
-      getter uri, exchange, exchange_key
+    abstract class Destination
+      abstract def start
 
-      def initialize(raw_uri : String, queue : String?,
-                     @exchange : String? = nil, @exchange_key : String? = nil)
-        @uri = URI.parse(raw_uri)
+      abstract def stop
+
+      abstract def push(msg)
+    end
+
+    class AMQPDestination < Destination
+      @conn : ::AMQP::Client::Connection?
+      @ch : ::AMQP::Client::Channel?
+
+      def initialize(@name : String, @uri : URI, @queue : String?, @exchange : String? = nil,
+          @exchange_key : String? = nil,
+          @delete_after = DEFAULT_DELETE_AFTER, @prefetch = DEFAULT_PREFETCH, @ack_mode = DEFAULT_ACK_MODE)
         cfg = Config.instance
         @uri.host ||= "#{cfg.amqp_bind}:#{cfg.amqp_port}"
         unless @uri.user
@@ -194,6 +141,149 @@ module AvalancheMQ
         if @exchange.nil?
           raise ArgumentError.new("Shovel destination requires an exchange")
         end
+      end
+
+      def start
+        conn = ::AMQP::Client.new(@uri).connect
+        @conn = conn
+        @ch = conn.channel
+      end
+
+      def stop
+        @conn.try &.close
+      end
+
+      def push(msg)
+        raise "Not started" if @ch.nil?
+        ch = @ch.not_nil!
+        ch.confirm_select if @ack_mode == AckMode::OnConfirm
+        msgid = ch.basic_publish(
+          msg.body_io,
+          @exchange || msg.exchange,
+          @exchange_key || msg.routing_key)
+        ch.wait_for_confirm(msgid) if @ack_mode == AckMode::OnConfirm
+      end
+    end
+
+    class HTTPDestination < Destination
+      @client : ::HTTP::Client?
+
+      def initialize(@name : String, @uri : URI, @ack_mode = DEFAULT_ACK_MODE)
+      end
+
+      def start
+        client = ::HTTP::Client.new @uri
+        client.connect_timeout = 10
+        client.read_timeout = 30
+        client.basic_auth(@uri.user, @uri.password || "") if @uri.user
+        @client = client
+      end
+
+      def stop
+        @client.try &.close
+      end
+
+      def push(msg)
+        raise "Not started" unless @client
+        c = @client.not_nil!
+        headers = ::HTTP::Headers{"User-Agent" => "AvalancheMQ"}
+        headers["X-Shovel"] = @name
+        msg.properties.content_type.try { |v| headers["Content-Type"] = v }
+        msg.properties.message_id.try { |v| headers["X-Message-Id"] = v }
+        msg.properties.headers.try do |hs|
+          hs.each do |k, v|
+            headers["X-#{k}"] = v.to_s
+          end
+        end
+        path = case
+               when !@uri.path.empty?
+                 @uri.path
+               when p = msg.properties.headers.try { |h| h["uri_path"]? }
+                 p.to_s
+               else
+                 "/"
+               end
+        response = c.post(path, headers: headers, body: msg.body_io)
+        raise FailedDeliveryError.new if @ack_mode == AckMode::OnConfirm && !response.success?
+      end
+    end
+
+    class Runner
+      include SortableJSON
+      @log : Logger
+      @state = State::Terminated
+      @error : String?
+      @message_count : UInt64 = 0
+
+      getter name, vhost
+
+      def initialize(@source : AMQPSource, @destination : Destination,
+          @name : String, @vhost : VHost, @reconnect_delay = DEFAULT_RECONNECT_DELAY)
+        @log = @vhost.log.dup
+        @log.progname += " shovel=#{@name}"
+      end
+
+      def state
+        @state.to_s
+      end
+
+      def run
+        loop do
+          @state = State::Starting
+          @log.info { "starting..." }
+          @source.start
+          @destination.start
+          @log.info { "started..." }
+          @state = State::Running
+          @source.each do |msg|
+            @message_count += 1
+            @destination.push(msg)
+          end
+          break
+        rescue ex : ::AMQP::Client::Connection::ClosedException
+          @log.error { ex.message }
+          @error = ex.message
+          @state = State::Stopped
+          sleep
+        rescue ex
+          @log.error { ex.message }
+          @error = ex.message
+          sleep @reconnect_delay.seconds
+        end
+      ensure
+        @log.info { "stopping..." }
+        @source.stop
+        @destination.stop
+        @log.info { "stopped..." }
+        @state = State::Terminated
+      end
+
+      def details_tuple
+        {
+          name:  @name,
+          vhost: @vhost.name,
+          state: @state.to_s,
+          error: @error,
+          message_count: @message_count
+        }
+      end
+
+      # Does not trigger reconnect, but a graceful close
+      def terminate
+        return if terminated?
+        @source.stop
+        @destination.stop
+        @state = State::Terminated
+        @log.info { "Terminated" }
+      end
+
+      def delete
+        terminate
+        @vhost.delete_parameter("shovel", @name)
+      end
+
+      def terminated?
+        @state == State::Terminated
       end
     end
   end
