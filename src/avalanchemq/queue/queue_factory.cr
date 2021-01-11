c6a5900e7dd64180c9cca6d512fb017ca3c9754b
Magnus Landerblom <mange@84codes.com>
2021-01-11 13:29:58 +0100

    Validate x-max-priority argument (#169)
    Validate x-max-priority argument
    
    x-max-priority should only be allowed to be a int with
    values 0 <= x<= 255.
    It should fail with PRECONDITION_FAILED if the validation failed
    
    Co-authored-by: Carl Hörberg <carl@84codes.com>

--- src/avalanchemq/queue/queue_factory.cr
+++ src/avalanchemq/queue/queue_factory.cr
@@ -13,7 +13,7 @@ module AvalancheMQ
     end
 
     private def self.make_durable(vhost, frame)
-      if frame.arguments["x-max-priority"]?
+      if prio_queue? frame
         DurablePriorityQueue.new(vhost, frame.queue_name, frame.exclusive, frame.auto_delete, frame.arguments.to_h)
       else
         DurableQueue.new(vhost, frame.queue_name, frame.exclusive, frame.auto_delete, frame.arguments.to_h)
@@ -21,10 +21,20 @@ module AvalancheMQ
     end
 
     private def self.make_queue(vhost, frame)
-      if frame.arguments["x-max-priority"]?
+      if prio_queue? frame
         PriorityQueue.new(vhost, frame.queue_name, frame.exclusive, frame.auto_delete, frame.arguments.to_h)
       else
         Queue.new(vhost, frame.queue_name, frame.exclusive, frame.auto_delete, frame.arguments.to_h)
+      end
+    end
+
+    private def self.prio_queue?(frame)
+      if value = frame.arguments["x-max-priority"]?
+        p_value = value.as?(Int) || raise Error::PreconditionFailed.new("x-max-priority must be an int")
+        unless p_value >= 0 && p_value <= 255
+          raise Error::PreconditionFailed.new("x-max-priority must be between 0 and 255")
+        end
+        true
       end
     end
   end
