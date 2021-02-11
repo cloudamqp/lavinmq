8dbbec68c143a9134c2754670a1a7146805bc50d
Carl Hörberg <carl@84codes.com>
2021-02-11 10:40:11 +0100

    AMQP websocket support

---
+++ spec/websocket_spec.cr
@@ -0,0 +1,22 @@
+require "./spec_helper"
+
+describe "Websocket support" do
+  it "should connect over websocket" do
+    c = AMQP::Client.new("ws://localhost:#{HTTP_PORT}")
+    conn = c.connect
+    conn.should_not be_nil
+    conn.close
+  end
+
+  it "can publish large messages" do
+    c = AMQP::Client.new("ws://localhost:#{HTTP_PORT}")
+    conn = c.connect
+    ch = conn.channel
+    q = ch.queue
+    q.publish "b" * 150_000
+    msg = q.get(no_ack: true)
+    msg.should_not be_nil
+    msg.body_io.to_s.should eq "b" * 150_000 if msg
+    conn.close
+  end
+end
