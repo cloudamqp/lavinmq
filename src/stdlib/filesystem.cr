b603eb17ba2ed144c56caf90e500cb4fc32b6065
Carl Hörberg <carl@84codes.com>
2021-04-01 14:02:41 +0200

    freebsd filesystem info compability

--- src/stdlib/filesystem.cr
+++ src/stdlib/filesystem.cr
@@ -16,7 +16,9 @@ lib LibC
       flags : Int64
       spare : StaticArray(Long, 4)
     end
-  {% elsif flag?(:darwin) || flag?(:bsd) %}
+
+    fun statfs64(file : Char*, buf : Statfs*) : Int
+  {% elsif flag?(:darwin) %}
     struct Statfs
       bsize : UInt32
       iosize : Int32
@@ -35,11 +37,39 @@ lib LibC
       mntfromname : StaticArray(Int16, 1024)
       reserved : StaticArray(Long, 8)
     end
+
+    fun statfs64(file : Char*, buf : Statfs*) : Int
+  {% elsif flag?(:freebsd) %}
+    # https://www.freebsd.org/cgi/man.cgi?query=statfs&manpath=FreeBSD+12.2-RELEASE
+    struct Statfs
+      version : UInt32 # structure version number
+      type : UInt32 #	type of	filesystem
+      flags : UInt64 # copy of	mount exported flags
+      bsize : UInt64 # 	filesystem fragment size
+      iosize : UInt64 # 	optimal	transfer block size
+      blocks : UInt64 # 	total data blocks in filesystem
+      bfree : UInt64 # 	free blocks in filesystem
+      bavail : Int64  # 	free blocks avail to non-superuser
+      files : UInt64 # 	total file nodes in filesystem
+      ffree : Int64        # 	free nodes avail to non-superuser
+      syncwrites : UInt64  # 	count of sync writes since mount
+      asyncwrites : UInt64 # 	count of async writes since mount
+      syncreads : UInt64 # 	count of sync reads since mount
+      asyncreads : UInt64 # 	count of async reads since mount
+      spare : StaticArray(UInt64, 10) # [10]	unused spare
+      namemax : UInt32 # 	maximum	filename length
+      owner : UidT     # user that mounted the filesystem
+      fsid : StaticArray(Int32, 2)    # filesystem id
+      charspare : StaticArray(UInt8, 80) # /* spare string space
+      fstypename : StaticArray(UInt8, 16) # [MFSNAMELEN] /* filesystem type name
+      mntfromname : StaticArray(UInt8, 1024) # [MNAMELEN]	 /* mounted filesystem
+      mntonname : StaticArray(UInt8, 1024) # [MNAMELEN]	 /* directory on which mounted
+    end
+
+    fun statfs(file : Char*, buf : Statfs*) : Int
   {% else %}
     {% raise "No statfs implementation" %}
   {% end %}
-
-  fun statfs64(file : Char*, buf : Statfs*) : Int
 end
 
 struct FilesystemInfo
@@ -55,12 +85,24 @@ struct FilesystemInfo
   getter total : UInt64
 end
 
-module Filesystem
-  def self.info(path)
-    statfs = uninitialized LibC::Statfs
-    unless LibC.statfs64(path.check_no_null_byte, pointerof(statfs)).zero?
-      raise File::Error.from_errno("Error getting statfs", file: path)
+{% if flag?(:linux) || flag?(:darwin) %}
+  module Filesystem
+    def self.info(path)
+      statfs = uninitialized LibC::Statfs
+      unless LibC.statfs64(path.check_no_null_byte, pointerof(statfs)).zero?
+        raise File::Error.from_errno("Error getting statfs", file: path)
+      end
+      FilesystemInfo.new(statfs)
     end
-    FilesystemInfo.new(statfs)
   end
-end
+{% elsif flag?(:freebsd) %}
+  module Filesystem
+    def self.info(path)
+      statfs = uninitialized LibC::Statfs
+      unless LibC.statfs(path.check_no_null_byte, pointerof(statfs)).zero?
+        raise File::Error.from_errno("Error getting statfs", file: path)
+      end
+      FilesystemInfo.new(statfs)
+    end
+  end
+{% end %}
