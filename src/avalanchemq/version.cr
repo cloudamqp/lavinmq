287995384046ff60d4292a447cd7e4fad95b9f43
Johan Eckerström <johan.e@84codes.com>
2021-10-13 13:20:19 +0200

    New --build-info option to return build information
    When comparing results or debugging, it's very important to know the
    Crystal, LLVM version and any optimizations (`--release`)

--- src/avalanchemq/version.cr
+++ src/avalanchemq/version.cr
@@ -1,3 +1,22 @@
 module AvalancheMQ
   VERSION = {{ `git describe --tags 2>/dev/null || shards version`.chomp.stringify.gsub(/^v/, "") }}
+
+  macro build_flags
+    flags = [] of String
+    {% if flag?(:release) %}
+    flags << "--release"
+    {% end %}
+    {% if flag?(:debug) %}
+    flags << "--debug"
+    {% else %}
+    flags << "--no-debug"
+    {% end %}
+    flags
+  end
+
+  BUILD_INFO = <<-INFO
+    AvalancheMQ #{VERSION}
+    #{Crystal::DESCRIPTION.lines.reject(&.empty?).join("\n")}
+    Build flags: #{build_flags.join(" ")}
+    INFO
 end
