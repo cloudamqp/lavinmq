860dfa9db9e101c5dc358f1e325392e2c56cad5e
Carl Hörberg <carl@84codes.com>
2021-11-05 09:41:14 +0100

    more wip

--- src/avalanchemq/queue/ready.cr
+++ src/avalanchemq/queue/ready.cr
@@ -2,22 +2,61 @@ require "../segment_position"
 
 module AvalancheMQ
   class Queue
-    # ReadyQueue is a sorted Deque of SegmentPositions
+    # MessageIndex
+    # New messages are written to "enq.#{sp.segment}"
+    # Deleting msg from index appends to "ack.#{sp.segment}"
+    # When enq.X and ack.X are of equal size they can both be deleted
+    # ack.X files are not sorted but enq.X are (naturally)
+    # Requeued messages are put in a Dequeue (kept sorted)
+    # When shifting messages from the MessageIndex the Requeue is emptied first
+    # Then the enq.X files are read in order
+    # All ack.X entries are read at start and kept in a sorted array @acked
+    # When reading from enq.X make sure they aren't already in @acked
     class ReadyQueue
       @lock = Mutex.new(:reentrant)
       getter count = 0
 
       def initialize(queue_name)
-        @enq = MFile.new(File.join(@index_dir, "enq"), capacity: 1024 * 1024 )
-        @ack = MFile.new(File.join(@index_dir, "ack"), capacity: 1024 * 1024 )
+        @index_dir = File.join(@vhost.data_dir, Digest::SHA1.hexdigest @name)
+        @enq = Hash(UInt32, MFile).new do |h, seg|
+          h[seg] = MFile.new(File.join(@index_dir, "enq.#{seg}"), capacity: 8 * 1024 * 1024).tap do |file|
+              file.seek(0, IO::Seek::End)
+          end
+        end
+        @ack = Hash(UInt32, MFile).new do |h, seg|
+          h[seg] = MFile.new(File.join(@index_dir, "ack.#{seg}"), capacity: 8 * 1024 * 1024).tap do |file|
+              file.seek(0, IO::Seek::End)
+          end
+        end
+        load_enq_files_in_order
+        @acked = load_acked
         @requeued = Deque(SegmentPosition).new
+      end
+
+      private def load_enq_files_in_order : Nil
+        files = Dir.children(@index_dir).select! &.starts_with?("enq.")
+        segs = files.map(&.split('.', 2).last.to_i).sort!
+        segs.each { |seg| @enq[seg] }
+      end
+
+      private def load_acked : Array(SegmentPosition)
+        arr = Array(SegmentPosition).new
+        Dir.new(@index_dir).each do |name|
+          next unless name.starts_with?("ack.")
+          File.open(File.join(@index_dir, name)) do |f|
+            loop do
+              arr << f.read_bytes SegmentPosition
+            rescue IO::EOFError
+              break
+            end
+          end
+        end
+        arr.sort!
       end
 
       # Enqueue a new SegmentPosition, do not use for requeuing
       def enqueue(sp : SegmentPosition)
-        @lock.synchronize do
-          @enq.write_bytes sp
-        end
+        @enq[sp.segment].write_bytes sp
       end
 
       # insert a SP, keeps the deque sorted
@@ -48,17 +87,30 @@ module AvalancheMQ
         end
       end
 
-      def shift
-        shift? || raise IndexError.new
-      end
-
       def shift?
         @lock.synchronize do
-          sp = @requeued.shift?
-          sp ||= @enq.read_bytes(SegmentPosition) if @enq.pos + SP_SIZE <= @enq.size
+          sp = @requeued.shift? || next_enqued
           @count -= 1 if sp
           sp
         end
+      end
+
+      private def next_enqued : SegmentPosition?
+        @enq.each do |seg, mfile|
+          loop do
+            sp = mfile.read_bytes(SegmentPosition)
+            return sp unless sp == @acked.first?
+
+            @acked.shift
+          rescue IO::EOFError
+            @enq.delete(seg)
+            break
+          end
+        end
+      end
+
+      def shift
+        shift? || raise IndexError.new
       end
 
       # Shift until block breaks or it returns false
@@ -78,25 +130,18 @@ module AvalancheMQ
       # Deletes a SP somewhere in the deque
       # returns true/false whether found
       def ack(sp) : Bool
-        @ack.write_bytes sp
+        @ack[sp.segment].write_bytes sp
       end
 
       def limit_size(size, &blk : SegmentPosition -> Nil)
-        @lock.synchronize do
-          while @ready.size > size
-            sp = @ready.shift? || break
-            yield sp
-          end
+        until @count < size
+          sp = shift? || break
+          yield sp
         end
       end
 
       def limit_byte_size(bytesize, &blk : SegmentPosition -> Nil)
-        @lock.synchronize do
-          while @ready.sum(&.bytesize) > bytesize
-            sp = @ready.shift? || break
-            yield sp
-          end
-        end
+        raise NotImplemented.new
       end
 
       def empty?
