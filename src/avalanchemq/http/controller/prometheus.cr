227571b3c4b56b9284d998b65dc52c93d4b473e2
Carl Hörberg <carl.hoerberg@gmail.com>
2022-03-25 19:53:22 +0100

    telemetry_scrape_mem is measured in bytes
    docs said Kb

--- src/avalanchemq/http/controller/prometheus.cr
+++ src/avalanchemq/http/controller/prometheus.cr
@@ -105,7 +105,7 @@ module AvalancheMQ
                       help:  "Duration for metrics collection in seconds"})
         writer.write({name:  "scrape_mem",
                       value: mem,
-                      help:  "Memory used for metrics collections in Kb"})
+                      help:  "Memory used for metrics collections in bytes"})
       end
 
       private def overview_broker_metrics(writer)
