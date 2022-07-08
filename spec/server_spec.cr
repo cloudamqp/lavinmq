577ffe6fdc0a373ca8f3c79e42e302e05ba9dbb8
Weistrand <erica@84codes.com>
2022-07-08 16:36:03 +0200

    Measure time to collect metrics in stats_loop

--- spec/server_spec.cr
+++ spec/server_spec.cr
@@ -965,4 +965,11 @@ describe LavinMQ::Server do
   ensure
     LavinMQ::Config.instance.max_message_size = 128 * 1024**2
   end
+
+  it "should measure time it takes to collect metrics in stats_loop" do
+    server = LavinMQ::Server.new("/tmp/spec")
+    should_eventually(be_true, 10.seconds) {server.stats_loop_duration_seconds_total > Time::Span.zero}
+    server.system_metrics_duration_seconds.should_not eq Time::Span.zero
+    server.stats_rates_duration_seconds.should_not eq Time::Span.zero
+  end
 end
