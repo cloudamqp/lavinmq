6bb769885188484975613f996334cb0c3e8a770c
Weistrand <erica@84codes.com>
2022-08-16 16:18:39 +0200

    Extract collection of system metrics to own method

--- src/lavinmq/server.cr
+++ src/lavinmq/server.cr
@@ -246,6 +246,54 @@ module LavinMQ
       end
     end
 
+    def system_metrics(statm)
+      interval = Config.instance.stats_interval.milliseconds.to_i
+      log_size = Config.instance.stats_log_size
+      rusage = System.resource_usage
+
+      {% for m in METRICS %}
+        until @{{m.id}}_log.size < log_size
+          @{{m.id}}_log.shift
+        end
+        {% if m.id.ends_with? "_time" %}
+          {{m.id}} = rusage.{{m.id}}.total_milliseconds.to_i64
+          {{m.id}}_rate = (({{m.id}} - @{{m.id}}) / (interval * 1000)).round(2)
+        {% else %}
+          {{m.id}} = rusage.{{m.id}}.to_i64
+          {{m.id}}_rate = (({{m.id}} - @{{m.id}}) / interval).round(2)
+        {% end %}
+        @{{m.id}}_log.push {{m.id}}_rate
+        @{{m.id}} = {{m.id}}
+      {% end %}
+
+      until @rss_log.size < log_size
+        @rss_log.shift
+      end
+
+      rss = statm_rss(statm) || ps_rss
+      @rss = rss
+      @rss_log.push @rss
+
+      @mem_limit = cgroup_memory_max || System.physical_memory.to_i64
+
+      fs_stats = Filesystem.info(@data_dir)
+      until @disk_free_log.size < log_size
+        @disk_free_log.shift
+      end
+      disk_free = fs_stats.available.to_i64
+      @disk_free_log.push disk_free
+      @disk_free = disk_free
+
+      until @disk_total_log.size < log_size
+        @disk_total_log.shift
+      end
+      disk_total = fs_stats.total.to_i64
+      @disk_total_log.push disk_total
+      @disk_total = disk_total
+
+      control_flow!
+    end
+
     private def stats_loop
       # statm holds rss value in linux
       if File.exists?("/proc/self/statm")
@@ -258,53 +306,8 @@ module LavinMQ
           @stats_rates_collection_duration_seconds = Time.measure do
             update_stats_rates
           end
-
           @stats_system_collection_duration_seconds = Time.measure do
-            interval = Config.instance.stats_interval.milliseconds.to_i
-            log_size = Config.instance.stats_log_size
-            rusage = System.resource_usage
-
-            {% for m in METRICS %}
-              until @{{m.id}}_log.size < log_size
-                @{{m.id}}_log.shift
-              end
-              {% if m.id.ends_with? "_time" %}
-                {{m.id}} = rusage.{{m.id}}.total_milliseconds.to_i64
-                {{m.id}}_rate = (({{m.id}} - @{{m.id}}) / (interval * 1000)).round(2)
-              {% else %}
-                {{m.id}} = rusage.{{m.id}}.to_i64
-                {{m.id}}_rate = (({{m.id}} - @{{m.id}}) / interval).round(2)
-              {% end %}
-              @{{m.id}}_log.push {{m.id}}_rate
-              @{{m.id}} = {{m.id}}
-            {% end %}
-
-            until @rss_log.size < log_size
-              @rss_log.shift
-            end
-
-            rss = statm_rss(statm) || ps_rss
-            @rss = rss
-            @rss_log.push @rss
-
-            @mem_limit = cgroup_memory_max || System.physical_memory.to_i64
-
-            fs_stats = Filesystem.info(@data_dir)
-            until @disk_free_log.size < log_size
-              @disk_free_log.shift
-            end
-            disk_free = fs_stats.available.to_i64
-            @disk_free_log.push disk_free
-            @disk_free = disk_free
-
-            until @disk_total_log.size < log_size
-              @disk_total_log.shift
-            end
-            disk_total = fs_stats.total.to_i64
-            @disk_total_log.push disk_total
-            @disk_total = disk_total
-
-            control_flow!
+            system_metrics(statm)
           end
         end
       end
