e483dd519de56c875b5db0f3c2ceea251f8e0c30
Carl Hörberg <carl@84codes.com>
2022-08-19 09:55:57 +0200

    fixup! Convert all js scripts to js modules

---
+++ static/js/connection.js
@@ -0,0 +1,80 @@
+import * as HTTP from './http.js'
+import * as DOM from './dom.js'
+import * as Table from './table.js'
+import * as Chart from './chart.js'
+
+const urlEncodedConnection = new URLSearchParams(window.location.search).get('name')
+const connection = decodeURIComponent(urlEncodedConnection)
+const chart = Chart.render('chart', 'bytes/s')
+
+document.title = connection + ' | LavinMQ'
+document.querySelector('#connection').textContent = connection
+
+const connectionUrl = '/api/connections/' + urlEncodedConnection
+function updateConnection (all) {
+  HTTP.request('GET', connectionUrl).then(item => {
+    const stats = { send_details: item.send_oct_details, receive_details: item.recv_oct_details }
+    Chart.update(chart, stats)
+    const stateEl = document.getElementById('state')
+    if (item.state !== stateEl.textContent) {
+      stateEl.textContent = item.state
+    }
+    if (all) {
+      document.getElementById('conn-username').textContent = item.user
+      document.getElementById('connected_at').textContent = new Date(item.connected_at).toLocaleString()
+      document.getElementById('heartbeat').textContent = item.timeout + 's'
+      document.getElementById('authentication').textContent = item.auth_mechanism
+      document.getElementById('channel_max').textContent = item.channel_max
+      document.getElementById('frame_max').textContent = item.frame_max
+      document.getElementById('tls_version').textContent = item.tls_version
+      document.getElementById('cipher').textContent = item.cipher
+      const cp = item.client_properties
+      document.getElementById('cp-name').textContent = cp.connection_name
+      document.getElementById('cp-capabilities').textContent = DOM.jsonToText(cp.capabilities)
+      document.getElementById('cp-product').textContent = cp.product
+      document.getElementById('cp-platform').textContent = cp.platform
+      document.getElementById('cp-version').textContent = cp.version
+      const infoEl = document.getElementById('cp-information')
+      if (cp.information && cp.information.startsWith('http')) {
+        const infoLink = document.createElement('a')
+        infoLink.textContent = cp.information
+        infoLink.href = cp.information
+        DOM.setChild(infoEl, infoLink)
+      } else {
+        infoEl.textContent = cp.information || ''
+      }
+    }
+  }).catch(HTTP.standardErrorHandler).catch(e => clearInterval(cTimer))
+}
+updateConnection(true)
+const cTimer = setInterval(updateConnection, 5000)
+const channelsUrl = connectionUrl + '/channels'
+const tableOptions = { url: channelsUrl, keyColumns: ['name'], interval: 5000 }
+Table.renderTable('table', tableOptions, function (tr, item, all) {
+  if (all) {
+    const channelLink = document.createElement('a')
+    const urlEncodedChannel = encodeURIComponent(item.name)
+    channelLink.textContent = item.name
+    channelLink.href = '/channel?name=' + urlEncodedChannel
+    Table.renderCell(tr, 0, channelLink)
+    Table.renderCell(tr, 1, item.vhost)
+    Table.renderCell(tr, 2, item.username)
+  }
+  let mode = ''
+  mode += item.confirm ? ' C' : ''
+  Table.renderCell(tr, 3, mode, 'center')
+  Table.renderCell(tr, 4, item.consumer_count, 'right')
+  Table.renderCell(tr, 5, item.prefetch_count, 'right')
+  Table.renderCell(tr, 6, item.messages_unacknowledged, 'right')
+})
+
+document.querySelector('#closeConnection').addEventListener('submit', function (evt) {
+  evt.preventDefault()
+  const url = '/api/connections/' + urlEncodedConnection
+  const headers = new window.Headers({
+    'X-Reason': document.querySelector('[name=reason]').value
+  })
+  HTTP.request('DELETE', url, { headers })
+    .then(() => { window.location = '/connections' })
+    .catch(HTTP.standardErrorHandler)
+})
--- static/js/users.js
+++ static/js/users.js
@@ -1,29 +1,24 @@
-/* global lavinmq */
-(function () {
-  window.lavinmq = window.lavinmq || {}
+import * as HTTP from './http.js'
 
-  function fetch (cb) {
-    const url = '/api/users'
-    const raw = window.sessionStorage.getItem(url)
-    if (raw) {
-      var users = JSON.parse(raw)
-      cb(users)
-    }
-    lavinmq.http.request('GET', url).then(function (users) {
-      try {
-        window.sessionStorage.setItem('/api/users', JSON.stringify(users))
-      } catch (e) {
-        console.error('Saving sessionStorage', e)
-      }
-      cb(users)
-    }).catch(function (e) {
-      console.error(e.message)
-    })
+function fetch (cb) {
+  const url = '/api/users'
+  const raw = window.sessionStorage.getItem(url)
+  if (raw) {
+    var users = JSON.parse(raw)
+    cb(users)
   }
-
-  Object.assign(window.lavinmq, {
-    users: {
-      fetch
+  HTTP.request('GET', url).then(function (users) {
+    try {
+      window.sessionStorage.setItem('/api/users', JSON.stringify(users))
+    } catch (e) {
+      console.error('Saving sessionStorage', e)
     }
+    cb(users)
+  }).catch(function (e) {
+    console.error(e.message)
   })
-})()
+}
+
+export {
+  fetch
+}
