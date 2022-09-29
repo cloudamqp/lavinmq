b09bf8331b9b34b47a5deee9bd0adbd7d2997751
Johan Eckerström <johan@duh.se>
2022-09-29 09:50:20 +0200

    Ensure Http tag does not give management access

--- src/lavinmq/http/controller.cr
+++ src/lavinmq/http/controller.cr
@@ -198,7 +198,7 @@ module LavinMQ
       end
 
       private def refuse_unless_management(context, user, vhost = nil)
-        if user.tags.empty?
+        unless user.tags.any? { |t| t.management? || t.administrator? || t.policy_maker? }
           @log.warn { "user=#{user.name} does not have management access on vhost=#{vhost}" }
           access_refused(context)
         end
