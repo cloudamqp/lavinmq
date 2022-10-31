0a484a3092f2fdd7b547d0c733b12a6ddb6b366b
Carl Hörberg <carl@84codes.com>
2022-10-31 11:37:53 +0100

    Channel#try_send?

--- src/stdlib/channel.cr
+++ src/stdlib/channel.cr
@@ -1,7 +1,6 @@
 class Channel(T)
   # Sends a value to the channel if it has capacity.
-  # If the channel has spare capacity, then `true` is returned.
-  # Otherwise it returns `false` without blocking, no value will be deliviered.
+  # It doesn't block if the channel doesn't have capacity.
   #
   # Returns `true` if the value could be deliviered immediately, `false` otherwise.
   # Raises `ClosedError` if the channel is closed or closes while waiting on a full channel.
@@ -12,6 +11,24 @@ class Channel(T)
       true
     in .closed?
       raise ClosedError.new
+    in .none?
+      false
+    end
+  ensure
+    @lock.unlock
+  end
+
+  # Sends a value to the channel if it has capacity.
+  # It doesn't block if the channel doesn't have capacity.
+  #
+  # Returns `true` if the value could be deliviered immediately, `false` otherwise, also when the channel is closed.
+  def try_send?(value : T) : Bool
+    @lock.lock
+    case send_internal(value)
+    in .delivered?
+      true
+    in .closed?
+      false
     in .none?
       false
     end
