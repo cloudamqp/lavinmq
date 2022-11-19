3945ef586172777a748d1cdf1fb8ec6e552ff01c
Carl Hörberg <carl@84codes.com>
2022-11-20 00:45:29 +0100

    start lavinmq servers manually

--- spec/disk_full_spec.cr
+++ spec/disk_full_spec.cr
@@ -2,37 +2,36 @@ require "./spec_helper"
 
 describe "when disk is full" do
   before_each do
-    close_servers
     system "mkdir -p /tmp/tmpfs"
   end
 
   after_each do
-    close_servers
-    system "umount /tmp/tmpfs"
+    system "umount /tmp/tmpfs 2> /dev/null"
   end
 
   it "should raise error on start server" do
     system("mount -t tmpfs -o size=500k lavinmq-spec /tmp/tmpfs") || pending! "Root required for tmpfs"
     expect_raises(File::Error) do
-      TestHelpers.create_servers("/tmp/tmpfs")
+      LavinMQ::Server.new("/tmp/tmpfs")
     end
   end
 
   it "should raise error on publish" do
     system("mount -t tmpfs -o size=1m lavinmq-spec /tmp/tmpfs") || pending! "Root required for tmpfs"
-    TestHelpers.create_servers("/tmp/tmpfs")
+    s = LavinMQ::Server.new("/tmp/tmpfs")
+    spawn { s.listen("127.0.0.2", 5672) }
     message = "m"
-    with_channel do |ch|
+    with_channel(host: "127.0.0.2") do |ch|
       q = ch.queue("queue")
       expect_raises(AMQP::Client::Error) do
         q.publish message * 134_217_728
       end
     end
+    s.close
   end
 
   pending "should raise error on close server" do
     expect_raises(File::Error) do
-      close_servers
     end
   end
 end
