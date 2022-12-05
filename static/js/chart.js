e996187bfdd4e56429bc78ed501a6f9d05fad4b2
Christina <christina@84codes.com>
2022-12-05 12:03:24 +0100

    add Filler import

--- static/js/chart.js
+++ static/js/chart.js
@@ -1,5 +1,5 @@
 import * as helpers from './helpers.js'
-import { Chart, TimeScale, LinearScale, LineController, PointElement, LineElement, Legend, Tooltip, Title } from './lib/chart.js'
+import { Chart, TimeScale, LinearScale, LineController, PointElement, LineElement, Legend, Tooltip, Title, Filler } from './lib/chart.js'
 import './lib/chartjs-adapter-luxon.esm.js'
 Chart.register(TimeScale)
 Chart.register(LinearScale)
@@ -9,6 +9,7 @@ Chart.register(LineElement)
 Chart.register(Legend)
 Chart.register(Tooltip)
 Chart.register(Title)
+Chart.register(Filler)
 
 const chartColors = ['#003f5c', '#ffa600', '#665191', '#a05195', '#d45087', '#f95d6a', '#ff7c43', '#2f4b7c',
   '#EE6868', '#2F6497', '#6C8893']
