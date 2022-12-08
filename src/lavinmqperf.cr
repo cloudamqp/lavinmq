9414c285a4492e165457aa26c0f2a04a4ad1e1c1
Carl Hörberg <carl@84codes.com>
2022-12-08 12:10:50 +0100

    Remove unused (or merge into crystal) stdlib fixes

--- src/lavinmqperf.cr
+++ src/lavinmqperf.cr
@@ -1,5 +1,5 @@
 require "./lavinmq/version"
-require "./stdlib/*"
+require "./stdlib/resource"
 require "option_parser"
 require "amqp-client"
 require "benchmark"
--- src/stdlib/string_pool.cr
+++
@@ -1,8 +0,0 @@
-class StringPool
-  def clear
-    @capacity = 256
-    @hashes = Pointer(UInt64).malloc(@capacity, 0_u64)
-    @values = Pointer(String).malloc(@capacity, "")
-    @size = 0
-  end
-end
