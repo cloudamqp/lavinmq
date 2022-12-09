4d3b68cc854b6cb89e3fc029dcaf09df1c729009
Carl Hörberg <carl@84codes.com>
2022-12-10 00:00:55 +0100

    Remove Deque#to/from_json, as it's in Crystal stdlib now
    https://github.com/crystal-lang/crystal/pull/8850

--- src/stdlib/deque.cr
+++ src/stdlib/deque.cr
@@ -1,22 +1,4 @@
-require "json"
-
 class Deque(T)
-  def to_json(builder : JSON::Builder)
-    builder.array do
-      each do |v|
-        v.to_json(builder)
-      end
-    end
-  end
-
-  def self.from_json(json : JSON::PullParser) : Deque(T)
-    q = Deque(T).new
-    json.read_array do
-      q << T.from_json(json)
-    end
-    q
-  end
-
   def capacity
     @capacity
   end
