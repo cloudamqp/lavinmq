ff269a0786cc97658ecc24789aedc032f41e2e0e
Christina <christina@84codes.com>
2022-12-14 09:52:23 +0100

    make menu responsive to small browser windows

--- static/js/layout.js
+++ static/js/layout.js
@@ -3,12 +3,12 @@ import * as Vhosts from './vhosts.js'
 import * as Overview from './overview.js'
 
 document.getElementById('menu').innerHTML = `
-  <h1>
+  <h1 id="menu-header">
     <a href="/"><img id="amq-logo" src="/img/logo-lavinmq-white.png"></a>
     <small id="version"></small>
     <small id="cluster_name"></small>
   </h1>
-  <ul>
+  <ul id="menu-content">
     <li><a href="/">Overview</a></li>
     <li><a href="/connections">Connections</a></li>
     <li><a href="/channels">Channels</a></li>
@@ -24,12 +24,6 @@ document.getElementById('menu').innerHTML = `
     <li><a href="/docs/" target="_blank">HTTP API</a></li>
   </ul>
 `
-const path = window.location.pathname
-const active = document.querySelector('#menu a[href^="' + path.slice(0,-1) + '"]')
-if (active) {
-  const activeLi = active.parentElement
-  activeLi.classList.add('active')
-}
 
 document.getElementById('user-menu').innerHTML = `
    <ul>
@@ -52,3 +46,55 @@ document.getElementById('user-menu').innerHTML = `
 
 document.getElementById("userMenuVhost").onchange = (e) => Auth.selectVhost(e)
 document.getElementById("signoutLink").onclick = (e) => Auth.signOut(e)
+
+document.getElementById('small-menu').innerHTML = `
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <button id="menu-button" onclick=toggleSmallMenu()><i class="fa fa-lg fa-bars"></i></button>
+  <ul id="small-menu-content">
+        <li><a id="menu-item" href="/">Overview</a></li>
+        <li><a id="menu-item" href="/connections">Connections</a></li>
+        <li><a id="menu-item" href="/channels">Channels</a></li>
+        <li><a id="menu-item" href="/exchanges">Exchanges</a></li>
+        <li><a id="menu-item" href="/queues">Queues</a></li>
+        <li><a id="menu-item" href="/policies">Policies</a></li>
+        <li><a id="menu-item" href="/operator-policies">Operator policies</a></li>
+        <li><a id="menu-item" href="/shovels">Shovels</a></li>
+        <li><a id="menu-item" href="/federation">Federation</a></li>
+        <li><a id="menu-item" href="/vhosts">Virtual hosts</a></li>
+        <li><a id="menu-item" href="/users">Users</a></li>
+        <li><a id="menu-item" href="/nodes">Nodes</a></li>
+        <li><a id="menu-item" href="/docs/" target="_blank">HTTP API</a></li>
+  </ul>
+`
+const smallMenuContent = document.getElementById('small-menu-content')
+const menuButton = document.getElementById('menu-button')
+const smallMenu = document.getElementById('small-menu')
+
+menuButton.onclick = (e) => toggleSmallMenu()
+
+function toggleSmallMenu () {
+  if (smallMenu.classList.contains('show-menu')) {
+    smallMenu.classList.remove('show-menu')
+    smallMenuContent.classList.remove('show-menu')
+    menuButton.firstChild.classList.remove('fa-close')
+    menuButton.firstChild.classList.add('fa-bars')
+  } else {
+    smallMenu.classList.add('show-menu')
+    smallMenuContent.classList.add('show-menu')
+    menuButton.firstChild.classList.remove('fa-bars')
+    menuButton.firstChild.classList.add('fa-close')
+  }
+}
+
+const path = window.location.pathname
+var active = ''
+if (window.innerWidth > 1000) {
+  var active = document.querySelector('#menu a[href^="' + path.slice(0,-1) + '"]')
+} else {
+  var active = document.querySelector('#small-menu a[href^="' + path.slice(0,-1) + '"]')
+}
+
+if (active) {
+  const activeLi = active.parentElement
+  activeLi.classList.add('active')
+}
