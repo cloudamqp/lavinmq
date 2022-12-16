12e6a4ef0469f2fcc63f9682d96891da6dc2686d
Carl Hörberg <carl@84codes.com>
2022-12-16 22:38:30 +0100

    Channel spec fixed for MT

--- spec/stdlib/channel_spec.cr
+++ spec/stdlib/channel_spec.cr
@@ -3,21 +3,25 @@ require "../../src/stdlib/channel"
 
 describe Channel do
   it "can send non blocking" do
+    ready = Channel(Nil).new
     ch = Channel(Int32).new
     spawn do
+      ready.send nil
       ch.receive.should eq 1
     end
-    Fiber.yield # required to let the receiving fiber start
+    ready.receive
     ch.try_send(1).should be_true
     ch.try_send(2).should be_false
   end
 
   it "can send non blocking to buffered channel" do
+    ready = Channel(Nil).new
     ch = Channel(Int32).new(1)
     spawn do
+      ready.send nil
       ch.receive.should eq 1
     end
-    Fiber.yield # required to let the receiving fiber start
+    ready.receive
     ch.try_send(1).should be_true
     ch.try_send(2).should be_true
     ch.try_send(3).should be_false
