c3fa98f80d07b4a21ee67991a6c228e9129ed769
Jon Börjesson <jon@spuun.se>
2023-02-17 16:55:37 +0100

    Add support for view helpers

--- src/lavinmq/http/controller/find_templates.cr
+++ src/lavinmq/http/controller/find_templates.cr
@@ -1,8 +1,9 @@
 exit(0) if ARGV.size == 0
-path = ARGV[0]
+path = Path[ARGV[0]].expand
 file_extension = ARGV[1]?.to_s.strip
 file_extension = ".html" if file_extension.empty?
 p = Path[path].join("*#{file_extension}")
+puts path
 Dir[p].each do |file_path|
   puts file_path
 end
--- src/lavinmq/http/controller/templates.cr
+++ src/lavinmq/http/controller/templates.cr
@@ -2,25 +2,29 @@ require "ecr/macros"
 
 module LavinMQ::HTTP::Templates
   extend self
-  Log = Log.for("Templates")
+  Log = ::Log.for("LavinMQ.HTTP.Templates")
 
   abstract class Template
-    Log = Log.for("Template")
+    Log = Templates::Log.for("Template")
     getter filename
 
     def initialize(@filename : String, @registry : Registry)
     end
 
     macro layout(layout_name, vars = NamedTuple(), &block)
-      partial({{layout_name}}, vars) { {{block.body}} }
+      _partial(io, {{layout_name}}, vars) { {{block.body}} }
     end
 
     macro partial(partial_name, vars = NamedTuple(), &block)
-      _partial({{partial_name}}, io, {{vars}}) { {{block.body}} }
+      _partial(io, {{partial_name}},{{vars}}) { {{block.body}} }
     end
 
-    def _partial(partial_name, io, vars, &block)
-      @registry[partial_name].render(io, vars, &block)
+    def _partial(io, partial_name, vars, &block)
+      if partial = @registry[partial_name]?
+        @registry[partial_name].render(io, vars, &block)
+      else
+        Log.error { "missing partial: #{partial_name}" }
+      end
     end
 
     def render(io, vars = NamedTuple(), layout = "")
@@ -37,20 +41,45 @@ module LavinMQ::HTTP::Templates
       @templates[name]
     end
 
-    TEMPLATES = {} of String => Templates::Template.class
+    def []?(name : String) : Templates::Template?
+      @templates[name]?
+    end
+
+    TEMPLATES = [] of NamedTuple(disk_path: String, mapped_path: String, klass: Class)
+    HELPERS   = [] of Class
+
+    macro helpers(*helpers)
+      {% for h in helpers %}
+        {% HELPERS << h %}
+      {% end %}
+    end
 
     macro inherited
       {% verbatim do %}
         macro finished
+          # Define all template classes
+          {% for tpl in TEMPLATES %}
+            class {{tpl[:klass].id}} < Templates::Template
+              {% for h in HELPERS %}
+                include {{h.id}}
+              {% end %}
+              def render(io, vars, layout_name = "", &)
+                ECR.embed {{tpl[:disk_path]}}, io
+              end
+            end
+          {% end %}
+
+          # Define a constructor and populate templates. Also include any previously existing contructor.
           def initialize
-            {% for f, k in TEMPLATES %}
-              @templates[{{f}}] = {{@type.name}}::{{k.id}}.new({{f}}, self)
+            {% for tpl in TEMPLATES %}
+              @templates[{{tpl[:mapped_path]}}] = {{@type.name}}::{{tpl[:klass].id}}.new({{tpl[:mapped_path]}}, self)
             {% end %}
             {% if @type.has_method?(:initialize) %}
               previous_def
             {% end %}
           end
         end
+        {{debug}}
       {% end %}
     end
 
@@ -64,39 +93,23 @@ module LavinMQ::HTTP::Templates
         template_files = run("./find_templates", path, extension).split('\n').map(&.strip).reject(&.empty?)
         file_to_template = {} of String => String
         prefix = prefix.gsub(%r{^/|/$}, "")
-      %}
-      {% for template_file in template_files %}
-        {%
-          class_name = "Template#{template_file.gsub(/^[^a-z0-9]+/, "").gsub(/[^a-z0-9]+/, "_").camelcase.id}" # file_to_template[template_file] = class_name
-          # Remove "path" from the file path
-          mapped_path = template_file[path.size..]
+        # find_templates will do Path.expand for us, since we can't from macro
+        expanded_path = template_files[0]
+        template_files[1..].each do |template_file|
+          mapped_path = template_file[expanded_path.size..]
           # Remove initial slash, if any
           mapped_path = mapped_path[1..] if mapped_path.starts_with?("/")
-          file_to_template[mapped_path] = class_name
-        %}
-        class {{class_name.id}} < Templates::Template
-          def render(io, vars, layout_name = "", &)
-            ECR.embed {{template_file}}, io
-          end
-        end
-        add_template({{mapped_path}}, {{class_name}}, {{prefix}}, {{overwrite}})
-      {% end %}
-    end
+          mapped_path = if prefix.empty?
+                          "/#{mapped_path.id}"
+                        else
+                          "/#{mapped_prefix}/#{mapped_path.id}"
+                        end
 
-    macro add_template(path, class_name, prefix = "", overwrite = false)
-      {%
-        if prefix.empty?
-          file = "/#{path.id}"
-        else
-          file = "/#{prefix}/#{path.id}"
-        end
-      %}
-      {%
-        if TEMPLATES[file] && !overwrite
-          Log.warn { "Template {{file.d}} already added" }
-        else
-          Log.debug { "Registering template {{file.id}}" }
-          TEMPLATES[file] = class_name
+          # e.g. /index.htmlk
+          class_name = mapped_path.gsub(/^[^a-z0-9]+/, "").gsub(/[^a-z0-9]+/, "_").camelcase
+          class_name = "Template#{class_name.id}"
+          file_to_template[mapped_path] = class_name
+          TEMPLATES << {disk_path: template_file, mapped_path: mapped_path, klass: class_name}
         end
       %}
     end
