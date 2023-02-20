40faf87613d5ef130300a04817c6b58dc02e5936
Carl Hörberg <carl@84codes.com>
2023-02-20 17:26:15 +0100

    Revert body size to UInt64
    To make migration less complicated
    
    This reverts commit 0e0df48e5375ce5cc6b3d475232e9821cba4a4d2.

--- src/lavinmq/message.cr
+++ src/lavinmq/message.cr
@@ -3,16 +3,16 @@ module LavinMQ
   struct BytesMessage
     getter timestamp, exchange_name, routing_key, properties, bodysize, body
 
-    MIN_BYTESIZE = 8 + 1 + 1 + 2 + 4
+    MIN_BYTESIZE = 8 + 1 + 1 + 2 + 8
 
     def initialize(@timestamp : Int64, @exchange_name : String,
                    @routing_key : String, @properties : AMQP::Properties,
-                   @bodysize : UInt32, @body : Bytes)
+                   @bodysize : UInt64, @body : Bytes)
     end
 
     def bytesize
       sizeof(Int64) + 1 + @exchange_name.bytesize + 1 + @routing_key.bytesize +
-        @properties.bytesize + sizeof(UInt32) + @bodysize
+        @properties.bytesize + sizeof(UInt64) + @bodysize
     end
 
     def persistent?
@@ -37,26 +37,44 @@ module LavinMQ
       nil
     end
 
-    def self.skip(io, format = IO::ByteFormat::SystemEndian) : UInt32
-      skipped = 0_u32
-      skipped += io.skip(sizeof(Int64))                              # ts
+    def self.skip(io, format = IO::ByteFormat::SystemEndian) : UInt64
+      skipped = 0_u64
+      skipped += io.skip(sizeof(UInt64))                             # ts
       skipped += io.skip(io.read_byte || raise IO::EOFError.new) + 1 # ex
       skipped += io.skip(io.read_byte || raise IO::EOFError.new) + 1 # rk
       skipped += AMQP::Properties.skip(io, format)
-      skipped += io.skip(UInt32.from_io io, format) + sizeof(UInt32)
+      skipped += io.skip(UInt64.from_io io, format) + sizeof(UInt64)
       skipped
     end
 
     def self.from_bytes(bytes, format = IO::ByteFormat::SystemEndian) : self
       ts = format.decode(Int64, bytes[0, 8]); pos = 8
-      ex_len = bytes[pos]; pos += 1
-      ex = String.new(bytes.to_unsafe + pos, ex_len); pos += ex_len
-      rk_len = bytes[pos]; pos += 1
-      rk = String.new(bytes.to_unsafe + pos, rk_len); pos += rk_len
+      ex = AMQP::ShortString.from_bytes(bytes + pos); pos += 1 + ex.bytesize
+      rk = AMQP::ShortString.from_bytes(bytes + pos); pos += 1 + rk.bytesize
       pr = AMQP::Properties.from_bytes(bytes + pos, format); pos += pr.bytesize
-      sz = format.decode(UInt32, bytes[pos, 4]); pos += 4
+      sz = format.decode(UInt64, bytes[pos, 8]); pos += 8
       body = bytes[pos, sz]
       BytesMessage.new(ts, ex, rk, pr, sz, body)
+    end
+
+    def self.from_io(io, format = IO::ByteFormat::SystemEndian) : self
+      ts = Int64.from_io io, format
+      ex = AMQP::ShortString.from_io io, format
+      rk = AMQP::ShortString.from_io io, format
+      pr = AMQP::Properties.from_io io, format
+      sz = UInt64.from_io io, format
+      body = io.to_slice(io.pos, sz)
+      io.seek(sz, IO::Seek::Current)
+      BytesMessage.new(ts, ex, rk, pr, sz, body)
+    end
+
+    def to_io(io : IO, format = IO::ByteFormat::SystemEndian)
+      io.write_bytes @timestamp, format
+      io.write_bytes AMQP::ShortString.new(@exchange_name), format
+      io.write_bytes AMQP::ShortString.new(@routing_key), format
+      io.write_bytes @properties, format
+      io.write_bytes @bodysize, format
+      io.write @body
     end
   end
 
@@ -67,19 +85,19 @@ module LavinMQ
 
     def initialize(@timestamp : Int64, @exchange_name : String,
                    @routing_key : String, @properties : AMQP::Properties,
-                   @bodysize : UInt32, @body_io : IO)
+                   @bodysize : UInt64, @body_io : IO)
     end
 
     def initialize(@exchange_name : String, @routing_key : String,
                    body : String, @properties = AMQP::Properties.new)
       @timestamp = RoughTime.unix_ms
-      @bodysize = body.bytesize.to_u32
+      @bodysize = body.bytesize.to_u64
       @body_io = IO::Memory.new(body)
     end
 
     def bytesize
       sizeof(Int64) + 1 + @exchange_name.bytesize + 1 + @routing_key.bytesize +
-        @properties.bytesize + sizeof(UInt32) + @bodysize
+        @properties.bytesize + sizeof(UInt64) + @bodysize
     end
 
     def persistent?
