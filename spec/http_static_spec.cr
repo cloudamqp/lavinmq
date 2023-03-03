c3a2596c798a992fe65c65f888d8e3745a02116a
Jon Börjesson <jon@spuun.se>
2023-03-03 15:25:39 +0100

    Dont use helper, we dont want auth headers

--- spec/http_static_spec.cr
+++ spec/http_static_spec.cr
@@ -2,41 +2,41 @@ require "./spec_helper"
 
 describe LavinMQ::HTTP::StaticController do
   it "GET /" do
-    response = get "/"
+    response = HTTP::Client.get "#{Helper.base_url}/"
     response.status_code.should eq 200
     response.headers["Content-Type"].should contain("text/html")
     response.body.should contain("LavinMQ")
   end
 
   it "GET /robots.txt" do
-    response = get "/robots.txt"
+    response = HTTP::Client.get "#{Helper.base_url}/robots.txt"
     response.status_code.should eq 200
     response.headers["Content-Type"].should contain("text/plain")
     response.body.should contain("Disallow")
   end
 
   it "GET /img/favicon.png" do
-    response = get "/img/favicon.png"
+    response = HTTP::Client.get "#{Helper.base_url}/img/favicon.png"
     response.status_code.should eq 200
     response.headers["Content-Type"].should contain("image/png")
   end
 
   it "GET /test/ serves index.html in the directory" do
-    response = get "/test/"
+    response = HTTP::Client.get "#{Helper.base_url}/test/"
     response.status_code.should eq 200
     response.headers["Content-Type"].should contain("text/html")
     response.body.should eq("This is a test\n")
   end
 
   it "GET /test (without trailing /) serves index.html in the directory" do
-    response = get "/test"
+    response = HTTP::Client.get "#{Helper.base_url}/test"
     response.status_code.should eq 200
     response.headers["Content-Type"].should contain("text/html")
     response.body.should eq("This is a test\n")
   end
 
   it "GET /test/index.html" do
-    response = get "/test/index.html"
+    response = HTTP::Client.get "#{Helper.base_url}/test/index.html"
     response.status_code.should eq 200
     response.headers["Content-Type"].should contain("text/html")
     response.body.should eq("This is a test\n")
