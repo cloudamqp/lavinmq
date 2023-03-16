a7ee17466860d8c69b34dd3a3552a166e19c2af0
Jon Börjesson <jon@spuun.se>
2023-03-16 12:29:45 +0100

    Item doesnt always have the value dictionary

--- static/js/form.js
+++ static/js/form.js
@@ -5,7 +5,8 @@ function editItem(form, item, valueFactories)  {
   const pkfield = form.querySelector('[data-primary-key]')
   if (pkfield) { pkfield.setAttribute("readonly", true) }
   form.querySelectorAll('input, select, textarea').forEach(input => {
-    let value = item[input.name] || item.value[input.name]
+    let value = item[input.name]
+    if (!value && item.value) { value = item.value[input.name] }
     if (valueFactories[input.name]) { value = valueFactories[input.name](item) }
     if (input instanceof HTMLSelectElement) {
       input.selectedIndex = Array.from(input.options).findIndex(i => i.value == value)
