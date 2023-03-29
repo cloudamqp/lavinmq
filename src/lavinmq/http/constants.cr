b4b71eb861552af402cebdf0fad188b4091b8eb5
Carl Hörberg <carl@84codes.com>
2023-03-29 19:50:26 +0200

    new path to internal http unix socket
    /tmp/lavinmqctl.sock and virtual @/tmp/lavinmqctl.sock on linux

--- src/lavinmq/http/constants.cr
+++ src/lavinmq/http/constants.cr
@@ -1,9 +1,9 @@
 module LavinMQ
   module HTTP
     {% if flag?(:linux) %}
-      INTERNAL_UNIX_SOCKET = "/dev/shm/lavinmq-http.sock"
+      INTERNAL_UNIX_SOCKET = "@/tmp/lavinmqctl.sock"
     {% else %}
-      INTERNAL_UNIX_SOCKET = "/tmp/lavinmq-http.sock"
+      INTERNAL_UNIX_SOCKET = "/tmp/lavinmqctl.sock"
     {% end %}
   end
 end
