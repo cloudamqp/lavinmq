82b83ac6a0bdced4f5e62adb104561abf506e3b0
Carl Hörberg <carl@84codes.com>
2023-03-29 23:35:17 +0200

    Use the same unix socket path for lavinmqctl on all platforms
    Abstract unix socket paths doesn't have permission checks.

--- src/lavinmq/http/constants.cr
+++ src/lavinmq/http/constants.cr
@@ -1,9 +1,5 @@
 module LavinMQ
   module HTTP
-    {% if flag?(:linux) %}
-      INTERNAL_UNIX_SOCKET = "@/tmp/lavinmqctl.sock"
-    {% else %}
-      INTERNAL_UNIX_SOCKET = "/tmp/lavinmqctl.sock"
-    {% end %}
+    INTERNAL_UNIX_SOCKET = "/tmp/lavinmqctl.sock"
   end
 end
