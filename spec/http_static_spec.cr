85e3e90f2704eddaed7f40fe16f701f6c9d0be2c
Carl Hörberg <carl@84codes.com>
2023-04-05 22:47:33 +0200

    rm /test/index.html, not serving html anymore

--- spec/http_static_spec.cr
+++ spec/http_static_spec.cr
@@ -13,25 +13,4 @@ describe LavinMQ::HTTP::StaticController do
     response.status_code.should eq 200
     response.headers["Content-Type"].should contain("image/png")
   end
-
-  it "GET /test/ serves index.html in the directory" do
-    response = ::HTTP::Client.get "#{BASE_URL}/test/"
-    response.status_code.should eq 200
-    response.headers["Content-Type"].should contain("text/html")
-    response.body.should eq("This is a test\n")
-  end
-
-  it "GET /test (without trailing /) serves index.html in the directory" do
-    response = ::HTTP::Client.get "#{BASE_URL}/test"
-    response.status_code.should eq 200
-    response.headers["Content-Type"].should contain("text/html")
-    response.body.should eq("This is a test\n")
-  end
-
-  it "GET /test/index.html" do
-    response = ::HTTP::Client.get "#{BASE_URL}/test/index.html"
-    response.status_code.should eq 200
-    response.headers["Content-Type"].should contain("text/html")
-    response.body.should eq("This is a test\n")
-  end
 end
