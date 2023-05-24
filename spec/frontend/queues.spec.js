9fb1d1fdd955b68c4a2a7877a4a31b287cca744b
Jon Börjesson <jon@spuun.se>
2023-05-24 13:37:28 +0200

    Add pagination specs

--- spec/frontend/queues.spec.js
+++ spec/frontend/queues.spec.js
@@ -1,3 +1,4 @@
+import * as qHelpers from './queues_helpers.js'
 import { test, expect } from '@playwright/test';
 
 test.describe("queues", _ => {
@@ -8,14 +9,15 @@ test.describe("queues", _ => {
     await page.route(/\/api\/vhosts/, route => route.fulfill({ json: vhostResponse }))
   })
 
-  function waitForPathRequest(page, path) {
+  function waitForPathRequest(page, path, response_data = {}) {
     return new Promise((resolve, reject) => {
       page.route('**/*', (route, request) => {
         const requestedUrl = new URL(request.url())
         if (requestedUrl.pathname !== path) {
           return route.fallback()
         }
-        route.fulfill({ json: {} })
+        page.unroute('**/*')
+        route.fulfill({ json: response_data })
         resolve(request)
       })
     })
@@ -58,4 +60,26 @@ test.describe("queues", _ => {
       await expect(apiQueuesRequest).toHaveQueryParams(q)
     })
   })
+
+  //
+  test.describe('pagination', _ => {
+    const queues = Array.from(Array(10), (_, i) => qHelpers.queue(`queue-${i}`))
+    const queues_response = qHelpers.response(queues, { total_count: 100, page: 1, page_count: 10, page_size: 10 })
+
+    test('is visible for page_count=10', async ({ page }) => {
+      const apiQueuesRequest = waitForPathRequest(page, '/api/queues', queues_response)
+      await page.goto('/queues')
+      await apiQueuesRequest
+      await expect(page.locator('#table .pagination .page-item')).toContainText(['Previous', 'Next'], { timeout: 10 })
+    })
+
+    test('updates url when Next is clicked', async ({page }) => {
+      const apiQueuesRequest = waitForPathRequest(page, '/api/queues', queues_response)
+      await page.goto('/queues')
+      await apiQueuesRequest
+      await page.locator('#table .pagination .page-item').getByText('Next').click()
+      await expect(page).toHaveURL(/page=2/)
+    })
+  })
+
 })
