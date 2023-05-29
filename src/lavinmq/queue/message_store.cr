e845069ebd232b0f270058b32292e5a8fde3e9ce
Viktor Erlingsson <viktor.erlingsson@gmail.com>
2023-05-29 15:48:42 +0200

    dont truncate mfiles on startup (#518)
    Makes startup after a crash much quicker with many queues.
    
    ---------
    
    Co-authored-by: Carl Hörberg <carl@84codes.com>

--- src/lavinmq/queue/message_store.cr
+++ src/lavinmq/queue/message_store.cr
@@ -296,7 +296,7 @@ module LavinMQ
             pos = mfile.pos
             raise IO::EOFError.new if pos + BytesMessage::MIN_BYTESIZE >= mfile.size # EOF or a message can't fit, truncate
             ts = IO::ByteFormat::SystemEndian.decode(Int64, mfile.to_slice + pos)
-            raise IO::EOFError.new if ts.zero? # This means that the rest of the file is zero, so truncate it
+            break mfile.resize(pos) if ts.zero? # This means that the rest of the file is zero, so resize it
 
             bytesize = BytesMessage.skip(mfile)
             count += 1
