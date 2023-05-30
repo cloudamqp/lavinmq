eb9db98b394bcc6099f33382b27d5050f2bb5acf
Jon Börjesson <jon@spuun.se>
2023-05-30 09:27:58 +0200

    Ensure multiple waitForPathRequest can be used

--- spec/frontend/helpers.js
+++ spec/frontend/helpers.js
@@ -1,15 +1,16 @@
 function waitForPathRequest(page, path, response_data = {}) {
   const matchUrl = new URL(path, 'http://example.com')
   return new Promise((resolve, reject) => {
-    page.route('**/*', (route, request) => {
+    const handler = (route, request) => {
       const requestedUrl = new URL(request.url())
       if (requestedUrl.pathname !== matchUrl.pathname) {
         return route.fallback()
       }
-      page.unroute('**/*')
+      page.unroute('**/*', handler)
       route.fulfill({ json: response_data })
       resolve(request)
-    })
+    }
+    page.route('**/*', handler)
   })
   return page.waitForRequest(req => {
     const reqUrl = new URL(req.url())
