2866b2c5a8e2c6b18e9edf24751b7a7a0bfc3619
Carl Hörberg <carl@84codes.com>
2023-07-07 01:09:40 +0200

    replace @log with Log in Consumer

--- src/lavinmq/client/channel/consumer.cr
+++ src/lavinmq/client/channel/consumer.cr
@@ -7,20 +7,19 @@ module LavinMQ
     class Channel
       class Consumer
         include SortableJSON
+        Log = ::Log.for("consumer")
 
         getter channel, tag, queue, no_ack, exclusive, priority
         getter prefetch_count = 0u16
         getter unacked = 0_u32
         getter? closed = false
-        @log : Log
         @flow : Bool
 
         def initialize(@channel : Client::Channel, @tag : String,
                        @queue : Queue, @no_ack : Bool, @exclusive : Bool, @priority : Int32)
           @prefetch_count = @channel.prefetch_count
           @flow = @channel.flow?
-          @log = @channel.log.for "consumer=#{@tag}"
-          spawn deliver_loop, name: "Consumer deliver loop", same_thread: true
+          spawn deliver_loop, name: "Consumer#deliver_loop", same_thread: true
         end
 
         def close
@@ -45,6 +44,7 @@ module LavinMQ
         end
 
         private def deliver_loop
+          Log.context.set(vhost: @channel.client.vhost.name, client: @channel.client.remote_address.to_s, channel: @channel.id.to_s, consumer_tag: @tag)
           wait_for_single_active_consumer
           queue = @queue
           no_ack = @no_ack
@@ -61,7 +61,7 @@ module LavinMQ
               break
             end
             {% unless flag?(:release) %}
-              @log.debug { "Getting a new message" }
+              Log.debug { "Getting a new message" }
             {% end %}
             queue.consume_get(no_ack) do |env|
               deliver(env.message, env.segment_position, env.redelivered)
@@ -69,13 +69,13 @@ module LavinMQ
             Fiber.yield if (i &+= 1) % 32768 == 0
           end
         rescue ex : ClosedError | Queue::ClosedError | Client::Channel::ClosedError | ::Channel::ClosedError
-          @log.debug { "deliver loop exiting: #{ex.inspect}" }
+          Log.debug { "deliver loop exiting: #{ex.inspect}" }
         end
 
         private def wait_for_global_capacity
           ch = @channel
           return if ch.has_capacity?
-          @log.debug { "Waiting for global prefetch capacity" }
+          Log.debug { "Waiting for global prefetch capacity" }
           select
           when ch.has_capacity.receive
           when @notify_closed.receive
@@ -86,18 +86,18 @@ module LavinMQ
         private def wait_for_single_active_consumer
           case @queue.single_active_consumer
           when self
-            @log.debug { "This consumer is the single active consumer" }
+            Log.debug { "This consumer is the single active consumer" }
           when nil
-            @log.debug { "The queue isn't a single active consumer queue" }
+            Log.debug { "The queue isn't a single active consumer queue" }
           else
-            @log.debug { "Waiting for this consumer to become the single active consumer" }
+            Log.debug { "Waiting for this consumer to become the single active consumer" }
             loop do
               select
               when sca = @queue.single_active_consumer_change.receive
                 if sca == self
                   break
                 else
-                  @log.debug { "New single active consumer, but not me" }
+                  Log.debug { "New single active consumer, but not me" }
                 end
               when @notify_closed.receive
                 break
@@ -111,7 +111,7 @@ module LavinMQ
           # single active consumer queues can't have priority consumers
           if @queue.has_priority_consumers? && @queue.single_active_consumer.nil?
             if @queue.consumers.any? { |c| c.priority > @priority && c.accepts? }
-              @log.debug { "Waiting for higher priority consumers to not have capacity" }
+              Log.debug { "Waiting for higher priority consumers to not have capacity" }
               begin
                 ::Channel.receive_first(@queue.consumers.map(&.has_capacity))
               rescue ::Channel::ClosedError
@@ -123,10 +123,10 @@ module LavinMQ
 
         private def wait_for_queue_ready
           if @queue.empty?
-            @log.debug { "Waiting for queue not to be empty" }
+            Log.debug { "Waiting for queue not to be empty" }
             select
             when is_empty = @queue.empty_change.receive
-              @log.debug { "Queue is #{is_empty ? "" : "not"} empty" }
+              Log.debug { "Queue is #{is_empty ? "" : "not"} empty" }
             when @notify_closed.receive
             end
             return true
@@ -135,10 +135,10 @@ module LavinMQ
 
         private def wait_for_paused_queue
           if @queue.paused?
-            @log.debug { "Waiting for queue not to be paused" }
+            Log.debug { "Waiting for queue not to be paused" }
             select
             when is_paused = @queue.paused_change.receive
-              @log.debug { "Queue is #{is_paused ? "" : "not"} paused" }
+              Log.debug { "Queue is #{is_paused ? "" : "not"} paused" }
             when @notify_closed.receive
             end
             return true
@@ -147,9 +147,9 @@ module LavinMQ
 
         private def wait_for_flow
           unless @flow
-            @log.debug { "Waiting for flow" }
+            Log.debug { "Waiting for flow" }
             is_flow = @flow_change.receive
-            @log.debug { "Channel flow=#{is_flow}" }
+            Log.debug { "Channel flow=#{is_flow}" }
             return true
           end
         end
@@ -162,7 +162,7 @@ module LavinMQ
         private def wait_for_capacity : Nil
           if @prefetch_count > 0
             until @unacked < @prefetch_count
-              @log.debug { "Waiting for prefetch capacity" }
+              Log.debug { "Waiting for prefetch capacity" }
               @has_capacity.receive
             end
           end
@@ -188,9 +188,9 @@ module LavinMQ
             notify_hash_capacity(false) if @unacked == @prefetch_count
           end
           persistent = msg.properties.delivery_mode == 2_u8
-          # @log.debug { "Getting delivery tag" }
+          # Log.debug { "Getting delivery tag" }
           delivery_tag = @channel.next_delivery_tag(@queue, sp, persistent, @no_ack, self)
-          # @log.debug { "Sending BasicDeliver" }
+          # Log.debug { "Sending BasicDeliver" }
           deliver = AMQP::Frame::Basic::Deliver.new(@channel.id, @tag,
             delivery_tag,
             redelivered,
