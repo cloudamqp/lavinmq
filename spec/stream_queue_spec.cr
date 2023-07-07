0dfe3a0ef99c3bf07fbb10f2f6b5fe311ad40ffd
Carl Hörberg <carl@84codes.com>
2023-07-07 23:03:48 +0200

    formatting of stream queue specs

--- spec/stream_queue_spec.cr
+++ spec/stream_queue_spec.cr
@@ -10,7 +10,7 @@ describe LavinMQ::StreamQueue do
       q_name = "pub_and_consume"
       header = LavinMQ::AMQP::Table.new({"x-stream-offset" => 2})
       with_channel do |ch|
-        q = ch.queue(q_name, args:tbl)
+        q = ch.queue(q_name, args: tbl)
         10.times do |i|
           q.publish "m#{i}"
         end
@@ -28,14 +28,14 @@ describe LavinMQ::StreamQueue do
       q_name = "pub_and_consume_2"
       count = 0
       with_channel do |ch2|
-        q = ch2.queue(q_name, args:tbl)
+        q = ch2.queue(q_name, args: tbl)
         10.times do |i|
           q.publish "m#{i}"
         end
 
         ch2.prefetch 1
         subscriber_args = AMQP::Client::Arguments.new({"x-stream-offset" => 0})
-        
+
         q.subscribe(args: subscriber_args) do |msg|
           count += 1
           msg.ack
@@ -48,7 +48,7 @@ describe LavinMQ::StreamQueue do
       q_name = "pub_and_consume_3"
       count = 0
       with_channel do |ch|
-        q = ch.queue(q_name, args:tbl)
+        q = ch.queue(q_name, args: tbl)
         10.times do |i|
           q.publish "m#{i}"
         end
@@ -67,9 +67,8 @@ describe LavinMQ::StreamQueue do
   end
 
   describe "Expiration" do
-    #it "messages should expire with time" do
-    #end
-
+    # it "messages should expire with time" do
+    # end
 
     it "messages should be removed if max-length set" do
       opts_max_length = {"x-queue-type" => "stream", "x-max-length" => 10, "durable" => true}
@@ -77,7 +76,7 @@ describe LavinMQ::StreamQueue do
       q_name = "pub_and_consume_4"
       count = 0
       with_channel do |ch|
-        q = ch.queue(q_name, args:tbl_max_length)
+        q = ch.queue(q_name, args: tbl_max_length)
         ch.prefetch 1
         50.times do |i|
           q.publish "m#{i}"
@@ -86,7 +85,7 @@ describe LavinMQ::StreamQueue do
 
       last_header = LavinMQ::AMQP::Table.new({"x-stream-offset" => 0})
       with_channel do |ch|
-        q = ch.queue(q_name, args:tbl_max_length)
+        q = ch.queue(q_name, args: tbl_max_length)
         ch.prefetch 1
         subscriber_args = AMQP::Client::Arguments.new({"x-stream-offset" => 0})
         q.subscribe(args: subscriber_args) do |msg|
