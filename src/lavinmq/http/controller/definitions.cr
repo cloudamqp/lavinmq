4c1e008e691753a4882b73284cbe55b2df6fd0c6
Christina <christina@84codes.com>
2023-07-12 09:36:47 +0200

    handle whitespace

--- src/lavinmq/http/controller/definitions.cr
+++ src/lavinmq/http/controller/definitions.cr
@@ -205,7 +205,8 @@ module LavinMQ
               name = u["name"].as_s
               pass_hash = u["password_hash"].as_s
               hash_algo = u["hashing_algorithm"]?.try(&.as_s)
-              tags = u["tags"]?.to_s.tr("[]\"", "").split(",").compact_map { |t| Tag.parse?(t) }
+              tags = u["tags"]?.to_s.gsub(/[\[\]"\s]/, "").split(",").compact_map { |t| Tag.parse?(t) }
+              pp tags
               @amqp_server.users.add(name, pass_hash, hash_algo, tags, save: false)
             end
             @amqp_server.users.save!
