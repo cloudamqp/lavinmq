11802cec7e2b14417dcf8d80bdfa37c446e01189
Jon Börjesson <jon@spuun.se>
2023-10-24 08:23:16 +0200

    Add spec to verify new timestamp on dead lettering
    When a message is dead lettered the timestamp should be updated since
    it's a new publish happening.

--- spec/dlx_spec.cr
+++ spec/dlx_spec.cr
@@ -1,5 +1,6 @@
 require "./spec_helper"
 require "./../src/lavinmq/queue"
+require "./../src/lavinmq/rough_time"
 
 describe "Dead lettering" do
   q_name = "ttl"
@@ -28,5 +29,33 @@ describe "Dead lettering" do
       x_death[0].as(AMQ::Protocol::Table)["queue"].should eq q_delayed.name
       x_death[1].as(AMQ::Protocol::Table)["queue"].should eq q_delayed_2.name
     end
+  end
+
+  it "should update timestamp" do
+    v = Server.vhosts.create("test")
+    q_args = AMQ::Protocol::Table.new({
+      "x-message-ttl"             => 200,
+      "x-dead-letter-exchange"    => "",
+      "x-dead-letter-routing-key" => "q2",
+    })
+    v.declare_queue("q", true, false, q_args)
+    v.declare_queue("q2", true, false, AMQ::Protocol::Table.new)
+
+    ts = RoughTime.unix_ms
+    msg = LavinMQ::Message.new(ts, "", "q", AMQ::Protocol::Properties.new, 0, IO::Memory.new)
+
+    v.publish msg
+
+    select
+    when v.queues["q2"].empty_change.receive
+    when timeout(1.second)
+      fail "timeout: message not dead lettered?"
+    end
+
+    v.queues["q2"].basic_get(no_ack: true) do |env|
+      msg = env.message
+    end
+
+    msg.timestamp.should be > ts
   end
 end
