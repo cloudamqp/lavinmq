ad7d12833448412e23a4c991695ea3ca83f2b34c
Christina <christina@84codes.com>
2023-12-11 12:14:07 +0100

    Return early if variable is unchanged

--- src/lavinmq/user_store.cr
+++ src/lavinmq/user_store.cr
@@ -45,9 +45,11 @@ module LavinMQ
 
     def add_permission(user, vhost, config, read, write)
       perm = {config: config, read: read, write: write}
+      if @users[user].permissions[vhost]? && @users[user].permissions[vhost] == perm
+        return perm
+      end
       @users[user].permissions[vhost] = perm
       @users[user].invalidate_acl_caches
-      Log.info { "Updated permissions for user=#{user} on vhost=#{vhost}" }
       save!
       perm
     end
