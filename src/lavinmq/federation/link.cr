b8611c2dbc102788794f214d5cb6f38794439682
Viktor Erlingsson <viktor@84codes.com>
2024-02-28 12:00:24 +0100

    use if empty? instead of unless any?

--- src/lavinmq/federation/link.cr
+++ src/lavinmq/federation/link.cr
@@ -234,7 +234,7 @@ module LavinMQ
                 })
                 headers["x-received-from"] = received_from
                 msg.properties.headers = headers
-                unless @federated_q.@consumers.any?
+                if @federated_q.@consumers.empty?
                   pch.basic_reject(msg.delivery_tag, true)
                   pch.close("No consumer on downstream queue")
                   @consumer_available.receive?
