40fe93c496d9f7d8a72cc9a3a71265590f3d5c92
Jon Börjesson <jon@spuun.se>
2024-05-10 09:47:34 +0200

    More descriptive variable name

--- src/lavinmq/observable.cr
+++ src/lavinmq/observable.cr
@@ -5,19 +5,19 @@ module LavinMQ
 
   module Observable(EventT)
     macro included
-      {% ivar = ("@__" + EventT.name.stringify.downcase.gsub(/[^a-z_]+/, "_") + "_observers").id %}
-      {{ivar}} = Set(LavinMQ::Observer({{EventT}})).new
+      {% observers_ivar = ("@__" + EventT.name.stringify.downcase.gsub(/[^a-z_]+/, "_") + "_observers").id %}
+      {{observers_ivar}} = Set(LavinMQ::Observer({{EventT}})).new
 
       def register_observer(observer : LavinMQ::Observer({{EventT}}))
-        {{ivar}}.add(observer)
+        {{observers_ivar}}.add(observer)
       end
 
       def unregister_observer(observer : LavinMQ::Observer({{EventT}}))
-        {{ivar}}.delete(observer)
+        {{observers_ivar}}.delete(observer)
       end
 
       def notify_observers(event : {{EventT}}, data : Object? = nil)
-        {{ivar}}.each &.on(event, data)
+        {{observers_ivar}}.each &.on(event, data)
       end
     end
   end
