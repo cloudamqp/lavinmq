ceaaffd1f8dc7b27032fbeee8be7f8aa8bb8a15b
Carl Hörberg <carl@84codes.com>
2024-05-16 11:36:28 +0200

    Support WebSocket connections over UNIX domain socket (#680)

--- src/lavinmq/http/handler/websocket.cr
+++ src/lavinmq/http/handler/websocket.cr
@@ -5,8 +5,10 @@ module LavinMQ
     def self.new(amqp_server : Server)
       ::HTTP::WebSocketHandler.new do |ws, ctx|
         req = ctx.request
-        local_address = req.local_address.as(Socket::IPAddress)
-        remote_address = req.remote_address.as(Socket::IPAddress)
+        local_address = req.local_address.as?(Socket::IPAddress) ||
+                        Socket::IPAddress.new("127.0.0.1", 0) # Fake when UNIXAddress
+        remote_address = req.remote_address.as?(Socket::IPAddress) ||
+                         Socket::IPAddress.new("127.0.0.1", 0) # Fake when UNIXAddress
         connection_info = ConnectionInfo.new(remote_address, local_address)
         io = WebSocketIO.new(ws)
         spawn amqp_server.handle_connection(io, connection_info), name: "HandleWSconnection #{remote_address}"
