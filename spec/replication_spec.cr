40e7b197356c8e10bbf946a212b62af551b2b35b
Viktor Erlingsson <viktor@84codes.com>
2024-05-24 14:24:14 +0200

    Dont run append if @followers is empty (#684)
    Dont run append if followers is empty

--- spec/replication_spec.cr
+++ spec/replication_spec.cr
@@ -41,6 +41,7 @@ describe LavinMQ::Replication::Client do
       repli.follow("127.0.0.1", LavinMQ::Config.instance.replication_port)
       done.send nil
     end
+    wait_for { Server.followers.size == 1 }
     with_channel do |ch|
       q = ch.queue("repli")
       q.publish_confirm "hello world"
