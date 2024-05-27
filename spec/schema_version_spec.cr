dd6154ab9ebe9bd739f7c5fd3f3b4664986a26ed
Viktor Erlingsson <viktor@84codes.com>
2024-05-27 13:37:39 +0200

    spec

--- spec/schema_version_spec.cr
+++ spec/schema_version_spec.cr
@@ -19,5 +19,27 @@ describe LavinMQ::SchemaVersion do
       file.write_bytes LavinMQ::Schema::VERSION
       LavinMQ::SchemaVersion.verify(file, :message).should eq LavinMQ::SchemaVersion::VERSIONS[:message]
     end
+
+    it "Deletes empty file and creates a new when it is the first file" do
+      data_dir = "/tmp/lavinmq-spec"
+      path = File.join(data_dir, "msgs.0000000001")
+      file = MFile.new(path, LavinMQ::Config.instance.segment_size)
+      file.resize(LavinMQ::Config.instance.segment_size)
+      # init new message store
+      msg_store = LavinMQ::Queue::MessageStore.new(data_dir, nil)
+      msg_store.@segments.first[1].size.should eq 4
+    end
+
+    it "Deletes empty file when it is second file" do
+      v = Server.vhosts["/"].not_nil!
+      v.declare_queue("q", true, false)
+      data_dir = Server.vhosts["/"].queues["q"].@msg_store.@data_dir
+      path = File.join(data_dir, "msgs.0000000002")
+      file = MFile.new(path, LavinMQ::Config.instance.segment_size)
+      file.resize(LavinMQ::Config.instance.segment_size)
+      # init new message store
+      msg_store = LavinMQ::Queue::MessageStore.new(data_dir, nil)
+      msg_store.@segments.size.should eq 1
+    end
   end
 end
