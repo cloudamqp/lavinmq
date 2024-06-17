69cd07a7ee9497a4718c4de1af795693fe0a1928
Viktor Erlingsson <viktor@84codes.com>
2024-06-17 14:34:13 +0200

    remove unused code

--- src/lavinmq/mfile.cr
+++ src/lavinmq/mfile.cr
@@ -180,14 +180,6 @@ class MFile < IO
     @size = new_size
   end
 
-  def write_at(pos : Int64, slice : Bytes) : Nil
-    raise ClosedError.new if @closed
-    end_pos = pos + slice.size
-    raise IO::EOFError.new if end_pos > @capacity
-    @size = end_pos if end_pos > @size
-    slice.copy_to(buffer + pos, slice.size)
-  end
-
   def read(slice : Bytes)
     pos = @pos
     new_pos = pos + slice.size
