f292d1b399f35a102af9eb7ad99e5e77db88ef57
Jon Börjesson <jon@spuun.se>
2024-06-19 15:24:37 +0200

    Rewrite to use regex instead
    - Less code
    - More perfomant
    
    ```
     regex   3.06M (327.06ns) (± 0.40%)  704B/op        fastest
     parser   1.16M (862.69ns) (± 0.41%)  770B/op   2.64× slower
    ```

--- spec/http_router_spec.cr
+++ spec/http_router_spec.cr
@@ -24,112 +24,52 @@ describe LavinMQ::HTTP::Router do
   end
 
   describe "#call" do
+    it "supports single param" do
+      router = TestRouter.new
+      routed = false
+      router.get "/:foo" do |c, params|
+        params.should eq Hash(String, String){"foo" => "bar"}
+        routed = true
+        c
+      end
+      router.call(create_request("GET", "/bar"))
+      routed.should be_true
+    end
+
+    it "supports multiple params" do
+      router = TestRouter.new
+      routed = false
+      router.get "/:foo/none/:bar" do |c, params|
+        params.should eq Hash(String, String){"foo" => "bar", "bar" => "foo"}
+        routed = true
+        c
+      end
+      router.call(create_request("GET", "/bar/none/foo"))
+      routed.should be_true
+    end
+
+    it "supports rest param" do
+      router = TestRouter.new
+      routed = false
+      router.get "/a/*foo" do |c, params|
+        params.should eq Hash(String, String){"foo" => "bar/baz"}
+        routed = true
+        c
+      end
+      router.call(create_request("GET", "/a/bar/baz"))
+      routed.should be_true
+    end
+
     it "should route on method and path" do
       router = TestRouter.new
       routed = false
-      router.get "a/:foo" do |context, params|
+      router.get "/a/:foo" do |context, params|
         params.should eq Hash(String, String){"foo" => "bar"}
         routed = true
         context
       end
-      router.call(create_request("GET", "a/bar"))
+      router.call(create_request("GET", "/a/bar"))
       routed.should be_true
-    end
-  end
-end
-
-describe LavinMQ::HTTP::Router::StaticFragment do
-  describe "#match?" do
-    it "should be able to be empty string" do
-      fragment = LavinMQ::HTTP::Router::StaticFragment.new("")
-      params = Hash(String, String).new
-      fragment.match?("", params).should be_true
-    end
-    it "should match on equal values" do
-      fragment = LavinMQ::HTTP::Router::StaticFragment.new("foo")
-      params = Hash(String, String).new
-      fragment.match?("foo", params).should be_true
-    end
-
-    it "should not match on different values" do
-      fragment = LavinMQ::HTTP::Router::StaticFragment.new("foo")
-      params = Hash(String, String).new
-      fragment.match?("bar", params).should be_false
-    end
-  end
-end
-
-describe LavinMQ::HTTP::Router::ParamFragment do
-  describe "#match?" do
-    it "should match and add value to params" do
-      fragment = LavinMQ::HTTP::Router::ParamFragment.new("foo")
-      params = Hash(String, String).new
-      fragment.match?("bar", params).should be_true
-      params.should eq Hash(String, String){"foo" => "bar"}
-    end
-  end
-end
-
-describe LavinMQ::HTTP::Router::RestFragment do
-  describe "#match?" do
-    it "should match and add value to params" do
-      fragment = LavinMQ::HTTP::Router::RestFragment.new("foo")
-      params = Hash(String, String).new
-      fragment.match?("bar/baz/buz", params).should be_true
-      params.should eq Hash(String, String){"foo" => "bar/baz/buz"}
-    end
-  end
-end
-
-describe LavinMQ::HTTP::Router::Fragments do
-  describe ".create" do
-    it "parses path and creates correct fragments" do
-      fragments = LavinMQ::HTTP::Router::Fragments.create("a/:b/c/*d")
-      expected = [
-        LavinMQ::HTTP::Router::StaticFragment.new("a"),
-        LavinMQ::HTTP::Router::ParamFragment.new("b"),
-        LavinMQ::HTTP::Router::StaticFragment.new("c"),
-        LavinMQ::HTTP::Router::RestFragment.new("d"),
-      ]
-      fragments.@fragments.should eq expected
-    end
-  end
-
-  describe "#match?" do
-    it "should handle path '/'" do
-      fragments = LavinMQ::HTTP::Router::Fragments.create("/")
-      params = LavinMQ::HTTP::Router::Params.new
-      result = fragments.match?("/", params)
-      result.should be_true
-    end
-    it "should match if all fragment matches" do
-      fragments = LavinMQ::HTTP::Router::Fragments.create("a/:b/c")
-      params = LavinMQ::HTTP::Router::Params.new
-      result = fragments.match?("a/b/c", params)
-      result.should be_true
-      params.should eq Hash(String, String){"b" => "b"}
-    end
-
-    it "shouldn't match if one fragment doesn't match" do
-      fragments = LavinMQ::HTTP::Router::Fragments.create("a/:b/c")
-      params = LavinMQ::HTTP::Router::Params.new
-      result = fragments.match?("a/b/d", params)
-      result.should be_false
-    end
-
-    it "shouldn't match with too few fragments" do
-      fragments = LavinMQ::HTTP::Router::Fragments.create("a/:b/c")
-      params = LavinMQ::HTTP::Router::Params.new
-      result = fragments.match?("a/b", params)
-      result.should be_false
-    end
-
-    it "should collect all params" do
-      fragments = LavinMQ::HTTP::Router::Fragments.create("a/:b/c/:d/*rest")
-      params = LavinMQ::HTTP::Router::Params.new
-      result = fragments.match?("/a/b/c/d/foo/bar", params)
-      params.should eq LavinMQ::HTTP::Router::Params{"b" => "b", "d" => "d", "rest" => "foo/bar"}
-      result.should be_true
     end
   end
 end
--- src/lavinmq/http/router.cr
+++ src/lavinmq/http/router.cr
@@ -5,115 +5,42 @@ require "string_scanner"
 module LavinMQ::HTTP::Router
   include ::HTTP::Handler
 
-  alias Params = Hash(String, String)
   alias Routes = Array(Route)
   alias Action = Proc(::HTTP::Server::Context, Params, ::HTTP::Server::Context)
-
-  abstract struct Fragment
-    def initialize(@value : String)
-    end
-
-    def match?(path : String, params : Params) : Bool
-      match?(StringScanner.new(path), params)
-    end
-
-    abstract def match?(path : StringScanner, params : Params) : Bool
-  end
-
-  struct StaticFragment < Fragment
-    FRAGMENT_PATTERN = %r{[^/]*}
-
-    def match?(path : StringScanner, params : Params) : Bool
-      path_fragment = path.scan FRAGMENT_PATTERN
-      path_fragment == @value
-    end
-  end
-
-  struct ParamFragment < Fragment
-    FRAGMENT_PATTERN = %r{[^/]*}
-
-    def match?(path : StringScanner, params : Params) : Bool
-      if path_fragment = path.scan FRAGMENT_PATTERN
-        params[@value] = path_fragment
-        return true
-      end
-      false
-    end
-  end
-
-  struct RestFragment < Fragment
-    def match?(path : StringScanner, params : Params) : Bool
-      params[@value] = path.rest
-      path.terminate
-      true
-    end
-  end
-
-  struct Fragments
-    FRAGMENT_SEPERATOR = %r{/}
-
-    def self.create(path) : Fragments
-      path = path.lstrip '/'
-      new(path.split('/').map do |value|
-        case value[0]?
-        when ':'
-          ParamFragment.new(value[1..])
-        when '*'
-          RestFragment.new(value[1..])
-        else
-          StaticFragment.new(value)
-        end
-      end)
-    end
-
-    def initialize(@fragments : Array(Fragment))
-    end
-
-    def match?(path : String, params : Params) : Bool
-      match?(StringScanner.new(path), params)
-    end
-
-    def match?(path : StringScanner, params : Params) : Bool
-      fragments = @fragments.each
-      until path.eos?
-        fragment = fragments.next
-        case fragment
-        when Iterator::Stop then return false
-        else
-          path.skip FRAGMENT_SEPERATOR
-          return false unless fragment.match?(path, params)
-        end
-      end
-      # It's a match if we've matched against all fragments
-      fragments.next == fragments.stop
-    end
-  end
+  alias Params = Hash(String, String)
 
   struct Route
-    getter method, fragments, action
+    getter method, action, pattern
 
     def initialize(@method : String, @path : String, @action : Action)
-      @fragments = Fragments.create(@path)
+      path = @path.split("/").map do |part|
+        case part[0]?
+        when ':' then "(?<#{Regex.escape(part[1..])}>[^/]*)"
+        when '*' then "(?<#{Regex.escape(part[1..])}>.*)"
+        else          Regex.escape(part)
+        end
+      end.join("/")
+      @pattern = Regex.new("^/#{method}#{path}$")
     end
   end
 
   @_routes = Routes.new
 
-  {% for method in %w[delete get options patch post put head] %}
+  {% for method in %w[delete get head options patch post put] %}
     def {{method.id}}(path : String, &block : Action)
       @_routes << Route.new({{method.upcase}}, path, block)
     end
   {% end %}
 
-  private def find_route(method, path)
-    params = Params.new
-    path = StringScanner.new(path)
-    @_routes.each do |route|
-      next unless route.method == method
-      params.clear
-      path.reset
-      if route.fragments.match?(path, params)
-        return {params: params, action: route.action}
+  def find_route(method, path)
+    search_path = "/#{method}#{path}"
+    @_routes.each do |r|
+      if res = r.pattern.match(search_path)
+        return {
+          action: r.action,
+          # reject and transform_value to go from Hash(String, String | Nil) to Hash(String, String)
+          params: res.named_captures.reject! { |_k, v| v.nil? }.transform_values &.to_s,
+        }
       end
     end
     nil
