6f2ee5423ac2f46c86949bc7bc14acbcc97ffbbf
Christina <christina@84codes.com>
2024-06-20 16:18:06 +0200

    clean up any trailing '/' in path before routing

--- src/lavinmq/http/router.cr
+++ src/lavinmq/http/router.cr
@@ -33,7 +33,7 @@ module LavinMQ::HTTP::Router
   {% end %}
 
   def find_route(method, path)
-    search_path = "/#{method}#{path}"
+    search_path = "/#{method}#{path.sub(/\/$/, "")}"
     @_routes.each do |r|
       if res = r.pattern.match(search_path)
         return {
