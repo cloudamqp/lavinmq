75fece82267f39a9440ac71f0bf19e25ffb17f1a
Viktor Erlingsson <viktor@84codes.com>
2024-06-22 23:42:05 +0200

    use UrlDataSource

--- static/js/unacked.js
+++ static/js/unacked.js
@@ -1,5 +1,5 @@
 import * as Table from './table.js'
-import * as Helpers from './helpers.js'
+import { UrlDataSource } from './datasource.js'
 
 const search = new URLSearchParams(window.location.hash.substring(1))
 const queue = search.get('name')
@@ -11,7 +11,7 @@ document.querySelector('#pagename-label').textContent = queue + ' in virtual hos
 
 
 const tableOptions = {
-  url,
+  dataSource: new UrlDataSource(url, { useQueryState: false }),
   keyColumns: ['message_tag', 'consumer_tag'],
   interval: 5000,
   pagination: true,
@@ -22,7 +22,7 @@ const tableOptions = {
 
 Table.renderTable('table', tableOptions, function (tr, item, firstRender) {
   if (!firstRender) return
-      Table.renderCell(tr, 0, item.message_tag)
-      Table.renderCell(tr, 1, item.consumer_tag)
-      Table.renderCell(tr, 2, item.delivered_at)
-    })
+    Table.renderCell(tr, 0, item.message_tag)
+    Table.renderCell(tr, 1, item.consumer_tag)
+    Table.renderCell(tr, 2, item.delivered_at)
+})
