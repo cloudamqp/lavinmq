adcad2db036b9d71e41ffa897f1f33fea93cc947
Viktor Erlingsson <viktor@84codes.com>
2024-06-22 23:46:07 +0200

    lint

--- static/js/unacked.js
+++ static/js/unacked.js
@@ -4,11 +4,10 @@ import { UrlDataSource } from './datasource.js'
 const search = new URLSearchParams(window.location.hash.substring(1))
 const queue = search.get('name')
 const vhost = search.get('vhost')
-let url = `api/queues/${encodeURIComponent(vhost)}/${encodeURIComponent(queue)}/unacked`
+const url = `api/queues/${encodeURIComponent(vhost)}/${encodeURIComponent(queue)}/unacked`
 document.getElementById('queue-link').href = `/queue#vhost=${encodeURIComponent(vhost)}&name=${encodeURIComponent(queue)}`
 document.getElementById('queue-link').innerHTML = queue
 document.querySelector('#pagename-label').textContent = queue + ' in virtual host ' + vhost
-
 
 const tableOptions = {
   dataSource: new UrlDataSource(url, { useQueryState: false }),
@@ -22,7 +21,7 @@ const tableOptions = {
 
 Table.renderTable('table', tableOptions, function (tr, item, firstRender) {
   if (!firstRender) return
-    Table.renderCell(tr, 0, item.message_tag)
-    Table.renderCell(tr, 1, item.consumer_tag)
-    Table.renderCell(tr, 2, item.delivered_at)
+  Table.renderCell(tr, 0, item.message_tag)
+  Table.renderCell(tr, 1, item.consumer_tag)
+  Table.renderCell(tr, 2, item.delivered_at)
 })
