f43e5ad649211735ee7cbae26be05bf625438ef6
Viktor Erlingsson <viktor@84codes.com>
2024-06-23 10:46:03 +0200

    struct instead of class, add channel_name

--- src/lavinmq/unacked_message.cr
+++ src/lavinmq/unacked_message.cr
@@ -1,14 +1,16 @@
 module LavinMQ
-  class UnackedMessage
+  struct UnackedMessage
     include SortableJSON
     getter message_tag : UInt64
     getter consumer_tag : String
     getter delivered_at : String
+    getter channel_name : String
 
-    def initialize(message_tag, consumer_tag, delivered_at)
+    def initialize(message_tag, consumer_tag, delivered_at, channel_name)
       @message_tag = message_tag
       @consumer_tag = consumer_tag
       @delivered_at = delivered_at
+      @channel_name = channel_name
     end
 
     def details_tuple
@@ -16,6 +18,7 @@ module LavinMQ
         message_tag:  @message_tag,
         consumer_tag: @consumer_tag,
         delivered_at: @delivered_at,
+        channel_name: @channel_name
       }
     end
   end
