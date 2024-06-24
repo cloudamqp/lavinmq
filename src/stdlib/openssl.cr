b634a6be7a6856b92573d431263790d552bf7a24
Carl Hörberg <carl@84codes.com>
2024-06-24 11:16:07 +0200

    rm stdlib fix to openssl that has been merged
    Merge into crystal 1.10.0
    https://github.com/crystal-lang/crystal/pull/13695

--- src/stdlib/openssl.cr
+++ src/stdlib/openssl.cr
@@ -12,16 +12,3 @@ class OpenSSL::SSL::Context::Client
     {% end %}
   end
 end
-
-# PR: https://github.com/crystal-lang/crystal/pull/13695
-class OpenSSL::SSL::Context::Server
-  def initialize(method : LibSSL::SSLMethod = Context.default_method)
-    super(method)
-
-    {% if LibSSL.has_method?(:x509_verify_param_lookup) %}
-      self.default_verify_param = "ssl_client"
-    {% end %}
-    set_tmp_ecdh_key(curve: LibCrypto::NID_X9_62_prime256v1)
-    self.ciphers = CIPHERS_INTERMEDIATE
-  end
-end
