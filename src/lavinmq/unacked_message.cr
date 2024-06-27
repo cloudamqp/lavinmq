08bd823d9246ffcd46854d396a984cc36278c8da
Viktor Erlingsson <viktor@84codes.com>
2024-06-27 12:04:07 +0200

    save delivered_at instead

--- src/lavinmq/unacked_message.cr
+++ src/lavinmq/unacked_message.cr
@@ -1,16 +1,16 @@
 module LavinMQ
   struct UnackedMessage
     include SortableJSON
-    getter message_tag, consumer_tag, unacked_for_seconds, channel_name
+    getter message_tag, consumer_tag, channel_name, delivered_at
 
-    def initialize(@message_tag : UInt64, @consumer_tag : String, @unacked_for_seconds : Int64, @channel_name : String)
+    def initialize(@message_tag : UInt64, @consumer_tag : String, @channel_name : String, @delivered_at : Time::Span)
     end
 
     def details_tuple
       {
         message_tag:         @message_tag,
         consumer_tag:        @consumer_tag,
-        unacked_for_seconds: @unacked_for_seconds,
+        unacked_for_seconds: (RoughTime.monotonic - delivered_at).to_i,
         channel_name:        @channel_name,
       }
     end
