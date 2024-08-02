2ea3008a4aa88d86688c9f9cfd5e43b14d9a0f52
Carl Hörberg <carl@84codes.com>
2024-08-02 22:44:30 +0200

    monkeypatch Hash with an upcoming PR that fixes a memory leak

--- src/stdlib/hash.cr
+++ src/stdlib/hash.cr
@@ -2,4 +2,10 @@ class Hash(K, V)
   def capacity
     entries_capacity + indices_size * @indices_bytesize
   end
+
+  # PR: https://github.com/crystal-lang/crystal/pull/14862
+  private def delete_entry(index) : Nil
+    # sets `Entry#@hash` to 0 and removes stale references to key and value
+    (@entries + index).clear
+  end
 end
