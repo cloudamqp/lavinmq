eadfb6a5d747db9526e75be050bcfcda8c2285d0
Christina <christina@84codes.com>
2024-09-06 13:18:26 +0200

    amqp publish

--- src/lavinmq/amqp/channel.cr
+++ src/lavinmq/amqp/channel.cr
@@ -247,6 +247,7 @@ module LavinMQ
         end
 
         confirm do
+          #here
           ok = @client.vhost.publish msg, @next_publish_immediate, @visited, @found_queues
           basic_return(msg, @next_publish_mandatory, @next_publish_immediate) unless ok
         rescue e : LavinMQ::Error::PreconditionFailed
