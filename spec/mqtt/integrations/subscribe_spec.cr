6b896442d0e589dd8b8b62a77da65604af84359e
Magnus Landerblom <magnus@landerblom.se>
2024-09-10 13:58:18 +0200

    all integration specs (pending)

---
+++ spec/mqtt/integrations/subscribe_spec.cr
@@ -0,0 +1,104 @@
+require "../spec_helper"
+
+module MqttSpecs
+  extend MqttHelpers
+  extend MqttMatchers
+  describe "subscribe" do
+    pending "bits 3,2,1,0 must be set to 0,0,1,0 [MQTT-3.8.1-1]" do
+      with_server do |server|
+        with_client_io(server) do |io|
+          connect(io)
+
+          temp_io = IO::Memory.new
+          topic_filters = mk_topic_filters({"a/b", 0})
+          subscribe(MQTT::Protocol::IO.new(temp_io), topic_filters: topic_filters, expect_response: false)
+          temp_io.rewind
+          subscribe_pkt = temp_io.to_slice
+          # This will overwrite the protocol level byte
+          subscribe_pkt[0] |= 0b0000_1010u8
+          io.write_bytes_raw subscribe_pkt
+
+          # Verify that connection is closed
+          io.should be_closed
+        end
+      end
+    end
+
+    pending "must contain at least one topic filter [MQTT-3.8.3-3]" do
+      with_server do |server|
+        with_client_io(server) do |io|
+          connect(io)
+
+          topic_filters = mk_topic_filters({"a/b", 0})
+          temp_io = IO::Memory.new
+          subscribe(MQTT::Protocol::IO.new(temp_io), topic_filters: topic_filters, expect_response: false)
+          temp_io.rewind
+          sub_pkt = temp_io.to_slice
+          sub_pkt[1] = 2u8 # Override remaning length
+          io.write_bytes_raw sub_pkt
+
+          # Verify that connection is closed
+          io.should be_closed
+        end
+      end
+    end
+
+    pending "should not allow any payload reserved bits to be set [MQTT-3-8.3-4]" do
+      with_server do |server|
+        with_client_io(server) do |io|
+          connect(io)
+
+          topic_filters = mk_topic_filters({"a/b", 0})
+          temp_io = IO::Memory.new
+          subscribe(MQTT::Protocol::IO.new(temp_io), topic_filters: topic_filters, expect_response: false)
+          temp_io.rewind
+          sub_pkt = temp_io.to_slice
+          sub_pkt[sub_pkt.size - 1] |= 0b1010_0100u8
+          io.write_bytes_raw sub_pkt
+
+          # Verify that connection is closed
+          io.should be_closed
+        end
+      end
+    end
+
+    pending "should replace old subscription with new [MQTT-3.8.4-3]" do
+      with_server do |server|
+        with_client_io(server) do |io|
+          connect(io)
+
+          topic_filters = mk_topic_filters({"a/b", 0})
+          suback = subscribe(io, topic_filters: topic_filters)
+          suback.should be_a(MQTT::Protocol::SubAck)
+          suback = suback.as(MQTT::Protocol::SubAck)
+          # Verify that we subscribed as qos0
+          suback.return_codes.first.should eq(MQTT::Protocol::SubAck::ReturnCode::QoS0)
+
+          # Publish something to the topic we're subscribed to...
+          publish(io, topic: "a/b", payload: "a".to_slice, qos: 1u8)
+          # ... consume it...
+          pub = read_packet(io).as(MQTT::Protocol::Publish)
+          # ... and verify it be qos0 (i.e. our subscribe is correct)
+          pub.qos.should eq(0u8)
+
+          # Now do a second subscribe with another qos and do the same verification
+          topic_filters = mk_topic_filters({"a/b", 1})
+          suback = subscribe(io, topic_filters: topic_filters)
+          suback.should be_a(MQTT::Protocol::SubAck)
+          suback = suback.as(MQTT::Protocol::SubAck)
+          # Verify that we subscribed as qos1
+          suback.return_codes.should eq([MQTT::Protocol::SubAck::ReturnCode::QoS1])
+
+          # Publish something to the topic we're subscribed to...
+          publish(io, topic: "a/b", payload: "a".to_slice, qos: 1u8)
+          # ... consume it...
+          pub = read_packet(io).as(MQTT::Protocol::Publish)
+          # ... and verify it be qos0 (i.e. our subscribe is correct)
+          pub.qos.should eq(1u8)
+
+          io.should be_drained
+        end
+      end
+    end
+  end
+end
---
+++ spec/mqtt/integrations/will_spec.cr
@@ -0,0 +1,166 @@
+require "../spec_helper"
+
+module MqttSpecs
+  extend MqttHelpers
+  extend MqttMatchers
+
+  describe "client will" do
+    pending "will is not delivered on graceful disconnect [MQTT-3.14.4-3]" do
+      with_server do |server|
+        with_client_io(server) do |io|
+          connect(io)
+          topic_filters = mk_topic_filters({"#", 0})
+          subscribe(io, topic_filters: topic_filters)
+
+          with_client_io(server) do |io2|
+            will = MQTT::Protocol::Will.new(
+              topic: "will/t", payload: "dead".to_slice, qos: 0u8, retain: false)
+            connect(io2, client_id: "will_client", will: will, keepalive: 1u16)
+            disconnect(io2)
+          end
+
+          # If the will has been published it should be received before this
+          publish(io, topic: "a/b", payload: "alive".to_slice)
+
+          pub = read_packet(io)
+
+          pub.should be_a(MQTT::Protocol::Publish)
+          pub = pub.as(MQTT::Protocol::Publish)
+          pub.payload.should eq("alive".to_slice)
+          pub.topic.should eq("a/b")
+
+          disconnect(io)
+        end
+      end
+    end
+
+    pending "will is delivered on ungraceful disconnect" do
+      with_server do |server|
+        with_client_io(server) do |io|
+          connect(io)
+          topic_filters = mk_topic_filters({"will/t", 0})
+          subscribe(io, topic_filters: topic_filters)
+
+          with_client_io(server) do |io2|
+            will = MQTT::Protocol::Will.new(
+              topic: "will/t", payload: "dead".to_slice, qos: 0u8, retain: false)
+            connect(io2, client_id: "will_client", will: will, keepalive: 1u16)
+          end
+
+          pub = read_packet(io)
+
+          pub.should be_a(MQTT::Protocol::Publish)
+          pub = pub.as(MQTT::Protocol::Publish)
+          pub.payload.should eq("dead".to_slice)
+          pub.topic.should eq("will/t")
+
+          disconnect(io)
+        end
+      end
+    end
+
+    pending "will can be retained [MQTT-3.1.2-17]" do
+      with_server do |server|
+        with_client_io(server) do |io2|
+          will = MQTT::Protocol::Will.new(
+            topic: "will/t", payload: "dead".to_slice, qos: 0u8, retain: true)
+          connect(io2, client_id: "will_client", will: will, keepalive: 1u16)
+        end
+
+        with_client_io(server) do |io|
+          connect(io)
+          topic_filters = mk_topic_filters({"will/t", 0})
+          subscribe(io, topic_filters: topic_filters)
+
+          pub = read_packet(io)
+
+          pub.should be_a(MQTT::Protocol::Publish)
+          pub = pub.as(MQTT::Protocol::Publish)
+          pub.payload.should eq("dead".to_slice)
+          pub.topic.should eq("will/t")
+          pub.retain?.should eq(true)
+
+          disconnect(io)
+        end
+      end
+    end
+
+    pending "will won't be published if missing permission" do
+      with_server do |server|
+        with_client_io(server) do |io|
+          connect(io)
+          topic_filters = mk_topic_filters({"topic-without-permission/t", 0})
+          subscribe(io, topic_filters: topic_filters)
+
+          with_client_io(server) do |io2|
+            will = MQTT::Protocol::Will.new(
+              topic: "will/t", payload: "dead".to_slice, qos: 0u8, retain: false)
+            connect(io2, client_id: "will_client", will: will, keepalive: 1u16)
+          end
+
+          # Send a ping to ensure we can read at least one packet, so we're not stuck
+          # waiting here (since this spec verifies that nothing is sent)
+          ping(io)
+
+          pkt = read_packet(io)
+          pkt.should be_a(MQTT::Protocol::PingResp)
+
+          disconnect(io)
+        end
+      end
+    end
+
+    pending "will qos can't be set of will flag is unset [MQTT-3.1.2-13]" do
+      with_server do |server|
+        with_client_io(server) do |io|
+          temp_io = IO::Memory.new
+          connect(MQTT::Protocol::IO.new(temp_io), client_id: "will_client", keepalive: 1u16, expect_response: false)
+          temp_io.rewind
+          connect_pkt = temp_io.to_slice
+          connect_pkt[9] |= 0b0001_0000u8
+          io.write connect_pkt
+
+          expect_raises(IO::Error) do
+            read_packet(io)
+          end
+        end
+      end
+    end
+
+    pending "will qos must not be 3 [MQTT-3.1.2-14]" do
+      with_server do |server|
+        with_client_io(server) do |io|
+          temp_io = IO::Memory.new
+          will = MQTT::Protocol::Will.new(
+            topic: "will/t", payload: "dead".to_slice, qos: 0u8, retain: false)
+          connect(MQTT::Protocol::IO.new(temp_io), will: will, client_id: "will_client", keepalive: 1u16, expect_response: false)
+          temp_io.rewind
+          connect_pkt = temp_io.to_slice
+          connect_pkt[9] |= 0b0001_1000u8
+          io.write connect_pkt
+
+          expect_raises(IO::Error) do
+            read_packet(io)
+          end
+        end
+      end
+    end
+
+    pending "will retain can't be set of will flag is unset [MQTT-3.1.2-15]" do
+      with_server do |server|
+        with_client_io(server) do |io|
+          temp_io = IO::Memory.new
+          connect(MQTT::Protocol::IO.new(temp_io), client_id: "will_client", keepalive: 1u16, expect_response: false)
+          temp_io.rewind
+          connect_pkt = temp_io.to_slice
+          connect_pkt[9] |= 0b0010_0000u8
+          io.write connect_pkt
+
+          expect_raises(IO::Error) do
+            read_packet(io)
+          end
+        end
+      end
+    end
+  end
+end
