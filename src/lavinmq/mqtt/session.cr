4f20533a535ceb660ba4c94bf992b727eccb40da
Christina <christina@84codes.com>
2024-09-23 11:47:39 +0200

    pass more specs and handle session_present

--- src/lavinmq/mqtt/session.cr
+++ src/lavinmq/mqtt/session.cr
@@ -1,6 +1,8 @@
 module LavinMQ
   module MQTT
     class Session < Queue
+       @clean_session : Bool = false
+       getter clean_session
       def initialize(@vhost : VHost,
                      @name : String,
                      @exclusive = true,
@@ -9,6 +11,9 @@ module LavinMQ
         super
       end
 
+      def set_clean_session
+        @clean_session = true
+      end
 
       #TODO: implement subscribers array and session_present? and send instead of false
       def connect(client)
