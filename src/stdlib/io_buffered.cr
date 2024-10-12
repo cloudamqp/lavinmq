e988e1410d4e6c1caf9f9a888157c256b0142a6e
Jon Börjesson <jon@spuun.se>
2024-10-12 08:40:03 +0200

    Remove unused code

--- src/stdlib/io_buffered.cr
+++ src/stdlib/io_buffered.cr
@@ -2,16 +2,16 @@ module IO::Buffered
   def peek(size : Int)
     raise RuntimeError.new("Can't fill buffer when read_buffering is #{read_buffering?}") unless read_buffering?
     raise ArgumentError.new("size must be positive") unless size.positive?
-
     if size > @buffer_size
       raise ArgumentError.new("size (#{size}) can't be greater than buffer_size #{@buffer_size}")
     end
 
+    # Enough data in buffer already
     return @in_buffer_rem[0, size] if size < @in_buffer_rem.size
 
-    remaining_capacity = @buffer_size - @in_buffer_rem.size
     in_buffer = in_buffer()
 
+    # Move data to beginning of in_buffer if needed
     if @in_buffer_rem.to_unsafe != in_buffer
       @in_buffer_rem.copy_to(in_buffer, @in_buffer_rem.size)
     end
@@ -20,7 +20,6 @@ module IO::Buffered
       target = Slice.new(in_buffer + @in_buffer_rem.size, @buffer_size - @in_buffer_rem.size)
       bytes_read = unbuffered_read(target).to_i
       break if bytes_read.zero?
-      remaining_capacity -= bytes_read
       @in_buffer_rem = Slice.new(in_buffer, @in_buffer_rem.size + bytes_read)
     end
 
