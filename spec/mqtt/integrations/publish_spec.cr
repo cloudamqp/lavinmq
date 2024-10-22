cf192227e270ac6b0d8eb806ccf21e1f7be31694
Jon Börjesson <jon@spuun.se>
2024-10-22 21:55:13 +0200

    Lint

--- spec/mqtt/integrations/publish_spec.cr
+++ spec/mqtt/integrations/publish_spec.cr
@@ -9,7 +9,7 @@ module MqttSpecs
       with_server do |server|
         with_client_io(server) do |io|
           connect(io)
-          payload = slice = Bytes[1, 254, 200, 197, 123, 4, 87]
+          payload = Bytes[1, 254, 200, 197, 123, 4, 87]
           ack = publish(io, topic: "test", payload: payload, qos: 1u8)
           ack.should be_a(MQTT::Protocol::PubAck)
         end
@@ -21,7 +21,7 @@ module MqttSpecs
         with_client_io(server) do |io|
           connect(io)
 
-          payload = slice = Bytes[1, 254, 200, 197, 123, 4, 87]
+          payload = Bytes[1, 254, 200, 197, 123, 4, 87]
           ack = publish(io, topic: "test", payload: payload, qos: 0u8)
           ack.should be_nil
         end
