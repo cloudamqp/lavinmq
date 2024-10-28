f5b8191c9422f0e0970a6769254116f93e473dd6
Jon Börjesson <jon@spuun.se>
2024-10-28 09:54:44 +0100

    fixup! Use write_timeout instead of fibers and write_close

--- src/lavinmq/amqp/client.cr
+++ src/lavinmq/amqp/client.cr
@@ -417,7 +417,7 @@ module LavinMQ
         @log.debug { "#{ex.inspect} when closing socket" }
       end
 
-      def close(reason = nil, timeout force_close_timeout : Time::Span = 60.seconds)
+      def close(reason = nil, timeout : Time::Span = 60.seconds)
         reason ||= "Connection closed"
         @log.info { "Closing: #{reason}" }
 
