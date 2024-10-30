c834d87b4f05aaa180a9a92e1e144d838549f5b9
Jon Börjesson <jon@spuun.se>
2024-10-30 08:26:16 +0100

    Move Sessions to separate file

---
+++ src/lavinmq/mqtt/sessions.cr
@@ -0,0 +1,37 @@
+require "./session"
+require "../vhost"
+
+module LavinMQ
+  module MQTT
+    struct Sessions
+      @queues : Hash(String, Queue)
+
+      def initialize(@vhost : VHost)
+        @queues = @vhost.queues
+      end
+
+      def []?(client_id : String) : Session?
+        @queues["amq.mqtt-#{client_id}"]?.try &.as(Session)
+      end
+
+      def [](client_id : String) : Session
+        @queues["amq.mqtt-#{client_id}"].as(Session)
+      end
+
+      def declare(client_id : String, clean_session : Bool)
+        self[client_id]? || begin
+          @vhost.declare_queue("amq.mqtt-#{client_id}", !clean_session, clean_session, AMQP::Table.new({"x-queue-type": "mqtt"}))
+          self[client_id]
+        end
+      end
+
+      def delete(client_id : String)
+        @vhost.delete_queue("amq.mqtt-#{client_id}")
+      end
+
+      def delete(session : Session)
+        session.delete
+      end
+    end
+  end
+end
