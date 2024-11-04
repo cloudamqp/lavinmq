75c66666d6ed57bbb7fac7a46d6abd5e898d023f
Christina <christina@84codes.com>
2024-11-04 14:53:32 +0100

    remove redundant return value

--- src/lavinmq/name_validator.cr
+++ src/lavinmq/name_validator.cr
@@ -5,7 +5,6 @@ class NameValidator
 
   def self.valid_prefix?(name)
     return true if PREFIX_LIST.any? { |prefix| name.starts_with? prefix }
-    return false
   end
 
   def self.valid_entity_name(name) : Bool
