713f10d8420252642f6d453ea3a2c984f76f6f05
Jon Börjesson <jon@spuun.se>
2024-11-11 14:39:42 +0100

    Use default timeout

--- src/lavinmq/http/controller/connections.cr
+++ src/lavinmq/http/controller/connections.cr
@@ -38,7 +38,7 @@ module LavinMQ
         delete "/api/connections/:name" do |context, params|
           with_connection(context, params) do |c|
             reason = context.request.headers["X-Reason"]? || "Closed via management plugin"
-            c.close(reason, timeout: 3.seconds)
+            c.close(reason)
             context.response.status_code = 204
           end
         end
