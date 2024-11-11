d09bdaac4ec9f0c4defdec2db53734ca8961775e
Viktor Erlingsson <viktor@84codes.com>
2024-11-11 15:16:54 +0100

    set both indexes when opening new segment

--- src/lavinmq/amqp/queue/stream_queue_message_store.cr
+++ src/lavinmq/amqp/queue/stream_queue_message_store.cr
@@ -167,7 +167,8 @@ module LavinMQ::AMQP
       private def open_new_segment(next_msg_size = 0) : MFile
         super.tap do
           drop_overflow
-          @offset_index[@segments.last_key] = @last_offset
+          @offset_index[@segments.last_key] = @last_offset + 1
+          @timestamp_index[@segments.last_key] = RoughTime.unix_ms
         end
       end
 
