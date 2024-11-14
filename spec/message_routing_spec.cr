437e3a4269d86d43b0c3a9ab259a38bde875b4b4
Magnus Landerblom <magnus@landerblom.se>
2024-11-14 08:50:59 +0100

    fix mqtt exchange routing spec

--- spec/message_routing_spec.cr
+++ spec/message_routing_spec.cr
@@ -422,29 +422,43 @@ describe LavinMQ::Exchange do
   end
 end
 
-# describe LavinMQ::MQTT::Exchange do
-#   it "should only allow Session to bind" do
-#     with_amqp_server do |s|
-#       vhost = s.vhosts.create("x")
-#       q1 = LavinMQ::AMQP::Queue.new(vhost, "q1")
-#       s1 = LavinMQ::MQTT::Session.new(vhost, "q1")
-#       x = LavinMQ::MQTT::Exchange.new(vhost, "", LavinMQ::MQTT::RetainStore.new(vhost.data_dir))
-#       x.bind(s1, "s1", LavinMQ::AMQP::Table.new)
-#       expect_raises(LavinMQ::Exchange::AccessRefused) do
-#         x.bind(q1, "q1", LavinMQ::AMQP::Table.new)
-#       end
-#     end
-#   end
+alias IndexTree = LavinMQ::MQTT::TopicTree(String)
+describe LavinMQ::MQTT::Exchange do
+  it "should only allow Session to bind" do
+    with_amqp_server do |s|
+      vhost = s.vhosts.create("x")
+      q1 = LavinMQ::AMQP::Queue.new(vhost, "q1")
+      s1 = LavinMQ::MQTT::Session.new(vhost, "q1")
+      index = IndexTree.new
+      store = LavinMQ::MQTT::RetainStore.new("tmp/retain_store", LavinMQ::Clustering::NoopServer.new, index)
+      x = LavinMQ::MQTT::Exchange.new(vhost, "", store)
+      x.bind(s1, "s1", LavinMQ::AMQP::Table.new)
+      expect_raises(LavinMQ::Exchange::AccessRefused) do
+        x.bind(q1, "q1", LavinMQ::AMQP::Table.new)
+      end
+    end
+  end
 
-#   it "publish messages to queues with it's own publish method" do
-#     with_amqp_server do |s|
-#       vhost = s.vhosts.create("x")
-#       s1 = LavinMQ::MQTT::Session.new(vhost, "session 1")
-#       x = LavinMQ::MQTT::Exchange.new(vhost, "mqtt.default", LavinMQ::MQTT::RetainStore.new(vhost.data_dir))
-#       x.bind(s1, "s1", LavinMQ::AMQP::Table.new)
-#       msg = LavinMQ::Message.new("mqtt.default", "s1", "hej")
-#       x.publish(msg, false)
-#       s1.message_count.should eq 1
-#     end
-#   end
-# end
+  it "publish messages to queues with it's own publish method" do
+    with_amqp_server do |s|
+      vhost = s.vhosts.create("x")
+      s1 = LavinMQ::MQTT::Session.new(vhost, "session 1")
+      index = IndexTree.new
+      store = LavinMQ::MQTT::RetainStore.new("tmp/retain_store", LavinMQ::Clustering::NoopServer.new, index)
+      x = LavinMQ::MQTT::Exchange.new(vhost, "mqtt.default", store)
+      x.bind(s1, "s1", LavinMQ::AMQP::Table.new)
+      # msg = LavinMQ::Message.new("mqtt.default", "s1", "hej")
+      pub_args = {
+        packet_id: 1u16,
+        payload:   Bytes.new(0),
+        dup:       false,
+        qos:       0u8,
+        retain:    false,
+        topic:     "s1",
+      }
+      msg = MQTT::Protocol::Publish.new(**pub_args)
+      x.publish(msg)
+      s1.message_count.should eq 1
+    end
+  end
+end
