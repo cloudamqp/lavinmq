43ec2952754f52545728c5c8581b0610aaaecd81
Christina <christina@84codes.com>
2024-11-18 17:46:41 +0100

    format

--- src/lavinmq/mqtt/consts.cr
+++ src/lavinmq/mqtt/consts.cr
@@ -1,7 +1,7 @@
 module LavinMQ
   module MQTT
-    EXCHANGE   = "mqtt.default"
-    QOS_HEADER = "mqtt.qos"
+    EXCHANGE      = "mqtt.default"
+    QOS_HEADER    = "mqtt.qos"
     RETAIN_HEADER = "mqtt.retain"
   end
 end
