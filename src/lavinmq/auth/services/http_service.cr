efdb88eab0d57486620fd584f14c203a744b6890
Jade D. Kharats <jade.kharats@gmail.com>
2024-11-25 22:34:42 +0100

    add auth chain

---
+++ src/lavinmq/auth/services/http_service.cr
@@ -0,0 +1,28 @@
+require "http/client"
+require "json"
+require "./auth_service"
+
+module LavinMQ
+  class HttpAuthService < AuthenticationService
+    def initialize(@method : String, @user_path : String, @whost_path : String, @resource_path : String, @topic_path : String)
+    end
+
+    def authorize?(username : String, password : String)
+
+      payload = {
+        "username" => username,
+        "password" => password
+      }.to_json
+
+      success = ::HTTP::Client.post(@user_path,
+        headers: ::HTTP::Headers{"Content-Type" => "application/json"}, 
+        body: payload).success?
+
+      if success
+        "allow"
+      else
+        try_next(username, password)
+      end
+    end
+  end
+end
