f0c204d24084169b03f2fabffd3cab108f2fbd93
Christina <christina@84codes.com>
2024-11-26 16:35:32 +0100

    format

--- src/lavinmq/auth/oauth2.cr
+++ src/lavinmq/auth/oauth2.cr
@@ -1,7 +1,6 @@
 module LavinMQ
   class OAuth2Handler < LavinMQ::AuthHandler
     def authenticate(username : String, password : String)
-
       # TODO: implement the OAuth2 authentication logic and permissions parser here
       if password.starts_with?("oauth")
         puts "OAuth2 authentication successful"
