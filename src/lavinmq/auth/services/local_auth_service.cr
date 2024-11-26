9e6bacf47f0085595c4e7cea09e3d2bf698e4cce
Jade D. Kharats <jade.kharats@gmail.com>
2024-11-26 21:52:12 +0100

    change return type

--- src/lavinmq/auth/services/local_auth_service.cr
+++ src/lavinmq/auth/services/local_auth_service.cr
@@ -2,23 +2,19 @@ require "./auth_service"
 
 module LavinMQ
   class LocalAuthService < AuthenticationService
-    def authenticate(username : String, password : String, users : UserStore, remote_address : Socket::Address)
-      if user = users[username]?
-        if user && user.password && user.password.not_nil!.verify(password) &&
-                     guest_only_loopback?(remote_address, user)
-          return user
+    def initialize(@users_store : UserStore)
+    end
+
+    def authorize?(username : String, password : String)
+      if user = @users_store[username]?
+        if user.password && user.password.not_nil!.verify(password)
+          true
         else
           try_next(username, password)
         end
       else
         try_next(username, password)
       end
-    end
-
-    private def guest_only_loopback?(remote_address, user) : Bool
-      return true unless user.name == "guest"
-      return true unless Config.instance.guest_only_loopback?
-      remote_address.loopback?
     end
   end
 end
