cb9080a8f6e55bf56bc8fbd3d9971612e509a614
Christina <christina@84codes.com>
2024-11-26 16:35:32 +0100

    format

--- src/lavinmq/auth/basic_auth.cr
+++ src/lavinmq/auth/basic_auth.cr
@@ -3,20 +3,19 @@ require "../server"
 
 module LavinMQ
   class BasicAuthHandler < LavinMQ::AuthHandler
-
     def authenticate(username : String, password : String, users : UserStore, remote_address : Socket::Address)
       user = users[username]
       # TODO: do not do authentication check if the user is not in the userstore, instead pass directly to the next handler
       return user if user && user.password && user.password.not_nil!.verify(password) &&
-                       guest_only_loopback?(remote_address, user)
+                     guest_only_loopback?(remote_address, user)
       puts "Basic authentication failed"
       @successor ? @successor.try &.authenticate(username, password) : nil
     end
 
     private def guest_only_loopback?(remote_address, user) : Bool
-        return true unless user.name == "guest"
-        return true unless Config.instance.guest_only_loopback?
-        remote_address.loopback?
+      return true unless user.name == "guest"
+      return true unless Config.instance.guest_only_loopback?
+      remote_address.loopback?
     end
   end
 end
