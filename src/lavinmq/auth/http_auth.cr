e233c71caae632929880be16d5eb9051ff0d73d8
Christina <christina@84codes.com>
2024-11-27 11:03:03 +0100

    adapt http_auth

--- src/lavinmq/auth/http_auth.cr
+++ src/lavinmq/auth/http_auth.cr
@@ -3,21 +3,16 @@ require "json"
 require "./auth_service"
 
 module LavinMQ
-  class HttpAuthService < AuthenticationService
-    def initialize(@method : String, @user_path : String, @whost_path : String, @resource_path : String, @topic_path : String)
-    end
-
-    def authorize?(username : String, password : String)
-
+  class HttpAuthHandler < AuthHandler
+    def authenticate(username : String, password : String)
       payload = {
         "username" => username,
         "password" => password
       }.to_json
 
       success = ::HTTP::Client.post(@user_path,
-        headers: ::HTTP::Headers{"Content-Type" => "application/json"}, 
+        headers: ::HTTP::Headers{"Content-Type" => "application/json"},
         body: payload).success?
-
       if success
         "allow"
       else
