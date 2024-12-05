2f56ef387dc85dcdbd84c8b0fd2e7e5beca23c57
Viktor Erlingsson <viktor@84codes.com>
2024-12-05 16:40:09 +0100

    add filtering functionality for streams

--- src/lavinmq/amqp/stream_consumer.cr
+++ src/lavinmq/amqp/stream_consumer.cr
@@ -9,6 +9,7 @@ module LavinMQ
       property segment : UInt32
       property pos : UInt32
       getter requeued = Deque(SegmentPosition).new
+      getter filter : String?
 
       def initialize(@channel : Client::Channel, @queue : StreamQueue, frame : AMQP::Frame::Basic::Consume)
         validate_preconditions(frame)
@@ -36,6 +37,12 @@ module LavinMQ
         case frame.arguments["x-stream-offset"]?
         when Nil, Int, Time, "first", "next", "last"
         else raise LavinMQ::Error::PreconditionFailed.new("x-stream-offset must be an integer, a timestamp, 'first', 'next' or 'last'")
+        end
+        case frame.arguments["x-stream-filter-value"]?
+        when String
+          @filter = frame.arguments["x-stream-filter-value"].to_s
+        when Nil
+        else raise LavinMQ::Error::PreconditionFailed.new("x-stream-filter-value must be a string")
         end
       end
 
