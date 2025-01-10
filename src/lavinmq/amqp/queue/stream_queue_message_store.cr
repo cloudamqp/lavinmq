6d1d98b5dcbea1edfe95526dfb0050b70f2a63f7
Viktor Erlingsson <viktor@84codes.com>
2025-01-10 09:56:38 +0100

    refactor
    Co-authored-by: Jon Börjesson <jon@84codes.com>

--- src/lavinmq/amqp/queue/stream_queue_message_store.cr
+++ src/lavinmq/amqp/queue/stream_queue_message_store.cr
@@ -137,9 +137,7 @@ module LavinMQ::AMQP
 
       private def matching?(msg_headers, consumer_filter, match_unfiltered) : Bool
         if msg_filter = filter_value_from_headers(msg_headers)
-          consumer_filter.each do |filter|
-            return true if msg_filter == filter
-          end
+            return consumer_filter.includes? msg_filter
         else
           return match_unfiltered
         end
