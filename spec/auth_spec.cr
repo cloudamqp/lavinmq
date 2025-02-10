fa9d0469c566f66d34adeb360f66fd772e24d9b2
Christina <christina@84codes.com>
2025-02-10 11:46:54 +0100

    format

--- spec/auth_spec.cr
+++ spec/auth_spec.cr
@@ -3,13 +3,12 @@ require "../src/lavinmq/auth/chain"
 require "../src/lavinmq/auth/authenticators/basic"
 
 describe LavinMQ::Auth::Chain do
-
   it "Creates a default authentication chain if not configured" do
-     with_amqp_server do |s|
+    with_amqp_server do |s|
       chain = LavinMQ::Auth::Chain.create(s.@config, s.@users)
       chain.@backends.should be_a Array(LavinMQ::Auth::Authenticator)
       chain.@backends.size.should eq 1
-     end
+    end
   end
 
   it "Successfully authenticates and returns a basic user" do
