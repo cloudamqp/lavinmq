f19fbd23d4e0a6977b5cf143709e3996d7919b77
Christina <christina@84codes.com>
2025-02-10 12:46:35 +0100

    remove redundant beign

--- src/lavinmq/auth/authenticators/basic.cr
+++ src/lavinmq/auth/authenticators/basic.cr
@@ -8,12 +8,10 @@ module LavinMQ
       end
 
       def authenticate(username : String, password : String) : User?
-        begin
-          user = @users[username]
-          return user if user && user.password && user.password.not_nil!.verify(password)
-        rescue ex : Exception
-          Log.error { "Basic authentication failed: #{ex.message}" }
-        end
+        user = @users[username]
+        return user if user && user.password && user.password.not_nil!.verify(password)
+      rescue ex : Exception
+        Log.error { "Basic authentication failed: #{ex.message}" }
       end
     end
   end
