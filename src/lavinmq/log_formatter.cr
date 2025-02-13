c99c23eee23cd407c77766e9314576986e1559c9
Anders Bälter <anders@84codes.com>
2025-02-13 13:13:41 +0100

    Add priority on journald log format

--- src/lavinmq/log_formatter.cr
+++ src/lavinmq/log_formatter.cr
@@ -3,12 +3,26 @@ require "log"
 module LavinMQ
   struct JournalLogFormat < ::Log::StaticFormatter
     def run
+      string "<#{severity_to_priority}> "
       source
       context(before: '[', after: ']')
       data(before: '[', after: ']')
       string ' '
       message
       exception
+    end
+
+    private def severity_to_priority
+      case @entry.severity
+      when ::Log::Severity::Trace  then 7 # journald doesn't have trace
+      when ::Log::Severity::Debug  then 7
+      when ::Log::Severity::Info   then 6
+      when ::Log::Severity::Notice then 5
+      when ::Log::Severity::Warn   then 4
+      when ::Log::Severity::Error  then 3
+      when ::Log::Severity::Fatal  then 2
+      else                              6 # Default to "info"
+      end
     end
   end
 
