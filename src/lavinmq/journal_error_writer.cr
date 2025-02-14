2606e11bd62422e65f46b2241945a6f97f9c208b
Anders Bälter <anders@84codes.com>
2025-02-14 14:20:49 +0100

    Make less syscall during logging

--- src/lavinmq/journal_error_writer.cr
+++ src/lavinmq/journal_error_writer.cr
@@ -5,10 +5,7 @@ module LavinMQ
     end
 
     def write(slice : Bytes) : Nil
-      self.write_byte(60_u8) # '<'
-      self.write_byte(51_u8) # '3'
-      self.write_byte(62_u8) # '>'
-      self.write_byte(32_u8) # ' ' (space)
+      self.write_string "<3> ".to_slice
       super slice
     end
   end
