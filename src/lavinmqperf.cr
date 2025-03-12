06fa700eb200b57c8091f1e7a8859e12c06ed00e
Christina <christina@84codes.com>
2025-03-12 12:47:55 +0100

    wip

--- src/lavinmqperf.cr
+++ src/lavinmqperf.cr
@@ -1,6 +1,7 @@
 require "log"
 require "./lavinmq/version"
-require "./lavinmqperf/*"
+require "./lavinmqperf/amqp/*"
+require "./lavinmqperf/mqtt/*"
 require "./stdlib/resource"
 
 {% unless flag?(:release) %}
@@ -10,17 +11,28 @@ require "./stdlib/resource"
 Log.setup_from_env
 
 module LavinMQPerf
-  mode = ARGV.shift?
-  case mode
-  when "throughput"       then Throughput.new.run
-  when "bind-churn"       then BindChurn.new.run
-  when "queue-churn"      then QueueChurn.new.run
-  when "connection-churn" then ConnectionChurn.new.run
-  when "channel-churn"    then ChannelChurn.new.run
-  when "consumer-churn"   then ConsumerChurn.new.run
-  when "connection-count" then ConnectionCount.new.run
-  when "queue-count"      then QueueCount.new.run
-  when /^.+$/             then Perf.new.run([mode.not_nil!])
-  else                         abort Perf.new.banner
+   protocol = ARGV.shift? || "amqp"
+  case protocol
+  when "amqp"
+    mode = ARGV.shift?
+    case mode
+    when "throughput"       then AMQP::Throughput.new.run
+    when "bind-churn"       then AMQP::BindChurn.new.run
+    when "queue-churn"      then AMQP::QueueChurn.new.run
+    when "connection-churn" then AMQP::ConnectionChurn.new.run
+    when "channel-churn"    then AMQP::ChannelChurn.new.run
+    when "consumer-churn"   then AMQP::ConsumerChurn.new.run
+    when "connection-count" then AMQP::ConnectionCount.new.run
+    when "queue-count"      then AMQP::QueueCount.new.run
+    when /^.+$/             then Perf.new.run([mode.not_nil!])
+    else                         abort Perf.new.banner
+    end
+  when "mqtt"
+    mode = ARGV.shift?
+    case mode
+    when "throughput"       then pp "no mqtt throughput yet"
+    else                         abort Perf.new.banner
+    end
+  else abort Perf.new.banner
   end
 end
