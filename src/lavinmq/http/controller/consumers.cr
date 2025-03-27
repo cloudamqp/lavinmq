0edf131b7a8a26bf7206061991173f554fa1e7c4
Magnus Landerblom <magnus@landerblom.se>
2025-03-27 15:59:35 +0100

    API endpoint to adjust the consumer prefetch

--- src/lavinmq/http/controller/consumers.cr
+++ src/lavinmq/http/controller/consumers.cr
@@ -32,20 +32,7 @@ module LavinMQ
           with_vhost(context, params) do |vhost|
             user = user(context)
             refuse_unless_management(context, user, vhost)
-            consumer_tag = URI.decode_www_form(params["consumer_tag"])
-            conn_id = URI.decode_www_form(params["connection"])
-            ch_id = URI.decode_www_form(params["channel"]).to_i
-            connection = connections(user).find { |conn| conn.vhost.name == vhost && conn.name == conn_id }
-            unless connection
-              context.response.status_code = 404
-              break
-            end
-            channel = connection.channels[ch_id]?
-            unless channel
-              context.response.status_code = 404
-              break
-            end
-            consumer = channel.consumers.find(&.tag.==(consumer_tag))
+            consumer = find_consumer_from_params(user, vhost, params)
             unless consumer
               context.response.status_code = 404
               break
@@ -55,6 +42,37 @@ module LavinMQ
           end
           context
         end
+
+        put "/api/consumers/:vhost/:connection/:channel/:consumer_tag" do |context, params|
+          with_vhost(context, params) do |vhost|
+            user = user(context)
+            refuse_unless_management(context, user, vhost)
+            consumer = find_consumer_from_params(user, vhost, params)
+            unless consumer
+              context.response.status_code = 404
+              break
+            end
+            body = parse_body(context)
+            if prefetch = body["prefetch"]?.try(&.as_i?)
+              prefetch = 0 if prefetch < 0
+              prefetch = UInt16::MAX if prefetch > UInt16::MAX
+              consumer.prefetch_count = prefetch.to_u16
+            end
+            context.response.status_code = 204
+          end
+          context
+        end
+      end
+
+      private def find_consumer_from_params(user, vhost, params)
+        consumer_tag = URI.decode_www_form(params["consumer_tag"])
+        conn_id = URI.decode_www_form(params["connection"])
+        ch_id = URI.decode_www_form(params["channel"]).to_i
+        connection = connections(user).find { |conn| conn.vhost.name == vhost && conn.name == conn_id }
+        return unless connection
+        channel = connection.channels[ch_id]?
+        return unless channel
+        channel.consumers.find(&.tag.==(consumer_tag))
       end
 
       private def all_consumers(user)
