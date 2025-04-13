2ba9cd65ec31a811c03e18dcfae49df253ad174c
Carl Hörberg <carl@84codes.com>
2025-04-13 20:52:24 +0200

    Log memory usage and time for each http request
    Used when optimizing the paging method

--- src/lavinmq/http/handler/defaults_handler.cr
+++ src/lavinmq/http/handler/defaults_handler.cr
@@ -4,10 +4,21 @@ module LavinMQ
   module HTTP
     class ApiDefaultsHandler
       include ::HTTP::Handler
+      Log = LavinMQ::Log.for "http.performance"
 
       def call(context)
         context.response.content_type = "application/json"
-        call_next(context)
+        {% if flag?(:release) %}
+          call_next(context)
+        {% else %}
+          elapsed = Time::Span.new
+          mem = Benchmark.memory do
+            elapsed = Benchmark.realtime do
+              call_next(context)
+            end
+          end
+          Log.info { "request=#{context.request.path}?#{context.request.query} memory=#{mem.humanize_bytes} elapsed=#{elapsed.total_milliseconds.to_i}ms" }
+        {% end %}
       end
     end
   end
