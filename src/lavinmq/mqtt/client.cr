bee788566172fdb0f8d90400a96465749d3b85a4
Carl Hörberg <carl@84codes.com>
2025-04-13 20:23:47 +0200

    MQTT broker adds/rm clients
    Instead of the Client it self, separation of concerns

--- src/lavinmq/mqtt/client.cr
+++ src/lavinmq/mqtt/client.cr
@@ -38,7 +38,6 @@ module LavinMQ
         @name = "#{@connection_info.remote_address} -> #{@connection_info.local_address}"
         metadata = ::Log::Metadata.new(nil, {vhost: @broker.vhost.name, address: @connection_info.remote_address.to_s, client_id: client_id})
         @log = Logger.new(Log, metadata)
-        @broker.vhost.add_connection(self)
         @log.info { "Connection established for user=#{@user.name}" }
         spawn read_loop, name: "MQTT read_loop #{@connection_info.remote_address}"
       end
@@ -59,7 +58,7 @@ module LavinMQ
           # The disconnect packet has been handled and the socket has been closed.
           # If we dont breakt the loop here we'll get a IO/Error on next read.
           if packet.is_a?(MQTT::Disconnect)
-            @log.debug { "Recieved disconnect" }
+            @log.debug { "Received disconnect" }
             break
           end
         end
