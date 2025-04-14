1d386e1735f16360f3d6a87976760ecaa361277a
Viktor Erlingsson <viktor@84codes.com>
2025-04-14 10:23:14 +0200

    Add specs for x-filter-match-type.
    Add specs for different x-stream-filter types.
    
    Add PublishHelper to make handle repetitive tasks to make specs more readable.

--- spec/stream_queue_filters_spec.cr
+++ spec/stream_queue_filters_spec.cr
@@ -2,18 +2,11 @@ require "./spec_helper"
 require "./../src/lavinmq/amqp/queue"
 
 describe LavinMQ::AMQP::StreamQueue do
-  stream_queue_args = LavinMQ::AMQP::Table.new({"x-queue-type": "stream"})
-
   describe "Filters" do
     it "should only get message matching filter" do
       with_amqp_server do |s|
         with_channel(s) do |ch|
-          ch.prefetch 1
-          q = ch.queue("stream_filter_1", args: stream_queue_args)
-          q.publish("msg without filter")
-          hdrs = AMQP::Client::Arguments.new({"x-stream-filter-value" => "foo"})
-          q.publish("msg with filter", props: AMQP::Client::Properties.new(headers: hdrs))
-          q.publish("msg without filter")
+          q = PublishHelper.create_queue_and_messages(s, ch)
 
           msgs = Channel(AMQP::Client::DeliverMessage).new
           q.subscribe(no_ack: false, args: AMQP::Client::Arguments.new({"x-stream-filter": "foo"})) do |msg|
@@ -21,7 +14,7 @@ describe LavinMQ::AMQP::StreamQueue do
             msg.ack
           end
           msg = msgs.receive
-          msg.body_io.to_s.should eq "msg with filter"
+          msg.body_io.to_s.should eq "msg with filter: foo"
         end
       end
     end
@@ -29,14 +22,7 @@ describe LavinMQ::AMQP::StreamQueue do
     it "should ignore messages with non-matching filters" do
       with_amqp_server do |s|
         with_channel(s) do |ch|
-          ch.prefetch 1
-          q = ch.queue("stream_filter_2", args: stream_queue_args)
-          q.publish("msg without filter")
-          hdrs = AMQP::Client::Arguments.new({"x-stream-filter-value" => "foo"})
-          q.publish("msg with filter", props: AMQP::Client::Properties.new(headers: hdrs))
-          hdrs = AMQP::Client::Arguments.new({"x-stream-filter-value" => "bar"})
-          q.publish("msg with filter: bar", props: AMQP::Client::Properties.new(headers: hdrs))
-          q.publish("msg without filter")
+          q = PublishHelper.create_queue_and_messages(s, ch)
 
           msgs = Channel(AMQP::Client::DeliverMessage).new
           q.subscribe(no_ack: false, args: AMQP::Client::Arguments.new({"x-stream-filter": "bar"})) do |msg|
@@ -52,19 +38,8 @@ describe LavinMQ::AMQP::StreamQueue do
     it "should support multiple filters" do
       with_amqp_server do |s|
         with_channel(s) do |ch|
-          ch.prefetch 1
-          q = ch.queue(Random::Secure.hex, args: stream_queue_args)
-          q.publish("msg without filter")
-          hdrs = AMQP::Client::Arguments.new({"x-stream-filter-value" => "foo"})
-          q.publish("msg with filter: foo", props: AMQP::Client::Properties.new(headers: hdrs))
-          hdrs = AMQP::Client::Arguments.new({"x-stream-filter-value" => "xyz"})
-          #q.publish("msg with filter: xyz", props: AMQP::Client::Properties.new(headers: hdrs))
-          hdrs = AMQP::Client::Arguments.new({"x-stream-filter-value" => "foo,bar"})
-          q.publish("msg with filter: foo,bar", props: AMQP::Client::Properties.new(headers: hdrs))
-          hdrs = AMQP::Client::Arguments.new({"x-stream-filter-value" => "foo,bar,xyz"})
-          q.publish("msg with filter: foo,bar,xyz", props: AMQP::Client::Properties.new(headers: hdrs))
-          q.publish("msg without filter")
-          
+          q = PublishHelper.create_queue_and_messages(s, ch)
+
           msgs = Channel(AMQP::Client::DeliverMessage).new
           filters = "foo,bar"
           q.subscribe(no_ack: false, args: AMQP::Client::Arguments.new(
@@ -74,58 +49,9 @@ describe LavinMQ::AMQP::StreamQueue do
             msg.ack
           end
           msg = msgs.receive
-          msg.body_io.to_s.should eq "msg with filter: foo,bar"
+          msg.body_io.to_s.should eq "msg with filter: foo,bar and x-foo=>bar"
           msg = msgs.receive
           msg.body_io.to_s.should eq "msg with filter: foo,bar,xyz"
-        end
-      end
-    end
-
-    it "should support filtering on any header" do
-      with_amqp_server do |s|
-        with_channel(s) do |ch|
-          ch.prefetch 1
-          q = ch.queue(Random::Secure.hex, args: stream_queue_args)
-          q.publish("msg without filter")
-          hdrs = AMQP::Client::Arguments.new({"x-foo" => "bar"})
-          q.publish("msg with filter: x-foo => bar", props: AMQP::Client::Properties.new(headers: hdrs))
-          q.publish("msg without filter")
-          
-          msgs = Channel(AMQP::Client::DeliverMessage).new
-          filters = {"x-foo" => "bar"}
-          q.subscribe(no_ack: false, args: AMQP::Client::Arguments.new(
-            {"x-stream-filter": filters}
-          )) do |msg|
-            msgs.send msg
-            msg.ack
-          end
-          msg = msgs.receive
-          msg.body_io.to_s.should eq "msg with filter: x-foo => bar"
-        end
-      end
-    end
-
-
-    it "should support x-stream-filter and any header" do
-      with_amqp_server do |s|
-        with_channel(s) do |ch|
-          ch.prefetch 1
-          q = ch.queue(Random::Secure.hex, args: stream_queue_args)
-          q.publish("msg without filter")
-          hdrs = AMQP::Client::Arguments.new({"x-stream-filter-value" => "foo,bar", "x-foo" => "bar"})
-          q.publish("msg with filter: foo,bar", props: AMQP::Client::Properties.new(headers: hdrs))
-          q.publish("msg without filter")
-          
-          msgs = Channel(AMQP::Client::DeliverMessage).new
-          filters = AMQ::Protocol::Table.new(["foo,bar", {"x-foo" => "bar"}])
-          q.subscribe(no_ack: false, args: AMQP::Client::Arguments.new(
-            {"x-stream-filter": filters}
-          )) do |msg|
-            msgs.send msg
-            msg.ack
-          end
-          msg = msgs.receive
-          msg.body_io.to_s.should eq "msg with filter: foo,bar"
         end
       end
     end
@@ -133,13 +59,7 @@ describe LavinMQ::AMQP::StreamQueue do
     it "should get messages without filter when x-stream-match-unfiltered set" do
       with_amqp_server do |s|
         with_channel(s) do |ch|
-          ch.prefetch 1
-          q = ch.queue("stream_filter_4", args: stream_queue_args)
-          hdrs = AMQP::Client::Arguments.new({"x-stream-filter-value" => "foo"})
-          q.publish("msg with filter: foo", props: AMQP::Client::Properties.new(headers: hdrs))
-          hdrs = AMQP::Client::Arguments.new({"x-stream-filter-value" => "bar"})
-          q.publish("msg with filter: bar", props: AMQP::Client::Properties.new(headers: hdrs))
-          q.publish("msg without filter")
+          q = PublishHelper.create_queue_and_messages(s, ch)
 
           msgs = Channel(AMQP::Client::DeliverMessage).new
           q.subscribe(no_ack: false, args: AMQP::Client::Arguments.new(
@@ -151,38 +71,153 @@ describe LavinMQ::AMQP::StreamQueue do
             msgs.send msg
             msg.ack
           end
-          msg = msgs.receive
-          msg.body_io.to_s.should eq "msg with filter: foo"
+
           msg = msgs.receive
           msg.body_io.to_s.should eq "msg without filter"
+          msg = msgs.receive
+          msg.body_io.to_s.should eq "msg with filter: foo"
         end
       end
     end
 
-    it "should respect offset values while filtering" do
+    it "should still respect offset values while filtering" do
       with_amqp_server do |s|
         with_channel(s) do |ch|
-          ch.prefetch 1
-          q = ch.queue("stream_filter_5", args: stream_queue_args)
-          hdrs = AMQP::Client::Arguments.new({"x-stream-filter-value" => "foo"})
-          q.publish("msg with filter 1", props: AMQP::Client::Properties.new(headers: hdrs))
-          q.publish("msg with filter 2", props: AMQP::Client::Properties.new(headers: hdrs))
-          q.publish("msg without filter")
+          q = PublishHelper.create_queue_and_messages(s, ch)
 
           msgs = Channel(AMQP::Client::DeliverMessage).new
-          args = AMQP::Client::Arguments.new({"x-stream-filter": "foo", "x-stream-offset": 2})
+          args = AMQP::Client::Arguments.new({"x-stream-filter": "foo", "x-stream-offset": 3})
           q.subscribe(no_ack: false, args: args) do |msg|
             msgs.send msg
             msg.ack
           end
           msg = msgs.receive
-          msg.body_io.to_s.should eq "msg with filter 2"
+          msg.body_io.to_s.should eq "msg with filter: foo,bar and x-foo=>bar"
+          if headers = msg.properties.headers
+            headers["x-stream-offset"].should eq 4
+          else
+            fail "No headers in message"
+          end
         end
       end
     end
 
-    it "filter-match-type" do
-      
+    describe "Filter on any header" do
+      it "should support filtering on any header" do
+        with_amqp_server do |s|
+          with_channel(s) do |ch|
+            q = PublishHelper.create_queue_and_messages(s, ch)
+
+            msgs = Channel(AMQP::Client::DeliverMessage).new
+            filters = {"x-foo" => "bar"}
+            q.subscribe(no_ack: false, args: AMQP::Client::Arguments.new(
+              {"x-stream-filter": filters}
+            )) do |msg|
+              msgs.send msg
+              msg.ack
+            end
+            msg = msgs.receive
+            msg.body_io.to_s.should eq "msg with filter: foo,bar and x-foo=>bar"
+          end
+        end
+      end
+
+      it "x-stream-filter can be a AMQ::Protocol::Table with hashes" do
+        with_amqp_server do |s|
+          with_channel(s) do |ch|
+            q = PublishHelper.create_queue_and_messages(s, ch)
+
+            msgs = Channel(AMQP::Client::DeliverMessage).new
+            filters = AMQ::Protocol::Table.new({"x-stream-filter" => "foo,bar", "x-foo" => "bar"})
+            q.subscribe(no_ack: false, args: AMQP::Client::Arguments.new(
+              {"x-stream-filter": filters}
+            )) do |msg|
+              msgs.send msg
+              msg.ack
+            end
+            msg = msgs.receive
+            msg.body_io.to_s.should eq "msg with filter: foo,bar and x-foo=>bar"
+          end
+        end
+      end
+
+      it "x-stream-filter can be an array with string and hashes" do
+        with_amqp_server do |s|
+          with_channel(s) do |ch|
+            q = PublishHelper.create_queue_and_messages(s, ch)
+
+            msgs = Channel(AMQP::Client::DeliverMessage).new
+            filters = ["foo,bar", {"x-foo" => "bar"}]
+            q.subscribe(no_ack: false, args: AMQP::Client::Arguments.new(
+              {"x-stream-filter": filters}
+            )) do |msg|
+              msgs.send msg
+              msg.ack
+            end
+            msg = msgs.receive
+            msg.body_io.to_s.should eq "msg with filter: foo,bar and x-foo=>bar"
+          end
+        end
+      end
     end
+
+    describe "Handle x-filter-match-type" do
+      it "ANY" do
+        with_amqp_server do |s|
+          with_channel(s) do |ch|
+            q = PublishHelper.create_queue_and_messages(s, ch)
+
+            msgs = Channel(AMQP::Client::DeliverMessage).new
+            filters = "foo,bar"
+            q.subscribe(no_ack: false, args: AMQP::Client::Arguments.new(
+              {"x-stream-filter": filters, "x-filter-match-type": "ANY"}
+            )) do |msg|
+              msgs.send msg
+              msg.ack
+            end
+            msg = msgs.receive
+            msg.body_io.to_s.should eq "msg with filter: foo"
+            msg = msgs.receive
+            msg.body_io.to_s.should eq "msg with filter: bar"
+          end
+        end
+      end
+      it "ALL" do
+        with_amqp_server do |s|
+          with_channel(s) do |ch|
+            q = PublishHelper.create_queue_and_messages(s, ch)
+
+            msgs = Channel(AMQP::Client::DeliverMessage).new
+            filters = "foo,bar"
+            q.subscribe(no_ack: false, args: AMQP::Client::Arguments.new(
+              {"x-stream-filter": filters, "x-filter-match-type": "ALL"}
+            )) do |msg|
+              msgs.send msg
+              msg.ack
+            end
+            msg = msgs.receive
+            msg.body_io.to_s.should eq "msg with filter: foo,bar and x-foo=>bar"
+          end
+        end
+      end
+    end
+  end
+end
+
+module PublishHelper
+  def self.create_queue_and_messages(s, ch)
+    ch.prefetch 1
+    q = ch.queue("stream_filters", args: LavinMQ::AMQP::Table.new({"x-queue-type": "stream"}))
+    q.publish("msg without filter")
+    hdrs = AMQP::Client::Arguments.new({"x-stream-filter-value" => "foo"})
+    q.publish("msg with filter: foo", props: AMQP::Client::Properties.new(headers: hdrs))
+    hdrs = AMQP::Client::Arguments.new({"x-stream-filter-value" => "bar"})
+    q.publish("msg with filter: bar", props: AMQP::Client::Properties.new(headers: hdrs))
+    hdrs = AMQP::Client::Arguments.new({"x-stream-filter-value" => "foo,bar", "x-foo" => "bar"})
+    q.publish("msg with filter: foo,bar and x-foo=>bar", props: AMQP::Client::Properties.new(headers: hdrs))
+    hdrs = AMQP::Client::Arguments.new({"x-stream-filter-value" => "foo,bar,xyz"})
+    q.publish("msg with filter: foo,bar,xyz", props: AMQP::Client::Properties.new(headers: hdrs))
+    q.publish("msg without filter")
+    q
   end
 end
