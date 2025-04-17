3dbedbec4c130159cd71124043c0db2ed12463b8
Carl Hörberg <carl@84codes.com>
2025-04-17 23:56:09 +0200

    Multi threading for lavinmqperf throughput

--- src/lavinmqperf/throughput.cr
+++ src/lavinmqperf/throughput.cr
@@ -118,17 +118,18 @@ module LavinMQPerf
     def run
       super
 
+      mt = Fiber::ExecutionContext::MultiThreaded.new("Clients", maximum: 20)
       done = WaitGroup.new(@consumers + @publishers)
       @consumers.times do
         if @poll
-          spawn { reconnect_on_disconnect(done) { poll_consume } }
+          mt.spawn { reconnect_on_disconnect(done) { poll_consume } }
         else
-          spawn { reconnect_on_disconnect(done) { consume } }
+          mt.spawn { reconnect_on_disconnect(done) { consume } }
         end
       end
 
       @publishers.times do
-        spawn { reconnect_on_disconnect(done) { pub } }
+        mt.spawn { reconnect_on_disconnect(done) { pub } }
       end
 
       if @timeout != Time::Span.zero
