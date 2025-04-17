967de6c6b6f20061d054a9641a1a78176811fb05
Carl Hörberg <carl@84codes.com>
2025-04-18 00:18:00 +0200

    Remove ACL cache in User and only cache write ACLs in Client
    user.can_write? is the only ACL check in the hot path. By trying to
    eliminate as many locks as possible the ACL check has been moved to the
    Client.
    
    Reorder permissions checks for faster lookups

--- src/lavinmq/reporter.cr
+++ src/lavinmq/reporter.cr
@@ -10,9 +10,6 @@ module LavinMQ
         puts "User #{name}"
         puts_size_capacity user.@tags, 4
         puts_size_capacity user.@permissions, 4
-        puts_size_capacity user.@acl_read_cache, 4
-        puts_size_capacity user.@acl_write_cache, 4
-        puts_size_capacity user.@acl_config_cache, 4
       end
       puts_size_capacity s.@vhosts
       s.vhosts.each do |name, vh|
@@ -35,6 +32,10 @@ module LavinMQ
         vh.connections.each do |c|
           puts "  #{c.name}"
           puts_size_capacity c.channels, 4
+          case c
+          when LavinMQ::AMQP::Client
+            puts_size_capacity c.@acl_cache, 4
+          end
           c.channels.each_value do |ch|
             puts "    #{ch.id} global_prefetch=#{ch.global_prefetch_count} prefetch=#{ch.prefetch_count}"
             puts_size_capacity ch.consumers, 8
