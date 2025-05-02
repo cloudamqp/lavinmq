1a64f951047284085ed57e30d03a8d1d71fa5a3b
Jon Börjesson <jon@84codes.com>
2025-05-02 13:57:30 +0200

    Bugfix: append to x-received-from instead of replacing (#1084)

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -99,6 +99,7 @@ end
 def with_amqp_server(tls = false, replicator = LavinMQ::Clustering::NoopServer.new,
                      config = LavinMQ::Config.instance, & : LavinMQ::Server -> Nil)
   LavinMQ::Config.instance = init_config(config)
+  FileUtils.rm_rf(config.data_dir)
   tcp_server = TCPServer.new("localhost", ENV.has_key?("NATIVE_PORTS") ? 5672 : 0)
   s = LavinMQ::Server.new(config, replicator)
   begin
