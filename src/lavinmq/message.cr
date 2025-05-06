a3b4f8e210df4288ed7ac6d2662a95a8c0247a9d
Viktor Erlingsson <viktor@84codes.com>
2025-05-06 09:42:04 +0200

    refactor

--- src/lavinmq/message.cr
+++ src/lavinmq/message.cr
@@ -37,11 +37,9 @@ module LavinMQ
 
     def self.skip(io, format = IO::ByteFormat::SystemEndian) : UInt64
       skipped = 0_u64
-      skipped += io.skip(sizeof(UInt64))                         # ts
-      skipped += io.skip(io.read_byte || raise IO::EOFError.new) # ex
-      skipped += 1                                               # next pos
-      skipped += io.skip(io.read_byte || raise IO::EOFError.new) # rk
-      skipped += 1                                               # next pos
+      skipped += io.skip(sizeof(UInt64))                             # ts
+      skipped += 1 + io.skip(io.read_byte || raise IO::EOFError.new) # ex
+      skipped += 1 + io.skip(io.read_byte || raise IO::EOFError.new) # rk
       skipped += AMQ::Protocol::Properties.skip(io, format)
       skipped += io.skip(UInt64.from_io io, format) + sizeof(UInt64)
       skipped
