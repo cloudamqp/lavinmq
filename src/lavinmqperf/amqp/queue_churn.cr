36cd8c9467c44c95e0e35daeb36f2700d1bf1744
Christina <christina@84codes.com>
2025-05-07 14:07:01 +0200

    add mqtt-perf

--- src/lavinmqperf/amqp/queue_churn.cr
+++ src/lavinmqperf/amqp/queue_churn.cr
@@ -1,26 +1,28 @@
 require "amqp-client"
 require "benchmark"
-require "./perf"
+require "../perf"
 
 module LavinMQPerf
-  class QueueChurn < Perf
-    def run
-      super
+  module AMQP
+    class QueueChurn < Perf
+      def run
+        super
 
-      r = Random::DEFAULT
-      AMQP::Client.start(@uri) do |c|
-        ch = c.channel
+        r = Random::DEFAULT
+        ::AMQP::Client.start(@uri) do |c|
+          ch = c.channel
 
-        Benchmark.ips do |x|
-          x.report("create/delete transient queue") do
-            q = ch.queue
-          ensure
-            q.delete if q
-          end
-          x.report("create/delete durable queue") do
-            q = ch.queue("queue-churn-durable-#{r.hex(8)}")
-          ensure
-            q.delete if q
+          Benchmark.ips do |x|
+            x.report("create/delete transient queue") do
+              q = ch.queue
+            ensure
+              q.delete if q
+            end
+            x.report("create/delete durable queue") do
+              q = ch.queue("queue-churn-durable-#{r.hex(8)}")
+            ensure
+              q.delete if q
+            end
           end
         end
       end
