3308e702585ec9826a35e15a2d99aead09ee0a35
lukas8219 <lucas.c4d@gmail.com>
2025-05-19 23:24:47 +0200

    removed puts

--- spec/channel_spec.cr
+++ spec/channel_spec.cr
@@ -7,13 +7,11 @@ describe LavinMQ::AMQP::Channel do
       config = LavinMQ::Config.new
       config.consumer_max_per_channel = 1
       with_amqp_server(config: config) do |s|
-        puts "Creating resources"
         connection = AMQP::Client.new(port: amqp_port(s)).connect
         channel = connection.channel
         channel.queue("test:queue:1")
         channel.queue("test:queue:2")
         channel.basic_consume("test:queue:1") {}
-        puts "Consuming Q1"
         begin
           channel.basic_consume("test:queue:2") {}
           fail "Expected NOT_ALLOWED error when trying to surpass consumer_max_per_channel"
