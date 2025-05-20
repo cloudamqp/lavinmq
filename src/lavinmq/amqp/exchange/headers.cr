e003ecd10872720004b1b4cd7a9e1836edf6d3f7
Jon Börjesson <jon@spuun.se>
2025-05-20 08:48:56 +0200

    Dont parse @arguments for each binding

--- src/lavinmq/amqp/exchange/headers.cr
+++ src/lavinmq/amqp/exchange/headers.cr
@@ -62,6 +62,7 @@ module LavinMQ
       end
 
       protected def each_destination(routing_key : String, headers : AMQP::Table?, & : LavinMQ::Destination ->)
+        default_x_match = @arguments["x-match"]?
         @bindings.each do |args, destinations|
           if headers.nil? || headers.empty?
             next unless args.empty?
@@ -69,7 +70,8 @@ module LavinMQ
               yield destination
             end
           else
-            is_match = case args["x-match"]?
+            x_match = args["x-match"]? || default_x_match
+            is_match = case x_match
                        when "any"
                          args.any? { |k, v| !k.starts_with?("x-") && (headers.has_key?(k) && headers[k] == v) }
                        else
