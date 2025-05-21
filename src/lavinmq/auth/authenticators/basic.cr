770984425e08842a5600f1d312aaf8247732db00
Viktor Erlingsson <viktor@84codes.com>
2025-05-21 13:54:06 +0200

    use []? instead of rescuing KeyError

--- src/lavinmq/auth/authenticators/basic.cr
+++ src/lavinmq/auth/authenticators/basic.cr
@@ -8,9 +8,9 @@ module LavinMQ
       end
 
       def authenticate(username : String, password : Bytes) : User?
-        user = @users[username]
-        return user if user && user.password && user.password.not_nil!.verify(password)
-      rescue KeyError # User not found
+        if user = @users[username]?
+          return user if user.password && user.password.not_nil!.verify(password)
+        end
       rescue ex : Exception
         Log.error { "Basic authentication failed: #{ex.message}" }
       end
