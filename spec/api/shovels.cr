45fc9ea45b6a02da80b3be8a688c6035dce597b7
lukas8219 <lucas.c4d@gmail.com>
2025-05-26 20:39:21 +0200

    [wip] directly change the shovel instead of going via vhost parameters

---
+++ spec/api/shovels.cr
@@ -0,0 +1,79 @@
+require "../spec_helper.cr"
+require "uri"
+
+describe LavinMQ::HTTP::ShovelsController do
+
+  describe "PUT api/shovels/:vhost/:pause" do
+    with_http_server do |http, s|
+     body = %({
+        "value": {
+          "src-uri": "#{s.amqp_url}",
+          "dest-uri": "#{s.amqp_url}",
+          "dest-queue":"q1",
+          "src-queue":"q2",
+          "src-prefetch-count":1000,
+          "src-delete-after":"never",
+          "reconnect-delay":5,
+          "ack-mode":"on-confirm"
+        }
+      })
+      response = http.put("/api/parameters/shovel/%2F/name", body: body)
+      response.status_code.should eq 201
+      wait_for {
+        current_state = JSON.parse(http.get("/api/shovels/%2F/name").body)["state"]
+        current_state === "Running"
+      }
+      response = http.put("/api/shovels/%2F/name/pause")
+      response.status_code.should eq 204
+
+      wait_for {
+        current_state = JSON.parse(http.get("/api/shovels/%2F/name").body)["state"]
+        current_state === "Paused"
+      }
+    end
+  end
+  
+  describe "PUT api/shovels/:vhost/:name/resume" do
+    with_http_server do |http, s|
+       vhost_url_encoded = URI.encode_path_segment("/")
+       body = %({
+          "value": {
+            "src-uri": "#{s.amqp_url}",
+            "dest-uri": "#{s.amqp_url}",
+            "dest-queue":"q1",
+            "src-queue":"q2",
+            "src-prefetch-count":1000,
+            "src-delete-after":"never",
+            "reconnect-delay":5,
+            "ack-mode":"on-confirm"
+          }
+        })
+        response = http.put("/api/parameters/shovel/#{vhost_url_encoded}/name", body: body)
+        response.status_code.should eq 201
+        wait_for {
+          current_state = JSON.parse(http.get("/api/shovels/#{vhost_url_encoded}/name").body)["state"]
+          current_state === "Running"
+        }
+        response = http.put("/api/shovels/%2F/name/pause")
+        response.status_code.should eq 204
+
+        wait_for {
+          current_state = JSON.parse(http.get("/api/shovels/#{vhost_url_encoded}/name").body)["state"]
+          current_state === "Paused"
+        }
+        
+        status_code = http.put("/api/shovels/#{vhost_url_encoded}/name/resume").status_code
+        status_code.should eq 204
+
+        wait_for {
+          response = http.get("/api/shovels/#{vhost_url_encoded}/name")
+          response.status_code.should eq 200
+          current_state = JSON.parse(response.body)["state"]?
+          current_state === "Running"
+        }
+        
+      end
+  end
+
+end
+
--- src/lavinmq/http/controller/shovels.cr
+++ src/lavinmq/http/controller/shovels.cr
@@ -4,7 +4,6 @@ module LavinMQ
     class  ShovelsController < Controller
       include StatsHelpers
 
-
       private def register_routes
         get "/api/shovels" do |context, _params|
           itrs = vhosts(user(context)).flat_map do |v|
@@ -19,26 +18,46 @@ module LavinMQ
           end
         end
 
+        get "/api/shovels/:vhost/:name" do |context, params|
+          with_vhost(context, params) do |vhost|
+            shovel_name = params["name"]
+            if shovel = @amqp_server.vhosts[vhost].shovels[shovel_name]?
+              shovel.to_json(context.response)
+            else
+              context.response.status_code = 404
+            end
+          end
+        end
+
         put "/api/shovels/:vhost/:name/pause" do |context, params|
           with_vhost(context, params) do |vhost|
-            name = params["name"]
-            oldParameter = @amqp_server.vhosts[vhost].parameters[{"shovel", name}]
-            oldParameter.value.as_h["state"] = JSON::Any.new("Paused")
-            @amqp_server.vhosts[vhost].add_parameter(oldParameter)
-            shovels = @amqp_server.vhosts[vhost].shovels[name]
-            shovels.to_json(context.response)
+            shovel_name = params["name"]
+            if current_shovel = @amqp_server.vhosts[vhost].shovels[shovel_name]?
+              if ! current_shovel.running?
+                context.response.status_code = 422
+                next
+              end
+              current_shovel.pause
+              context.response.status_code = 204
+            else
+              context.response.status_code = 404
+            end
           end
         end
 
         #TODO 911: found a bug when we restart the LMQ instance - shovel gets back to running
         put "/api/shovels/:vhost/:name/resume" do |context, params|
           with_vhost(context, params) do |vhost|
-            name = params["name"]
-            oldParameter = @amqp_server.vhosts[vhost].parameters[{"shovel", name}]
-            oldParameter.value.as_h["state"] = JSON::Any.new("Running")
-            @amqp_server.vhosts[vhost].add_parameter(oldParameter)
-            shovels = @amqp_server.vhosts[vhost].shovels[name]
-            shovels.to_json(context.response)
+            shovel_name = params["name"]
+            if current_shovel = @amqp_server.vhosts[vhost].shovels[shovel_name]?
+              if ! current_shovel.paused?
+                context.response.status_code = 422
+              end
+              current_shovel.resume
+              context.response.status_code = 204
+            else
+              context.response.status_code = 404
+            end
           end
         end
       end
