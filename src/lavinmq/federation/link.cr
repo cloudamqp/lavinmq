6901719fda6a598b4a6445e969ad68c733421747
Jon Börjesson <jon@spuun.se>
2025-05-28 09:49:56 +0200

    Remove unused code

--- src/lavinmq/federation/link.cr
+++ src/lavinmq/federation/link.cr
@@ -417,7 +417,6 @@ module LavinMQ
           hops = get_binding_hops(bound_from)
           return {false, arguments} if hops == 0
           bound_from.unshift AMQP::Table.new({
-            # "cluster-name": "dunno",
             "vhost":    @upstream.vhost.name,
             "exchange": @federated_ex.name,
             "hops":     hops,
