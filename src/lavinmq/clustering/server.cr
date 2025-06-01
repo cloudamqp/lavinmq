13ec269983485d3dd55f890fdf7f7f22b9d2f1ab
Carl Hörberg <carl@84codes.com>
2025-06-01 02:26:36 +0200

    Use more refined synchronization primitives
    Use the sync library by @ysbaddaden with nsync inspired synchronization
    primitives.
    
    First @followers was protected using a RWLock, yielding a big
    performance improvment, around 30% higher throughput, when clustering is
    enabled.

--- src/lavinmq/clustering/server.cr
+++ src/lavinmq/clustering/server.cr
@@ -9,6 +9,7 @@ require "../mfile"
 require "crypto/subtle"
 require "lz4"
 require "../etcd"
+require "sync/shared"
 
 module LavinMQ
   module Clustering
@@ -27,20 +28,19 @@ module LavinMQ
       include Replicator
       Log = LavinMQ::Log.for "clustering.server"
 
-      @lock = Mutex.new(:unchecked)
-      @followers = Array(Follower).new(4)
+      @followers : Sync::Shared(Array(Follower)) = Sync::Shared.new(Array(Follower).new(4), :unchecked)
       @password : String
       @files = Hash(String, MFile?).new
-      @dirty_isr = true
+      @dirty_isr = Atomic(Bool).new true
       @id : Int32
       @config : Config
 
-      def initialize(config : Config, @etcd : Etcd, @id : Int32)
+      def initialize(@config : Config, @etcd : Etcd, @id : Int32)
         Log.info { "ID: #{@id.to_s(36)}" }
-        @config = config
         @data_dir = @config.data_dir
         @password = password
         @checksums = Checksums.new(@data_dir)
+        @isr_key = "#{@config.clustering_etcd_prefix}/isr"
       end
 
       def clear
@@ -122,9 +122,7 @@ module LavinMQ
       end
 
       def followers : Array(Follower)
-        @lock.synchronize do
-          @followers.dup # for thread safety
-        end
+        @followers.read &.dup
       end
 
       def password : String
@@ -157,23 +155,22 @@ module LavinMQ
           Log.error { "Disconnecting follower with the clustering id of the leader" }
           return
         end
-        if stale_follower = @followers.find { |f| f.id == follower.id }
+        if stale_follower = @followers.read &.find { |f| f.id == follower.id }
           Log.error { "Disconnecting stale follower with id #{follower.id.to_s(36)}" }
           stale_follower.close
         end
         follower.full_sync # sync the bulk
-        @lock.synchronize do
+        @followers.write do |followers|
           follower.full_sync # sync the last
-          @followers << follower
-          update_isr
+          followers << follower
+          update_isr(followers)
+          @dirty_isr.set false, :relaxed
         end
         begin
           follower.action_loop
         ensure
-          @lock.synchronize do
-            @followers.delete(follower)
-            @dirty_isr = true
-          end
+          @followers.write &.delete(follower)
+          @dirty_isr.set true, :relaxed
         end
       rescue ex : AuthenticationError
         Log.warn { "Follower negotiation error" }
@@ -187,36 +184,34 @@ module LavinMQ
         follower.try &.close
       end
 
-      private def update_isr
-        isr_key = "#{@config.clustering_etcd_prefix}/isr"
-        ids = String.build do |str|
-          @followers.each { |f| f.id.to_s(str, 36); str << "," }
+      private def update_isr(followers)
+        ids = String.build(7 * (followers.size + 1)) do |str|
+          followers.each { |f| f.id.to_s(str, 36); str << "," }
           @id.to_s(str, 36)
         end
         Log.info { "In-sync replicas: #{ids}" }
-        @etcd.put(isr_key, ids)
-        @dirty_isr = false
+        @etcd.put(@isr_key, ids)
       end
 
       def close
         @listeners.each &.close
-        @lock.synchronize do
-          @followers.each &.close
-          @followers.clear
-        end
+        @followers.write &.reject! { |f| f.close; true }
         Fiber.yield # required for follower/listener fibers to actually finish
         @checksums.store
       end
 
       private def each_follower(& : Follower -> Nil) : Nil
-        @lock.synchronize do
-          update_isr if @dirty_isr
-          @followers.each do |f|
+        # remove stale follower from ISR set as new changes are coming
+        any_closed = false
+        @followers.read do |followers|
+          update_isr(followers) if @dirty_isr.swap(false, :relaxed)
+          followers.each do |f|
             yield f
           rescue Channel::ClosedError
-            Fiber.yield # Allow other fiber to run to remove the follower from array
+            any_closed = true
           end
         end
+        Fiber.yield if any_closed # let other fibers run to remove the follower
       end
 
       private def strip_datadir(path : String) : String
