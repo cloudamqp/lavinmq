499b8fa08244cbfb5f8fa6ab4fffd1bd7a2c9756
Christina <christina@84codes.com>
2025-06-04 09:42:03 +0200

    add mqtt-perf

--- src/lavinmqperf/amqp/bind_churn.cr
+++ src/lavinmqperf/amqp/bind_churn.cr
@@ -1,28 +1,30 @@
 require "amqp-client"
 require "benchmark"
-require "./perf"
+require "../perf"
 
 module LavinMQPerf
-  class BindChurn < Perf
-    def run
-      super
+  module AMQP
+    class BindChurn < Perf
+      def run
+        super
 
-      r = Random::DEFAULT
-      AMQP::Client.start(@uri) do |c|
-        ch = c.channel
-        temp_q = ch.queue
-        durable_q = ch.queue("bind-churn-durable-#{r.hex(8)}")
+        r = Random::DEFAULT
+        ::AMQP::Client.start(@uri) do |c|
+          ch = c.channel
+          temp_q = ch.queue
+          durable_q = ch.queue("bind-churn-durable-#{r.hex(8)}")
 
-        Benchmark.ips do |x|
-          x.report("bind non-durable queue") do
-            temp_q.bind "amq.direct", r.hex(16)
+          Benchmark.ips do |x|
+            x.report("bind non-durable queue") do
+              temp_q.bind "amq.direct", r.hex(16)
+            end
+            x.report("bind durable queue") do
+              durable_q.bind "amq.direct", r.hex(10)
+            end
           end
-          x.report("bind durable queue") do
-            durable_q.bind "amq.direct", r.hex(10)
-          end
+        ensure
+          durable_q.delete if durable_q
         end
-      ensure
-        durable_q.delete if durable_q
       end
     end
   end
--- src/lavinmqperf/amqp/connection_churn.cr
+++ src/lavinmqperf/amqp/connection_churn.cr
@@ -1,17 +1,19 @@
 require "amqp-client"
 require "benchmark"
-require "./perf"
+require "../perf"
 
 module LavinMQPerf
-  class ConnectionChurn < Perf
-    def run
-      super
-      c = AMQP::Client.new(@uri)
-      Benchmark.ips do |x|
-        x.report("open-close connection and channel") do
-          conn = c.connect
-          conn.channel
-          conn.close
+  module AMQP
+    class ConnectionChurn < Perf
+      def run
+        super
+        c = ::AMQP::Client.new(@uri)
+        Benchmark.ips do |x|
+          x.report("open-close connection and channel") do
+            conn = c.connect
+            conn.channel
+            conn.close
+          end
         end
       end
     end
