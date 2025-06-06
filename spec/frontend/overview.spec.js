d0674f66e3b274cfc57b6757acbdc588e07a8ff2
Jon Börjesson <jon@spuun.se>
2025-06-06 16:36:06 +0200

    Add spec to verify single vhost definition export

--- spec/frontend/overview.spec.js
+++ spec/frontend/overview.spec.js
@@ -8,12 +8,22 @@ test.describe("overview", _ => {
     await expect(apiOverviewRequest).toBeRequested()
   })
 
-  test('definitions export trigger GET to /api/definitions', async ({ page, baseURL }) => {
+  test('definitions export trigger GET to /api/definitions for all vhosts', async ({ page, baseURL }) => {
     const definitionsRequest  = helpers.waitForPathRequest(page, '/api/definitions')
     await page.goto('/')
     await page.locator('#exportDefinitions').getByRole('button', { name: /download/i }).click()
     await expect(definitionsRequest).toBeRequested()
   })
+
+  test('definitions export trigger GET to /api/definitions/<selected vhost>', async ({ page, baseURL }) => {
+    const definitionsRequest  = helpers.waitForPathRequest(page, '/api/definitions/foo')
+    await page.goto('/')
+    await page.locator('#exportDefinitions').getByRole('combobox').selectOption('foo')
+    await page.locator('#exportDefinitions').getByRole('button', { name: /download/i }).click()
+    await expect(definitionsRequest).toBeRequested()
+  })
+
+
 
   test('definitions import trigger POST to /api/definitions/upload', async ({ page, baseURL }) => {
     const definitionsRequest = helpers.waitForPathRequest(page, '/api/definitions/upload', { method: 'POST' })
