6965336abebec090f213f23a730c9381b11c955d
Jon Börjesson <jon@spuun.se>
2025-06-11 08:25:56 +0200

    Use vhostLoaded fixture

--- spec/frontend/fixtures.js
+++ spec/frontend/fixtures.js
@@ -1,13 +1,27 @@
+import * as helpers from './helpers.js'
 import { test as base, expect } from "@playwright/test";
 
 const vhosts = ['foo', 'bar']
+const vhostResponse = vhosts.map(x => { return {name: x} })
+
 const test = base.extend(
   {
     vhosts,
-    page: async ({ baseURL, page }, use) => {
-      const vhostResponse = vhosts.map(x => { return {name: x} })
-      await page.route(/\/api\/vhosts(\?|$)/, async route => await route.fulfill({ json: vhostResponse }))
-      use(page)
+    vhostsLoaded: async({ page }, use) => {
+      const ret = new Promise(async (resolve, reject) => {
+        // Wait for vhosts to be loaded and rendered
+        const vhostCombobox = page.locator('#userMenuVhost')
+        // + 1 for All
+        await expect(vhostCombobox.locator('option')).toHaveCount(vhostResponse.length + 1)
+        resolve(vhosts)
+      })
+      await use(ret)
+    },
+    page: async ({ page }, use) => {
+      await page.route(/.*\/api\/vhosts(\?.*)?$/, async route => {
+        await route.fulfill({ json: vhostResponse })
+      })
+      await use(page);
     },
     // Use to map api requests to responses
     apimap: async({ page }, use) => {
@@ -35,7 +49,7 @@ const test = base.extend(
           return map('POST', path, response)
         },
        }
-      use(apiMap)
+      await use(apiMap)
     }
   }
 )
--- spec/frontend/overview.spec.js
+++ spec/frontend/overview.spec.js
@@ -2,30 +2,31 @@ import * as helpers from './helpers.js'
 import { test, expect } from './fixtures.js';
 
 test.describe("overview", _ => {
-  test('are loaded', async ({ page, baseURL }) => {
+  test('are loaded', async ({ page }) => {
     const apiOverviewRequest = helpers.waitForPathRequest(page, '/api/overview')
     await page.goto('/')
     await expect(apiOverviewRequest).toBeRequested()
   })
 
-  test('definitions export trigger GET to /api/definitions for all vhosts', async ({ page, baseURL }) => {
+  test('definitions export trigger GET to /api/definitions for all vhosts', async ({ page }) => {
     const definitionsRequest  = helpers.waitForPathRequest(page, '/api/definitions')
     await page.goto('/')
     await page.locator('#exportDefinitions').getByRole('button', { name: /download/i }).click()
     await expect(definitionsRequest).toBeRequested()
   })
 
-  test('definitions export trigger GET to /api/definitions/<selected vhost>', async ({ page, baseURL }) => {
-    const definitionsRequest  = helpers.waitForPathRequest(page, '/api/definitions/foo')
+  test('definitions export trigger GET to /api/definitions/<selected vhost>', async ({ page, vhostsLoaded }) => {
     await page.goto('/')
-    await page.locator('#exportDefinitions').getByRole('combobox').selectOption('foo')
+    const vhosts = await vhostsLoaded
+    const definitionsRequest  = helpers.waitForPathRequest(page, `/api/definitions/${vhosts[0]}`)
+    await page.locator('#exportDefinitions').getByRole('combobox').selectOption(vhosts[0])
     await page.locator('#exportDefinitions').getByRole('button', { name: /download/i }).click()
     await expect(definitionsRequest).toBeRequested()
   })
 
 
 
-  test('definitions import trigger POST to /api/definitions/upload', async ({ page, baseURL }) => {
+  test('definitions import trigger POST to /api/definitions/upload', async ({ page }) => {
     const definitionsRequest = helpers.waitForPathRequest(page, '/api/definitions/upload', { method: 'POST' })
     await page.goto('/')
 
