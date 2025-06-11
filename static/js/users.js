81f4e7b32cd8e7b4cd06e7dc466dbdebfff64158
Christina <christina@84codes.com>
2025-06-11 16:48:22 +0200

    lint javascript

--- static/js/users.js
+++ static/js/users.js
@@ -62,17 +62,17 @@ document.querySelector('#dataTags').onclick = e => {
   Helpers.argumentHelper('createUser', 'tags', e)
 }
 
-document.querySelector('#generatePassword').addEventListener('click', function() {
+document.querySelector('#generatePassword').addEventListener('click', function () {
   const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?'
-  const password = Array.from(crypto.getRandomValues(new Uint8Array(16)), x => chars[x % chars.length]).join('')
+  const password = Array.from(window.crypto.getRandomValues(new Uint8Array(16)), x => chars[x % chars.length]).join('')
   const input = document.querySelector('#createUser input[name="password"]')
 
   input.value = password
   input.type = 'text'
-  setTimeout(() => input.type = 'password', 500)
+  setTimeout(() => { input.type = 'password' }, 500)
 })
 
-document.querySelector('.password-toggle').addEventListener('click', function() {
+document.querySelector('.password-toggle').addEventListener('click', function () {
   const input = document.querySelector('#createUser input[name="password"]')
   const isPassword = input.type === 'password'
   input.type = isPassword ? 'text' : 'password'
