0b25e675e246e8aee2397d8b3253708201960784
Carl Hörberg <carl@84codes.com>
2025-06-26 15:38:57 +0200

    delete orphan acks files on start and shutdown (#1175)
    This PR introduces a mechanism to automatically delete orphan files (those without a corresponding file) during both the startup and shutdown sequences of the server. This prevents the potential issues arising from stale or irrelevant acknowledgment data.

---
+++ spec/message_store_spec.cr
@@ -0,0 +1,33 @@
+require "./spec_helper"
+require "file_utils"
+require "time"
+require "../src/lavinmq/message_store"
+
+def mktmpdir(&)
+  path = File.tempname
+  Dir.mkdir_p(path)
+  begin
+    yield path
+  ensure
+    FileUtils.rm_r(path)
+  end
+end
+
+describe LavinMQ::MessageStore do
+  it "deletes orphaned ack files" do
+    mktmpdir do |dir|
+      # Create a dummy msgs file
+      File.write(File.join(dir, "msgs.0000000001"), "")
+      # Create a corresponding acks file
+      File.write(File.join(dir, "acks.0000000001"), "data")
+      # Create an orphaned acks file
+      File.write(File.join(dir, "acks.0000000002"), "data")
+
+      store = LavinMQ::MessageStore.new(dir, nil)
+      store.close
+
+      File.exists?(File.join(dir, "acks.0000000001")).should be_true
+      File.exists?(File.join(dir, "acks.0000000002")).should be_false
+    end
+  end
+end
