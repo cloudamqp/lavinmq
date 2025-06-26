5e16584b1d4246b8de36af919aa9b08b38ec34c2
Christina <christina@84codes.com>
2025-06-26 15:49:58 +0200

    wip temp user

--- src/lavinmq/auth/authenticators/oauth.cr
+++ src/lavinmq/auth/authenticators/oauth.cr
@@ -1,17 +1,24 @@
 require "../authenticator"
+require "../users/temp_user"
 require "../../server"
 
 module LavinMQ
   module Auth
     class OAuthAuthenticator < Authenticator
-      def initialize(@users : UserStore)
+      def initialize(@users : Auth::UserStore)
       end
 
-      def authenticate(username : String, password : Bytes) : User?
-        pp "oauth"
-        if user = @users[username]?
-          return user
+      def authenticate(username : String, password : Bytes) : Users::TempUser?
+
+        #Todo: send in JWT token and verify it,
+        # parse that body and return a TempUser
+
+        if user = @users[username]? # instead: if token is valid
+          temp_user = Users::TempUser.new
+          temp_user.set_expiration(Time.utc + 3600.milliseconds) # Set expiration to 1 hour
+          return temp_user
         end
+
       rescue ex : Exception
         Log.error { "Oauth authentication failed: #{ex.message}" }
       end
