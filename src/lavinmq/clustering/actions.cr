199eaf71ccbe2e50f9cd913df2969fcfae90f266
Carl Hörberg <carl@84codes.com>
2025-06-27 15:13:46 +0200

    ReplaceAction will handle underlaying file being replaced again (#1179)
    * ReplaceAction will handle underlaying file being replaced again
    
    When a file replacement is replicated using ReplaceAction, there was a
    bug that if the file was replaced again by the time the ReplaceAction
    was sent to a slow follower, the filesize could mismatch. This
    fixes it by opening the File when the ReplaceAction is constructed.
    
    In LavinMQ replaced files are always written to a tempfile file and then
    renamed, the content of them are not modified.
    
    * Add spec to verify original data is sent even though it has been appended to
    
    ---------
    
    Co-authored-by: Jon Börjesson <jon@spuun.se>

--- src/lavinmq/clustering/actions.cr
+++ src/lavinmq/clustering/actions.cr
@@ -19,39 +19,27 @@ module LavinMQ
     end
 
     struct ReplaceAction < Action
-      def initialize(@data_dir : String, @filename : String, @mfile : MFile? = nil)
+      def initialize(@data_dir : String, @filename : String)
+        @file = File.new(File.join(@data_dir, @filename))
+        @file_size = @file.size
       end
 
       def lag_size : Int64
-        if mfile = @mfile
-          0i64 + sizeof(Int32) + @filename.bytesize +
-            sizeof(Int64) + mfile.size.to_i64
-        else
-          0i64 + sizeof(Int32) + @filename.bytesize +
-            sizeof(Int64) + File.size(File.join(@data_dir, @filename)).to_i64
-        end
+        0i64 + sizeof(Int32) + @filename.bytesize + sizeof(Int64) + @file_size
       end
 
       def send(socket, log = Log) : Int64
         log.debug { "Replace #{@filename}" }
         send_filename(socket)
-        if mfile = @mfile
-          size = mfile.size.to_i64
-          socket.write_bytes size, IO::ByteFormat::LittleEndian
-          socket.write mfile.to_slice(0, size)
-          mfile.dontneed
-          size
-        else
-          File.open(File.join(@data_dir, @filename)) do |f|
-            size = f.size.to_i64
-            socket.write_bytes size, IO::ByteFormat::LittleEndian
-            IO.copy(f, socket, size) == size || raise IO::EOFError.new
-            size
-          end
-        end
+        socket.write_bytes @file_size.to_i64, IO::ByteFormat::LittleEndian
+        IO.copy(@file, socket, @file_size) == @file_size || raise IO::EOFError.new
+        @file_size
+      ensure
+        done
       end
 
       def done
+        @file.close
       end
     end
 
