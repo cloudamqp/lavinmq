cb55963c3cfe0fbd7cbc69efd8427af0a2be9a42
Christina <christina@84codes.com>
2025-08-08 14:35:31 +0200

    explain test file for oauth temp user example

--- src/lavinmq/auth/authenticators/oauth.cr
+++ src/lavinmq/auth/authenticators/oauth.cr
@@ -2,8 +2,9 @@ require "../authenticator"
 require "../users/temp_user"
 require "../../server"
 
+#
 # This file is only used for testing purposes, it is not going to be included in this pull request.
-
+#
 module LavinMQ
   module Auth
     class OAuthAuthenticator < Authenticator
@@ -11,8 +12,8 @@ module LavinMQ
       end
 
       def authenticate(username : String, password : Bytes) : Users::TempUser?
-        if user = @users[username]? # instead: if token is valid
-          temp_user = Users::TempUser.new(username, user.tags, user.permissions, Time.utc + 5000.milliseconds)
+        if user = @users[username]? # When this authenticator is implemented it will check: if token is valid
+          temp_user = Users::TempUser.new(username, user.tags, user.permissions, Time.utc + 5000.milliseconds) # get expiration time from JWT token
           return temp_user
         end
       rescue ex : Exception
