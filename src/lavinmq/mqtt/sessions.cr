776bcf5eabddf401a80e0c57b6bc7de6b8ca0e0e
Carl Hörberg <carl@84codes.com>
2025-08-17 22:30:09 +0200

    optimize mqtt sessions

--- src/lavinmq/mqtt/sessions.cr
+++ src/lavinmq/mqtt/sessions.cr
@@ -3,7 +3,10 @@ require "../vhost"
 
 module LavinMQ
   module MQTT
+    # Just a wrapper around the vhost queues
     class Sessions
+      QueueArguments = AMQP::Table.new({"x-queue-type": "mqtt"})
+
       def initialize(@vhost : VHost)
       end
 
@@ -15,11 +18,14 @@ module LavinMQ
         @vhost.queue("mqtt.#{client_id}").as(Session)
       end
 
-      def declare(client : Client)
-        self[client.client_id]? || begin
-          @vhost.declare_queue("mqtt.#{client.client_id}", !client.@clean_session, client.@clean_session, AMQP::Table.new({"x-queue-type": "mqtt"}))
-          self[client.client_id].client = client
-          self[client.client_id]
+      def declare(client : Client) : Session
+        if q = @vhost.queue("mqtt.#{client.client_id}")
+          q.as(Session)
+        else
+          @vhost.declare_queue("mqtt.#{client.client_id}", !client.clean_session?, client.clean_session?, QueueArguments)
+          session = @vhost.queue("mqtt.#{client.client_id}").as(Session)
+          session.client = client
+          session
         end
       end
 
