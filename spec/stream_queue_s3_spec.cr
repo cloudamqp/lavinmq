0695b5f4513ca5ca943df92e772c02cdae6d29bb
Viktor Erlingsson <viktor@84codes.com>
2025-08-21 15:57:53 +0200

    simplify specs by using real consumers

--- spec/stream_queue_s3_spec.cr
+++ spec/stream_queue_s3_spec.cr
@@ -1,7 +1,8 @@
 require "./spec_helper"
 require "./../src/lavinmq/amqp/queue/stream_s3_message_store"
-
-class S3SpecHelper
+DATA_DIR = "42099b4af021e53fd8fd4e056c2568d7c2e3ffa8/77d9712623c4368721b466d1c24d447e9c53c8d3"
+module S3SpecHelper
+  class_property responses : Hash(String, String | Bytes) = S3SpecHelper.setup_responses
   def self.meta_bytes(offset)
     io = IO::Memory.new
     io.write_bytes 100_u32 # count
@@ -28,11 +29,11 @@ class S3SpecHelper
   def self.setup_responses : Hash(String, String | Bytes)
     {
       "?delimiter=%2F&encoding-type=url&list-type=2&prefix=" => RESPONSE_FILE_LIST,
-      "/test_queue/msgs.0000000001"                          => S3SpecHelper.segment_bytes(0_i64),
-      "/test_queue/msgs.0000000002"                          => S3SpecHelper.segment_bytes(100_i64),
-      "/test_queue/msgs.0000000001.meta"                     => S3SpecHelper.meta_bytes(0_i64),
-      "/test_queue/msgs.0000000002.meta"                     => S3SpecHelper.meta_bytes(100_i64),
-      "/tmp/lavinmq-spec/test_queue"                         => RESPONSE_UPLOAD,
+      "/#{DATA_DIR}/msgs.0000000001"                          => S3SpecHelper.segment_bytes(0_i64),
+      "/#{DATA_DIR}/msgs.0000000002"                          => S3SpecHelper.segment_bytes(100_i64),
+      "/#{DATA_DIR}/msgs.0000000001.meta"                     => S3SpecHelper.meta_bytes(0_i64),
+      "/#{DATA_DIR}/msgs.0000000002.meta"                     => S3SpecHelper.meta_bytes(100_i64),
+      "/tmp/lavinmq-spec#{DATA_DIR}"                         => RESPONSE_UPLOAD,
     }
   end
 end
@@ -41,22 +42,22 @@ RESPONSE_FILE_LIST = <<-XML
           <?xml version="1.0" encoding="UTF-8"?>
           <ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
             <Contents>
-              <Key>test_queue/msgs.0000000001</Key>
+              <Key>#{DATA_DIR}/msgs.0000000001</Key>
               <ETag>"abc123"</ETag>
               <Size>5647</Size>
             </Contents>
             <Contents>
-              <Key>test_queue/msgs.0000000002</Key>
+              <Key>#{DATA_DIR}/msgs.0000000002</Key>
               <ETag>"def456"</ETag>
               <Size>5647</Size>
             </Contents>
             <Contents>
-              <Key>test_queue/msgs.0000000001.meta</Key>
+              <Key>#{DATA_DIR}/msgs.0000000001.meta</Key>
               <ETag>"meta123"</ETag>
               <Size>20</Size>
             </Contents>
             <Contents>
-              <Key>test_queue/msgs.0000000002.meta</Key>
+              <Key>#{DATA_DIR}/msgs.0000000002.meta</Key>
               <ETag>"meta456"</ETag>
               <Size>20</Size>
             </Contents>
@@ -65,8 +66,6 @@ RESPONSE_FILE_LIST = <<-XML
 RESPONSE_UPLOAD = ""
 
 class MockS3HTTPClient < HTTP::Client
-  property responses = Hash(String, String | Bytes).new
-
   def initialize(hostname)
     super(hostname, 443, tls: true)
   end
@@ -90,9 +89,9 @@ class MockS3HTTPClient < HTTP::Client
   def match_response(path, method = "GET") : HTTP::Client::Response
     resp = ""
     if path.starts_with?("?delimiter=%2F&encoding-type=url&list-type=2&prefix=")
-      resp = @responses.first_value
+      resp = S3SpecHelper.responses.first_value
     else
-      @responses.each do |key, response|
+      S3SpecHelper.responses.each do |key, response|
         if path == key
           resp = response
         end
@@ -112,17 +111,10 @@ class MockS3HTTPClient < HTTP::Client
   end
 end
 
-# Mock HTTP client for S3 specs
-class MockStreamS3MessageStore < LavinMQ::AMQP::StreamQueue::StreamS3MessageStore
-  @test_responses = Hash(String, String | Bytes).new
-
-  def initialize(@msg_dir : String, @test_responses : Hash(String, String | Bytes) = S3SpecHelper.setup_responses)
-    super(@msg_dir, nil, true, ::Log::Metadata.empty)
-  end
-
+# Mock S3 storage client that uses the mock HTTP client
+class LavinMQ::AMQP::S3StorageClient
   def http_client(with_timeouts = false)
     h = MockS3HTTPClient.new("test.lavinmq.com")
-    h.responses = @test_responses
 
     if signer = s3_signer
       signer.sign(::HTTP::Request.new("GET", "test.lavinmq.com"))
@@ -130,8 +122,8 @@ class MockStreamS3MessageStore < LavinMQ::AMQP::StreamQueue::StreamS3MessageStor
       raise "No S3 signer found"
     end
     if with_timeouts
-      h.connect_timeout = HTTP_CONNECT_TIMEOUT
-      h.read_timeout = HTTP_READ_TIMEOUT
+      h.connect_timeout = 200.milliseconds
+      h.read_timeout = 500.milliseconds
     end
     h.as(::HTTP::Client)
   end
@@ -139,20 +131,24 @@ end
 
 describe LavinMQ::AMQP::StreamQueue::StreamS3MessageStore do
   before_each do
+    LavinMQ::Config.instance.streams_s3_storage = true
+    LavinMQ::Config.instance.streams_s3_storage_bucket = "test_bucket"
+    LavinMQ::Config.instance.streams_s3_storage_endpoint = "test.lavinmq.com"
     LavinMQ::Config.instance.streams_s3_storage_region = "us-east-1"
     LavinMQ::Config.instance.streams_s3_storage_access_key_id = "test_access_key"
     LavinMQ::Config.instance.streams_s3_storage_secret_access_key = "test_secret_key"
   end
 
   after_each do
-    FileUtils.rm_rf("/tmp/lavinmq-spec/test_queue")
+    FileUtils.rm_rf("/tmp/lavinmq-spec/#{DATA_DIR}")
   end
 
   it "should get file list from s3" do
-    msg_dir = "/tmp/lavinmq-spec/test_queue"
+    msg_dir = "/tmp/lavinmq-spec/#{DATA_DIR}"
     FileUtils.rm_rf(msg_dir)
     Dir.mkdir_p(msg_dir)
-    msg_store = MockStreamS3MessageStore.new(msg_dir, S3SpecHelper.setup_responses)
+    S3SpecHelper.responses = S3SpecHelper.setup_responses
+    msg_store = LavinMQ::AMQP::StreamQueue::StreamS3MessageStore.new(msg_dir, nil, true, ::Log::Metadata.empty)
 
     msg_store.@s3_segments.size.should eq 2
     msg_store.@segments.size.should eq 2
@@ -160,11 +156,12 @@ describe LavinMQ::AMQP::StreamQueue::StreamS3MessageStore do
   end
 
   it "should read & upload local file" do
-    msg_dir = "/tmp/lavinmq-spec/test_queue"
+    msg_dir = "/tmp/lavinmq-spec/#{DATA_DIR}"
     FileUtils.rm_rf(msg_dir)
     Dir.mkdir_p(msg_dir)
     File.write(File.join(msg_dir, "msgs.0000000003"), S3SpecHelper.segment_bytes(200_i64))
-    msg_store = MockStreamS3MessageStore.new(msg_dir, S3SpecHelper.setup_responses)
+    S3SpecHelper.responses = S3SpecHelper.setup_responses
+    msg_store = LavinMQ::AMQP::StreamQueue::StreamS3MessageStore.new(msg_dir, nil, true, ::Log::Metadata.empty)
 
     msg_store.@s3_segments.size.should eq 3
     msg_store.@segments.size.should eq 3
@@ -173,22 +170,28 @@ describe LavinMQ::AMQP::StreamQueue::StreamS3MessageStore do
 
   # Starting with empty data dir, download files from s3 and consume
   it "can consume from s3" do
-    msg_dir = "/tmp/lavinmq-spec/test_queue"
+    msg_dir = "/tmp/lavinmq-spec/#{DATA_DIR}"
     FileUtils.rm_rf(msg_dir)
     Dir.mkdir_p(msg_dir)
-    msg_store = MockStreamS3MessageStore.new(msg_dir, S3SpecHelper.setup_responses)
-
-    consumer = MockStreamConsumer.new(0_i64, 0_u32, 0)
-    if env = msg_store.shift?(consumer)
-      String.new(env.message.body).should eq "body"
-    else
-      fail "Expected to get a message from S3"
+    with_amqp_server do |s|
+      with_channel(s) do |ch|
+        q_name = "test_queue"
+        ch.prefetch 1
+        q_args = LavinMQ::AMQP::Table.new({"x-queue-type": "stream"})
+        q = ch.queue(q_name, durable: true, args: q_args)
+        channel = Channel(String).new
+        q.subscribe(no_ack: false) do |msg|
+          channel.send msg.body_io.to_s
+          ch.basic_ack(msg.delivery_tag)
+        end
+        channel.receive.should eq "body"
+      end
     end
   end
 
   describe "no meta files" do
     it "should verify local segments" do
-      msg_dir = "/tmp/lavinmq-spec/test_queue"
+      msg_dir = "/tmp/lavinmq-spec/#{DATA_DIR}"
       FileUtils.rm_rf(msg_dir)
       Dir.mkdir_p(msg_dir)
       File.write(File.join(msg_dir, "msgs.0000000001"), S3SpecHelper.segment_bytes(0_i64))
@@ -204,12 +207,12 @@ describe LavinMQ::AMQP::StreamQueue::StreamS3MessageStore do
           <?xml version="1.0" encoding="UTF-8"?>
           <ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
             <Contents>
-              <Key>test_queue/msgs.0000000001</Key>
+              <Key>#{DATA_DIR}/msgs.0000000001</Key>
               <ETag>"#{etag1}"</ETag>
               <Size>5647</Size>
             </Contents>
             <Contents>
-              <Key>test_queue/msgs.0000000002</Key>
+              <Key>#{DATA_DIR}/msgs.0000000002</Key>
               <ETag>"#{etag2}"</ETag>
               <Size>5647</Size>
             </Contents>
@@ -217,11 +220,12 @@ describe LavinMQ::AMQP::StreamQueue::StreamS3MessageStore do
           XML
       responses = {
         "?delimiter=%2F&encoding-type=url&list-type=2&prefix=" => file_list,
-        "/test_queue/msgs.0000000001"                          => "foo".to_slice,
-        "/test_queue/msgs.0000000002"                          => "bar".to_slice,
-        "/tmp/lavinmq-spec/test_queue"                         => RESPONSE_UPLOAD,
+        "/#{DATA_DIR}/msgs.0000000001"                          => "foo".to_slice,
+        "/#{DATA_DIR}/msgs.0000000002"                          => "bar".to_slice,
+        "/tmp/lavinmq-spec/#{DATA_DIR}"                         => RESPONSE_UPLOAD,
       }
-      msg_store = MockStreamS3MessageStore.new(msg_dir, responses)
+      S3SpecHelper.responses = responses
+      msg_store = LavinMQ::AMQP::StreamQueue::StreamS3MessageStore.new(msg_dir, nil, true, ::Log::Metadata.empty)
 
       msg_store.@s3_segments.size.should eq 2
       msg_store.@segments.size.should eq 3
@@ -235,7 +239,7 @@ describe LavinMQ::AMQP::StreamQueue::StreamS3MessageStore do
     end
 
     it "should read local segments" do
-      msg_dir = "/tmp/lavinmq-spec/test_queue"
+      msg_dir = "/tmp/lavinmq-spec/#{DATA_DIR}"
       FileUtils.rm_rf(msg_dir)
       Dir.mkdir_p(msg_dir)
       File.write(File.join(msg_dir, "msgs.0000000001"), S3SpecHelper.segment_bytes(0_i64))
@@ -247,11 +251,12 @@ describe LavinMQ::AMQP::StreamQueue::StreamS3MessageStore do
           XML
       responses = {
         "?delimiter=%2F&encoding-type=url&list-type=2&prefix=" => file_list,
-        "/test_queue/msgs.0000000001"                          => S3SpecHelper.segment_bytes(0_i64),
-        "/test_queue/msgs.0000000002"                          => S3SpecHelper.segment_bytes(100_i64),
-        "/tmp/lavinmq-spec/test_queue"                         => RESPONSE_UPLOAD,
+        "/#{DATA_DIR}/msgs.0000000001"                          => S3SpecHelper.segment_bytes(0_i64),
+        "/#{DATA_DIR}/msgs.0000000002"                          => S3SpecHelper.segment_bytes(100_i64),
+        "/tmp/lavinmq-spec/#{DATA_DIR}"                         => RESPONSE_UPLOAD,
       }
-      msg_store = MockStreamS3MessageStore.new(msg_dir, responses)
+      S3SpecHelper.responses = responses
+      msg_store = LavinMQ::AMQP::StreamQueue::StreamS3MessageStore.new(msg_dir, nil, true, ::Log::Metadata.empty)
 
       msg_store.@s3_segments.size.should eq 2
       msg_store.@segments.size.should eq 3
@@ -259,19 +264,19 @@ describe LavinMQ::AMQP::StreamQueue::StreamS3MessageStore do
     end
 
     it "should download segments from s3" do
-      msg_dir = "/tmp/lavinmq-spec/test_queue"
+      msg_dir = "/tmp/lavinmq-spec/#{DATA_DIR}"
       FileUtils.rm_rf(msg_dir)
       Dir.mkdir_p(msg_dir)
       file_list = <<-XML
           <?xml version="1.0" encoding="UTF-8"?>
           <ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
             <Contents>
-              <Key>test_queue/msgs.0000000001</Key>
+              <Key>#{DATA_DIR}/msgs.0000000001</Key>
               <ETag>"abc"</ETag>
               <Size>5647</Size>
             </Contents>
             <Contents>
-              <Key>test_queue/msgs.0000000002</Key>
+              <Key>#{DATA_DIR}/msgs.0000000002</Key>
               <ETag>"cde"</ETag>
               <Size>5647</Size>
             </Contents>
@@ -279,11 +284,12 @@ describe LavinMQ::AMQP::StreamQueue::StreamS3MessageStore do
           XML
       responses = {
         "?delimiter=%2F&encoding-type=url&list-type=2&prefix=" => file_list,
-        "/test_queue/msgs.0000000001"                          => S3SpecHelper.segment_bytes(0_i64),
-        "/test_queue/msgs.0000000002"                          => S3SpecHelper.segment_bytes(100_i64),
-        "/tmp/lavinmq-spec/test_queue"                         => RESPONSE_UPLOAD,
+        "/#{DATA_DIR}/msgs.0000000001"                          => S3SpecHelper.segment_bytes(0_i64),
+        "/#{DATA_DIR}/msgs.0000000002"                          => S3SpecHelper.segment_bytes(100_i64),
+        "/tmp/lavinmq-spec/#{DATA_DIR}"                         => RESPONSE_UPLOAD,
       }
-      msg_store = MockStreamS3MessageStore.new(msg_dir, responses)
+      S3SpecHelper.responses = responses
+      msg_store = LavinMQ::AMQP::StreamQueue::StreamS3MessageStore.new(msg_dir, nil, true, ::Log::Metadata.empty)
       msg_store.@s3_segments.size.should eq 2
       msg_store.@segments.size.should eq 3
       msg_store.@size.should eq 200
@@ -295,54 +301,13 @@ describe LavinMQ::AMQP::StreamQueue::StreamS3MessageStore do
     LavinMQ::Config.instance.streams_s3_storage_access_key_id = nil
     LavinMQ::Config.instance.streams_s3_storage_secret_access_key = nil
 
-    msg_dir = "/tmp/lavinmq-spec/test_queue"
+    msg_dir = "/tmp/lavinmq-spec/#{DATA_DIR}"
     FileUtils.rm_rf(msg_dir)
     Dir.mkdir_p(msg_dir)
+    S3SpecHelper.responses = S3SpecHelper.setup_responses
 
     expect_raises(SpecExit) do
-      MockStreamS3MessageStore.new(msg_dir, S3SpecHelper.setup_responses)
+      msg_store = LavinMQ::AMQP::StreamQueue::StreamS3MessageStore.new(msg_dir, nil, true, ::Log::Metadata.empty)
     end
-  end
-end
-
-class MockStreamConsumer < LavinMQ::AMQP::StreamConsumer
-  @channel = MockChannel.new
-  @tag = "test_tag"
-
-  def initialize(@offset : Int64, @segment : UInt32 = 0, @pos : UInt32 = 0)
-    @users = LavinMQ::Auth::UserStore.new("/tmp/lavinmq-spec/test_queue", LavinMQ::Clustering::NoopServer.new)
-    @vhost = LavinMQ::VHost.new("vhost", "/tmp/lavinmq-spec/test_queue", @users, LavinMQ::Clustering::NoopServer.new)
-    @queue = LavinMQ::AMQP::Queue.new(@vhost, "test_queue", arguments: LavinMQ::AMQP::Table.new)
-  end
-end
-
-class MockChannel < LavinMQ::AMQP::Channel
-  @client = MockClient.new
-  @id = 1
-  @vhost = "/"
-  @metadata = ::Log::Metadata.empty
-
-  def initialize
-    @log = LavinMQ::Logger.new(Log, @metadata)
-  end
-end
-
-class MockClient < LavinMQ::AMQP::Client
-  @socket = IO::Memory.new
-  @connection_info = LavinMQ::ConnectionInfo.new(Socket::IPAddress.new("127.0.0.1", 5672), Socket::IPAddress.new("127.0.0.1", 5672))
-  @user = LavinMQ::Auth::User.new("test_user", "+pHuxkR9fCyrrwXjOD4BP4XbzO3l8LJr8YkThMgJ0yVHFRE+", "SHA256", Array(LavinMQ::Tag).new)
-  @max_frame_size = 4096_u32
-  @actual_channel_max = 1000_u16
-  @channel_max = 1000_u16
-  @heartbeat_timeout = 60_u32
-  @heartbeat_interval = 60_u32
-  @auth_mechanism = "PLAIN"
-  @client_properties = LavinMQ::AMQP::Table.new
-  @metadata = ::Log::Metadata.empty
-
-  def initialize
-    users = LavinMQ::Auth::UserStore.new("/tmp/lavinmq-spec/test_queue", LavinMQ::Clustering::NoopServer.new)
-    @vhost = LavinMQ::VHost.new("vhost", "/tmp/lavinmq-spec/test_queue", users, LavinMQ::Clustering::NoopServer.new)
-    @log = LavinMQ::Logger.new(Log, @metadata)
   end
 end
