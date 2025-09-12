15ceda368339c2d38341a1de2063d9b432206270
Christina <christina@84codes.com>
2025-09-12 15:28:28 +0200

    js lint

--- static/js/exchanges.js
+++ static/js/exchanges.js
@@ -8,8 +8,8 @@ Helpers.addVhostOptions('addExchange')
 HTTP.request('GET', 'api/overview').then(function (response) {
   const exchangeTypes = response.exchange_types
   const select = document.forms.addExchange.elements.type
-  exchangeTypes.forEach( item => {
-    const name  = (item && typeof item === 'object') ? item.name : item
+  exchangeTypes.forEach(item => {
+    const name = (item && typeof item === 'object') ? item.name : item
     const human = (item && typeof item === 'object' && 'human' in item) ? item.human : undefined
     const opt = document.createElement('option')
     opt.text = human || name
