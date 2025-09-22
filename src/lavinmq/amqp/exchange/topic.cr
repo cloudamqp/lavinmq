af70abfa6c7c51cd3baed132ed1ff88b144b54bd
Magnus Landerblom <magnus@landerblom.se>
2025-09-22 11:28:06 +0200

    chain

--- src/lavinmq/amqp/exchange/topic.cr
+++ src/lavinmq/amqp/exchange/topic.cr
@@ -2,134 +2,82 @@ require "./exchange"
 
 module LavinMQ
   module AMQP
-    USE_NEW = true
-
-    class TopicKey
-      enum SegmentResult
-        INVALID
-        VALID
-        NEXT
+    class TopicBindingKey
+      abstract class Segment
+        abstract def match?(rk : Array(String)) : Bool
       end
 
-      abstract struct Segment
-        abstract def match?(v) : SegmentResult
-      end
-
-      struct HashSegment < Segment
-        @done = false
-
-        def initialize(@until : String?)
+      class HashSegment < Segment
+        def initialize(@next : Segment?)
         end
 
-        def match?(v) : SegmentResult
-          if (c = @until) && c == v
-            return SegmentResult::NEXT
+        def match?(rk : Array(String)) : Bool
+          if n = @next
+            i = 0
+            while !rk.empty?
+              rest = rk[i..]?
+              break unless rest
+              return true if n.match?(rest)
+              i += 1
+            end
+            false
+          else
+            true
           end
-          SegmentResult::VALID
         end
       end
 
-      struct StarSegment < Segment
-        @done = false
+      class StarSegment < Segment
+        def initialize(@next : Segment?)
+        end
 
-        def match?(v) : SegmentResult
-          return SegmentResult::NEXT if @done
-          @done = true
-          SegmentResult::VALID
+        def match?(rk : Array(String)) : Bool
+          v = rk.first?
+          return false unless v
+          rest = rk[1..]
+          if check = @next
+            check.match? rest
+          else
+            rest.empty?
+          end
         end
       end
 
-      struct StringSegment < Segment
-        @done = false
-
-        def initialize(@s : String)
+      class StringSegment < Segment
+        def initialize(@s : String, @next : Segment?)
         end
 
-        def match?(v) : SegmentResult
-          return SegmentResult::NEXT if @done
-          @done = true
-          v == @s ? SegmentResult::VALID : SegmentResult::INVALID
+        def match?(rk : Array(String)) : Bool
+          v = rk.first?
+          return false unless v
+          rest = rk[1..]
+          if check = @next
+            v == @s && check.match?(rest)
+          else
+            v == @s && rest.empty?
+          end
         end
       end
 
-      @checkers : Array(Segment)
+      @checker : Segment?
 
       def initialize(key : Array(String))
-        l = key.size
-        i = 0
-
-        puts ""
-        puts "############################# "
-        pp key
-        parts = Array(Segment).new(l)
-        while i < l
-          v = key[i]
-          seg = case v
-                when "#" then HashSegment.new(key[i + 1]?)
-                when "*" then StarSegment.new
-                else          StringSegment.new(v)
-                end
-          parts.push seg
-          i += 1
-        end
-        @checkers = parts
-      end
-
-      def matches?(routing_key : Array(String))
-        pp routing_key, @checkers
-        checker = @checkers.shift?
-        puts "checker=#{checker}"
-        return false unless checker
-
-        i = 0
-        # use checker until it returns NEXT, then get next checker and continue
-        while i < routing_key.size
-          seg = routing_key[i]
-          res = checker.match?(seg)
-          pp "i=#{i} seg=#{seg} checker=#{checker} res=#{res}"
-          case res
-          when SegmentResult::NEXT
-            checker = @checkers.shift?
-            return false unless checker
-          when SegmentResult::VALID
-            i += 1
-          when SegmentResult::INVALID
-            return false
+        @checker = key.reverse.reduce(nil) do |prev, v|
+          case v
+          when "#" then HashSegment.new(prev)
+          when "*" then StarSegment.new(prev)
+          else          StringSegment.new(v, prev)
           end
         end
-        return false unless @checkers.empty?
-        true
       end
 
-      # def matches?(routing_key : Array(String))
-      #   puts " "
-      #   puts "segments=#{routing_key} segments_to_check=#{routing_key[0..]}"
-      #   puts "checkers=#{@parts.map(&.to_s)} rest_checkers=#{@parts[0..].map(&.to_s)}"
-      #   i = 0
-      #   prev_checker = nil
-      #   routing_key.each do |seg|
-      #     next if prev_checker.try(&.is_a?(HashSegment))
-      #     checker = @parts[i]?
-      #     return false unless checker
-      #     prev_checker = checker
-      #     return false unless checker.match?(seg)
-      #     i += 1
-      #   end
-
-      #   rest = @parts.size - i
-      #   return false if routing_key.size == i && @parts.size > i
-
-      #   puts " "
-      #   puts "segments=#{routing_key} segments_to_check=#{routing_key[-rest..]}"
-      #   puts "checkers=#{@parts.map(&.to_s)} rest_checkers=#{@parts[i..].map(&.to_s)}"
-      #   @parts[i..].each_with_index do |checker, i|
-      #     seg = routing_key[-rest]
-      #     # puts "rest=#{rest} routing_key=#{routing_key} seg=#{seg}"
-      #     return false unless checker.match?(seg)
-      #     rest -= 1
-      #   end
-      #   true
-      # end
+      def matches?(rk : Array(String)) : Bool
+        if checker = @checker
+          checker.match?(rk)
+        else
+          false
+        end
+      end
     end
 
     class TopicExchange < Exchange
@@ -181,89 +129,22 @@ module LavinMQ
         # optimize the case where the only binding key is '#'
         if bindings.size == 1
           bk, destinations = bindings.first
-          if bk.size == 1
-            if bk.first == "#"
-              destinations.each do |destination, _binding_key|
-                yield destination
-              end
+          if bk.size == 1 && bk.first == "#"
+            destinations.each do |destination, _binding_key|
+              yield destination
             end
           end
         end
 
         rk_parts = routing_key.split(".")
         bindings.each do |bks, dests|
-          ok = if USE_NEW
-                 rk = TopicKey.new(bks)
-                 rk.matches? rk_parts
-               else
-                 topic_bks_matches_rk?(rk_parts, bks)
-               end
-          if ok
+          b = TopicBindingKey.new(bks)
+          if b.matches? rk_parts
             dests.each do |destination, _binding_key|
               yield destination
             end
           end
         end
-      end
-
-      private def topic_bks_matches_rk?(rk_parts, bks)
-        ok = false
-        prev_hash = false
-        size = bks.size # binding keys can max be 256 chars long anyway
-        j = 0
-        i = 0
-        bks.each do |part|
-          if rk_parts.size <= j
-            ok = false
-            break
-          end
-          case part
-          when "#"
-            j += 1
-            prev_hash = true
-            ok = true
-          when "*"
-            prev_hash = false
-            # Is this the last bk and the last rk?
-            if size == i + 1 && rk_parts.size == j + 1
-              ok = true
-              break
-              # More than 1 rk left ok move on
-            elsif rk_parts.size > j + 1
-              j += 1
-              i += 1
-              next
-            else
-              ok = false
-              j += 1
-            end
-          else
-            if prev_hash
-              if size == (i + 1)
-                ok = rk_parts.last == part
-                j += 1
-              else
-                ok = false
-                rk_parts[j..-1].each do |rk_part|
-                  j += 1
-                  ok = part == rk_part
-                  break if ok
-                end
-              end
-            else
-              # Is this the last bk but not the last rk?
-              if size == i + 1 && rk_parts.size > j + 1
-                ok = false
-              else
-                ok = rk_parts[j] == part
-              end
-              j += 1
-            end
-          end
-          break unless ok
-          i += 1
-        end
-        ok
       end
     end
   end
