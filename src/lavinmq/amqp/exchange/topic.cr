0d703a20485f03f66e75e72c743985ae419474af
Magnus Landerblom <magnus@landerblom.se>
2025-09-22 13:56:43 +0200

    bindings

--- src/lavinmq/amqp/exchange/topic.cr
+++ src/lavinmq/amqp/exchange/topic.cr
@@ -14,16 +14,15 @@ module LavinMQ
         def match?(rk : Array(String)) : Bool
           if n = @next
             i = 0
-            while !rk.empty?
+            loop do
               rest = rk[i..]?
               break unless rest
               return true if n.match?(rest)
               i += 1
             end
-            false
-          else
-            true
+            return false
           end
+          true
         end
       end
 
@@ -61,7 +60,7 @@ module LavinMQ
 
       @checker : Segment?
 
-      def initialize(key : Array(String))
+      def initialize(@key : Array(String))
         @checker = key.reverse.reduce(nil) do |prev, v|
           case v
           when "#" then HashSegment.new(prev)
@@ -78,10 +77,16 @@ module LavinMQ
           false
         end
       end
+
+      def as_fanout?
+        @key.size == 1 && @key.first == "#"
+      end
+
+      def_equals_and_hash @key
     end
 
     class TopicExchange < Exchange
-      @bindings = Hash(Array(String), Set({AMQP::Destination, BindingKey})).new do |h, k|
+      @bindings = Hash(TopicBindingKey, Set({AMQP::Destination, BindingKey})).new do |h, k|
         h[k] = Set({AMQP::Destination, BindingKey}).new
       end
 
@@ -100,7 +105,8 @@ module LavinMQ
       def bind(destination : AMQP::Destination, routing_key, arguments = nil)
         validate_delayed_binding!(destination)
         binding_key = BindingKey.new(routing_key, arguments)
-        return false unless @bindings[routing_key.split(".")].add?({destination, binding_key})
+        rk = TopicBindingKey.new(routing_key.split("."))
+        return false unless @bindings[rk].add?({destination, binding_key})
         data = BindingDetails.new(name, vhost.name, binding_key, destination)
         notify_observers(ExchangeEvent::Bind, data)
         true
@@ -108,10 +114,11 @@ module LavinMQ
 
       def unbind(destination : AMQP::Destination, routing_key, arguments = nil)
         rks = routing_key.split(".")
-        bds = @bindings[routing_key.split(".")]
+        rk = TopicBindingKey.new(rks)
+        bds = @bindings[rk]
         binding_key = BindingKey.new(routing_key, arguments)
         return false unless bds.delete({destination, binding_key})
-        @bindings.delete(rks) if bds.empty?
+        @bindings.delete(rk) if bds.empty?
 
         data = BindingDetails.new(name, vhost.name, binding_key, destination)
         notify_observers(ExchangeEvent::Unbind, data)
@@ -120,7 +127,6 @@ module LavinMQ
         true
       end
 
-      # ameba:disable Metrics/CyclomaticComplexity
       protected def each_destination(routing_key : String, headers : AMQP::Table?, & : LavinMQ::Destination ->)
         bindings = @bindings
 
@@ -129,17 +135,17 @@ module LavinMQ
         # optimize the case where the only binding key is '#'
         if bindings.size == 1
           bk, destinations = bindings.first
-          if bk.size == 1 && bk.first == "#"
+          if bk.as_fanout?
             destinations.each do |destination, _binding_key|
               yield destination
             end
+            return
           end
         end
 
         rk_parts = routing_key.split(".")
         bindings.each do |bks, dests|
-          b = TopicBindingKey.new(bks)
-          if b.matches? rk_parts
+          if bks.matches? rk_parts
             dests.each do |destination, _binding_key|
               yield destination
             end
