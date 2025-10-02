34eaf4ab54737a8548c594fdbf6e25a7efc200f4
Viktor Erlingsson <viktor@84codes.com>
2025-10-02 17:17:20 +0200

    Fix --internal flag setting wrong option

--- src/lavinmqctl/parser.cr
+++ src/lavinmqctl/parser.cr
@@ -182,7 +182,7 @@ module LavinMQCtl
           @options["durable"] = "true"
         end
         @parser.on("--internal", "Make the exchange internal") do
-          @options["durable"] = "true"
+          @options["internal"] = "true"
         end
         @parser.on("--delayed", "Make the exchange delayed") do
           @options["delayed"] = "true"
