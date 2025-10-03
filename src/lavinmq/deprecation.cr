b17d93fb1b87d027c4d752e8c750f3aced61b396
Christina <christina@84codes.com>
2025-10-03 13:24:58 +0200

    use LavinMQ::VERISON, and compare_versions

--- src/lavinmq/deprecation.cr
+++ src/lavinmq/deprecation.cr
@@ -8,9 +8,8 @@ module LavinMQ
     @@features = [] of NamedTuple(name: String, deprecated_in: String, remove_in: String, message: String)
 
     macro deprecated(name, deprecated_in, remove_in, message)
-      {% current_version = `[ -n "$version" ] && echo "$version" || git describe --tags 2>/dev/null || shards version`.chomp.stringify.gsub(/^v/, "") %}
-      {% if current_version.split(".")[0].to_i >= remove_in.split(".")[0].to_i %}
-        {% raise "DEPRECATION ERROR: '#{name.id}' was marked for removal in #{remove_in.id} but is still present in version #{current_version.id}. #{message.id}" %}
+      {% if compare_versions(LavinMQ::VERSION, remove_in) >= 0 %}
+        {% raise "DEPRECATION ERROR: '#{name.id}' was marked for removal in #{remove_in.id} but is still present in version #{LavinMQ::VERSION.id}. #{message.id}" %}
       {% end %}
 
       LavinMQ::Deprecation.add({{ name }}, {{ deprecated_in }}, {{ remove_in }}, {{ message }})
@@ -19,10 +18,6 @@ module LavinMQ
     def self.add(name : String, deprecated_in : String, remove_in : String, message : String)
       @@features << {name: name, deprecated_in: deprecated_in, remove_in: remove_in, message: message}
     end
-
-    # def self.warn(feature_name : String, deprecated_in : String, remove_in : String, message : String)
-    #   Log.warn { "DEPRECATED: '#{feature_name}' was deprecated in #{deprecated_in} and will be removed in #{remove_in}. #{message}" }
-    # end
 
     def self.log_startup_warnings
       return if @@features.empty?
