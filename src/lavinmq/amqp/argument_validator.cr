9fe653015380eb7863e60ba46c8db0893d764ce7
Jon Börjesson <jon@spuun.se>
2025-10-06 08:36:39 +0200

    Refactor dlrk validator to know about dlx

--- src/lavinmq/amqp/argument_validator.cr
+++ src/lavinmq/amqp/argument_validator.cr
@@ -18,12 +18,17 @@ module LavinMQ
       struct DeadLetteringValidator
         include ArgumentValidator
 
+        @dlx : String?
+
+        def initialize(arguments : AMQP::Table)
+          @dlx = arguments["x-dead-letter-exchange"]?.try &.as?(String)
+        end
+
         def validate!(header : String, value : AMQP::Field, arguments) : Nil
           return if value.nil?
           dlrk = value.as?(String)
           raise_invalid!("#{header} header not a string") if dlrk.nil?
-          dlx = arguments["x-dead-letter-exchange"]?.try &.as?(String)
-          if dlx.nil?
+          if @dlx.nil?
             raise_invalid!("x-dead-letter-exchange required if x-dead-letter-routing-key is defined")
           end
           nil
--- src/lavinmq/amqp/queue/queue.cr
+++ src/lavinmq/amqp/queue/queue.cr
@@ -29,10 +29,11 @@ module LavinMQ::AMQP
       int_one = ArgumentValidator::IntValidator.new(min_value: 1)
       string = ArgumentValidator::StringValidator.new
       bool = ArgumentValidator::BoolValidator.new
+      dlrk = ArgumentValidator::DeadLetteringValidator.new(arguments)
 
       headers = {
         "x-dead-letter-exchange":    string,
-        "x-dead-letter-routing-key": string,
+        "x-dead-letter-routing-key": dlrk,
         "x-expires":                 int_one,
         "x-max-length":              int_zero,
         "x-max-length-bytes":        int_zero,
@@ -51,12 +52,6 @@ module LavinMQ::AMQP
         if validator = headers[k]?
           validator.validate!(k, v, arguments)
         end
-      end
-
-      if value = arguments["x-dead-letter-routing-key"]?
-        key = "x-dead-letter-routing-key"
-        validator = ArgumentValidator::DeadLetteringValidator.new
-        validator.validate!(key, value, arguments)
       end
     end
 
