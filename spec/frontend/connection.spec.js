06bfac5fffa65149463865e52cd968f2ea5f089d
Jon Börjesson <jon@spuun.se>
2025-10-28 09:50:04 +0100

    Verify that connection view is refreshed automatically

--- spec/frontend/connection.spec.js
+++ spec/frontend/connection.spec.js
@@ -2,24 +2,38 @@ import * as helpers from './helpers.js'
 import { test, expect } from './fixtures.js';
 
 test.describe("connection", _ => {
-  test('info is loaded', async ({ page, baseURL }) => {
-    const connectionName = "127.0.0.1:63610 -> 127.0.0.1:12345"
-    const apiConnectionRequest = helpers.waitForPathRequest(page, `/api/connections/${connectionName}`)
-    await page.goto(`/connection#name=${connectionName}`)
+  const connectionName = "127.0.0.1:63610 -> 127.0.0.1:12345"
+  const pagePath = `/connection#name=${connectionName}`
+  const apiPath = `/api/connections/${connectionName}`
+
+  test('is loaded', async ({ page, baseURL }) => {
+    const apiConnectionRequest = helpers.waitForPathRequest(page, apiPath)
+    await page.goto(pagePath)
     await expect(apiConnectionRequest).toBeRequested()
   })
+
+  test('is refreshed automatically', async({ page }) => {
+    page.clock.install()
+    await page.goto(pagePath)
+    // Verify that at least 3 requests are made
+    for (let i=0; i<3; i++) {
+      const apiConnectionRequest = helpers.waitForPathRequest(page, apiPath)
+      page.clock.runFor(10000) // advance time by 10 seconds
+      await expect(apiConnectionRequest).toBeRequested()
+    }
+  })
+
+
   test('channels are loaded', async ({ page, baseURL }) => {
-    const connectionName = "127.0.0.1:63610 -> 127.0.0.1:12345"
-    const apiChannelsRequest = helpers.waitForPathRequest(page, `/api/connections/${connectionName}/channels`)
-    await page.goto(`/connection#name=${connectionName}`)
+    const apiChannelsRequest = helpers.waitForPathRequest(page, `${apiPath}/channels`)
+    await page.goto(pagePath)
     await expect(apiChannelsRequest).toBeRequested()
   })
 
   test('close button trigger DELETE to api/connections/<name>', async ({ page, baseURL }) => {
     const response = {}
-    const connectionName = "127.0.0.1:63610 -> 127.0.0.1:12345"
-    await page.goto(`/connection#name=${connectionName}`)
-    const apiDeleteRequest = helpers.waitForPathRequest(page, `/api/connections/${connectionName}`, {method: 'DELETE'})
+    await page.goto(pagePath)
+    const apiDeleteRequest = helpers.waitForPathRequest(page, apiPath, {method: 'DELETE'})
     await page.locator('#closeConnection').getByRole('button').click()
     await expect(apiDeleteRequest).toBeRequested()
   })
