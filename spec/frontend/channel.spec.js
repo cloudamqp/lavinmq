e8e0245253cad5a7145d9d79ba96d5230fb0c6e8
Jon Börjesson <jon@spuun.se>
2025-10-28 09:46:01 +0100

    Verify that channel view is refreshed automatically

--- spec/frontend/channel.spec.js
+++ spec/frontend/channel.spec.js
@@ -2,10 +2,21 @@ import * as helpers from './helpers.js'
 import { test, expect } from './fixtures.js';
 
 test.describe("channel", _ => {
+  const channelName = "127.0.0.1:63221[1]"
   test('is loaded', async ({ page, baseURL }) => {
-    const channelName = "127.0.0.1:63221[1]"
     const apiChannelRequest = helpers.waitForPathRequest(page, `/api/channels/${channelName}`)
     await page.goto(`/channel#name=${channelName}`)
     await expect(apiChannelRequest).toBeRequested()
+  })
+
+  test('is refreshed automatically', async({ page }) => {
+    page.clock.install()
+    await page.goto(`/channel#name=${channelName}`)
+    // Verify that at least 3 requests are made
+    for (let i=0; i<3; i++) {
+      const apiChannelRequest = helpers.waitForPathRequest(page, `/api/channels/${channelName}`)
+      page.clock.runFor(10000) // advance time by 10 seconds
+      await expect(apiChannelRequest).toBeRequested()
+    }
   })
 })
