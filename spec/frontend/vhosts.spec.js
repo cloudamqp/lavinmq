eaa170e00b500911de7517c0599177f23dc5c381
Jon Börjesson <jon@spuun.se>
2025-10-29 10:14:57 +0100

    Verify vhosts can be added

--- spec/frontend/vhosts.spec.js
+++ spec/frontend/vhosts.spec.js
@@ -7,4 +7,14 @@ test.describe("vhosts", _ => {
     await page.goto('/vhosts')
     await Promise.all(apiPermissionsRequests.map(req => expect(req).toBeRequested()))
   })
+
+  test('can be added through form', async ({ page }) => {
+    const vhostName = 'baz'
+    const formRequest = helpers.waitForPathRequest(page, `/api/vhostsi/${vhostName}`, { method: 'PUT' })
+    await page.goto('/vhosts')
+    const form = await page.locator('#createVhost')
+    await form.getByLabel('Name').fill(vhostName)
+    await form.getByRole('button').click()
+    expect(formRequest).toBeRequested()
+  })
 })
