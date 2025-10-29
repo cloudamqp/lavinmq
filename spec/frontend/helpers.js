3847f7f4163fd1165a3d5d41090e1d7638766161
Jon Börjesson <jon@spuun.se>
2025-10-29 09:41:06 +0100

    Use condition function instead of wildcard matching

--- spec/frontend/helpers.js
+++ spec/frontend/helpers.js
@@ -1,18 +1,19 @@
 async function waitForPathRequest(page, path, {response = {}, method = 'GET'} = {}) {
   const matchUrl = new URL(path, 'http://example.com')
+  const pathCondition = (url) => {
+    const requestedUrl = new URL(url)
+    return decodeURIComponent(requestedUrl.pathname) === decodeURIComponent(matchUrl.pathname)
+  }
   return new Promise((resolve, reject) => {
     const handler = (route, request) => {
       const requestedUrl = new URL(request.url())
       if (request.method() !== method) {
-        return route.continue()
+        return route.fallback()
       }
-      if (decodeURIComponent(requestedUrl.pathname) !== decodeURIComponent(matchUrl.pathname)) {
-        return route.continue()
-      }
-      page.unroute('**/*', handler)
+      page.unroute(pathCondition, handler)
       route.fulfill({ json: response }).then(_ => resolve(request))
     }
-    page.route('**/*', handler)
+    page.route(pathCondition, handler)
   })
 }
 
