38d2fe6701668ba5c8dfa9377efdddc1d3b3a7fa
Jon Börjesson <jon@spuun.se>
2025-10-30 13:48:27 +0100

    Add spec to verify operator policies form

--- spec/frontend/operator-policies.spec.js
+++ spec/frontend/operator-policies.spec.js
@@ -7,4 +7,20 @@ test.describe("operator-policies", _ => {
     await page.goto('/operator-policies')
     await expect(apiPoliciesRequest).toBeRequested()
   })
+
+  test('add operator policy form works', async ({ page, vhosts }) => {
+    const vhost = vhosts[0]
+    const policyName = 'test-operator-policy'
+    const apiPoliciesRequest = helpers.waitForPathRequest(page, '/api/operator-policies')
+    await page.goto('/operator-policies')
+    await expect(apiPoliciesRequest).toBeRequested()
+    const apiAddPolicyRequest = helpers.waitForPathRequest(page, `/api/operator-policies/${vhost}/${policyName}`, { method: 'PUT' })
+    await page.getByLabel('Virtual host').selectOption(vhost)
+    await page.getByLabel('Name').fill(policyName)
+    await page.getByLabel('Pattern').fill('^test.*')
+    await page.getByLabel('Apply to').selectOption('queues')
+    await page.getByLabel('Priority').fill('10')
+    await page.getByRole('button', { name: /add operator policy/i }).click()
+    await expect(apiAddPolicyRequest).toBeRequested()
+  })
 })
