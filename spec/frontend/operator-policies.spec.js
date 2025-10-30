9198f0eb4e51b6ed50bb9dfeb0901b6ca6d65d56
Jon Börjesson <jon@spuun.se>
2025-10-30 14:28:38 +0100

    Use values from expectedBody when populating form

--- spec/frontend/operator-policies.spec.js
+++ spec/frontend/operator-policies.spec.js
@@ -11,15 +11,30 @@ test.describe("operator-policies", _ => {
   test('add operator policy form works', async ({ page, vhosts }) => {
     const vhost = vhosts[0]
     const policyName = 'test-operator-policy'
-    const apiPoliciesRequest = helpers.waitForPathRequest(page, '/api/operator-policies')
+    const expectedBody = {
+      pattern: '^test.*',
+      'apply-to': 'queues',
+      priority: 10,
+      definition: {
+        'max-length': 5000,
+        'message-ttl': 60000
+      }
+    }
     await page.goto('/operator-policies')
-    await expect(apiPoliciesRequest).toBeRequested()
-    const apiAddPolicyRequest = helpers.waitForPathRequest(page, `/api/operator-policies/${vhost}/${policyName}`, { method: 'PUT' })
+
+    const apiAddPolicyRequest = helpers.waitForPathRequest(page,
+      `/api/operator-policies/${vhost}/${policyName}`,
+      {
+         method: 'PUT',
+         body: expectedBody
+      }
+    )
     await page.getByLabel('Virtual host').selectOption(vhost)
     await page.getByLabel('Name').fill(policyName)
-    await page.getByLabel('Pattern').fill('^test.*')
-    await page.getByLabel('Apply to').selectOption('queues')
-    await page.getByLabel('Priority').fill('10')
+    await page.getByLabel('Pattern').fill(expectedBody.pattern)
+    await page.getByLabel('Apply to').selectOption(expectedBody['apply-to'])
+    await page.getByLabel('Priority').fill(String(expectedBody.priority))
+    await page.getByLabel('Definition').fill(JSON.stringify(expectedBody.definition))
     await page.getByRole('button', { name: /add operator policy/i }).click()
     await expect(apiAddPolicyRequest).toBeRequested()
   })
