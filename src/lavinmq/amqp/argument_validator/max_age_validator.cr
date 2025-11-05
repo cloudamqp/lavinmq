9883afb5c8cfd555bb345061f1e81916a30cb3ec
Jon Börjesson <jon@spuun.se>
2025-11-05 09:09:45 +0100

    Add explicit return

--- src/lavinmq/amqp/argument_validator/max_age_validator.cr
+++ src/lavinmq/amqp/argument_validator/max_age_validator.cr
@@ -8,6 +8,7 @@ module LavinMQ
         def validate!(header : String, value : AMQP::Field) : Nil
           max_age = value.as?(String) || raise_invalid! "#{header} must be a string"
           max_age.matches?(VALID_MAX_AGE_PATTERN) || raise_invalid! "#{header} format invalid"
+          nil
         end
       end
     end
