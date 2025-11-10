75e466dcfe7ed11e35d9a9b34c2a8a634eeb4487
Christina <christina@84codes.com>
2025-11-10 17:03:46 +0100

    dont clear cache just becuase chache doesnt include permissions

--- src/lavinmq/auth/user.cr
+++ src/lavinmq/auth/user.cr
@@ -142,12 +142,14 @@ module LavinMQ
 
       def can_write?(vhost : String, name : String, cache : PermissionCache) : Bool
         permission_revision = @permission_revision.lazy_get
-        if permission_revision == cache.revision
-          result = cache[{vhost, name}]?
-          return result unless result.nil?
+        if permission_revision != cache.revision
+          cache.clear
+          cache.revision = permission_revision
         end
-        cache.clear
-        cache.revision = permission_revision
+
+        result = cache[{vhost, name}]?
+        return result unless result.nil?
+
         return cache[{vhost, name}] = can_write?(vhost, name)
       end
 
