d083804defa2ec9161317ace65806aba44d5032c
Jon Börjesson <jon@spuun.se>
2025-11-26 17:19:38 +0100

    Run most specs for both durable and non-durable

--- spec/message_store_spec.cr
+++ spec/message_store_spec.cr
@@ -69,154 +69,165 @@ describe LavinMQ::MessageStore do
     end
   end
 
-  it "first? should return nil from empty segment" do
-    mktmpdir do |dir|
-      File.write(File.join(dir, "msgs.0000000001"), "\x04\x00\x00\x00")
-      store = LavinMQ::MessageStore.new(dir, nil)
-      store.@segments.first_value.truncate(1000)
-      store.first?.should be_nil
-      store.close
-    end
-  end
-
-  it "shift? should return nil from empty segment" do
-    mktmpdir do |dir|
-      File.write(File.join(dir, "msgs.0000000001"), "\x04\x00\x00\x00")
-      store = LavinMQ::MessageStore.new(dir, nil)
-      store.@segments.first_value.truncate(1000)
-      store.shift?.should be_nil
-      store.close
-    end
-  end
-
-  it "can ack messages after restart" do
-    mktmpdir do |dir|
-      File.write(File.join(dir, "msgs.0000000001"), "\x04\x00\x00\x00")
-      File.write(File.join(dir, "acks.0000000001"), "")
-      store = LavinMQ::MessageStore.new(dir, nil)
-      body_io = IO::Memory.new("hello")
-      message = LavinMQ::Message.new(RoughTime.unix_ms, "test_exchange", "test_key", AMQ::Protocol::Properties.new, 5u64, body_io)
-      store.push(message)
-      env = store.shift?.should_not be_nil
-      String.new(env.message.body).should eq "hello"
-      store.delete(env.segment_position)
-      store.close
-    end
-  end
-
-  describe "#push" do
-    it "should not raise when rolling over on new wfile" do
-      with_store(durable: false) do |store|
-        msg_size = LavinMQ::Config.instance.segment_size.to_u64 // 2 + 1
-        msg = LavinMQ::Message.new(
-          RoughTime.unix_ms, "e", "k",
-          AMQ::Protocol::Properties.new, msg_size, IO::Memory.new("a" * msg_size)
-        )
-        begin
-          10.times do
-            store.push msg
+  #
+  # Run all specs for both durable and non-durable stores
+  #
+  {false, true}.each do |durable|
+    describe "when #{durable ? "" : "non-"}durable" do
+      describe "#first?" do
+        it "should return nil from empty segment" do
+          mktmpdir do |dir|
+            File.write(File.join(dir, "msgs.0000000001"), "\x04\x00\x00\x00")
+            store = LavinMQ::MessageStore.new(dir, nil, durable)
+            store.@segments.first_value.truncate(1000)
+            store.first?.should be_nil
+            store.close
           end
-        rescue ex
-          fail ex.inspect_with_backtrace, line: (__LINE__ - 3) # -3 to get the push line
         end
       end
-    end
-  end
 
-  describe "#empty?" do
-    it "should be true for a fresh store" do
-      with_store do |store|
-        store.empty?.should be_true
+      describe "#shift?" do
+        it "should return nil from empty segment" do
+          mktmpdir do |dir|
+            File.write(File.join(dir, "msgs.0000000001"), "\x04\x00\x00\x00")
+            store = LavinMQ::MessageStore.new(dir, nil, durable)
+            store.@segments.first_value.truncate(1000)
+            store.shift?.should be_nil
+            store.close
+          end
+        end
       end
-    end
 
-    it "should be false after push" do
-      with_store do |store|
-        store.push LavinMQ::Message.new("ex", "rk", "body")
-        store.empty?.should be_false
+      it "can ack messages after restart" do
+        mktmpdir do |dir|
+          File.write(File.join(dir, "msgs.0000000001"), "\x04\x00\x00\x00")
+          File.write(File.join(dir, "acks.0000000001"), "")
+          store = LavinMQ::MessageStore.new(dir, nil, durable)
+          body_io = IO::Memory.new("hello")
+          message = LavinMQ::Message.new(RoughTime.unix_ms, "test_exchange", "test_key", AMQ::Protocol::Properties.new, 5u64, body_io)
+          store.push(message)
+          env = store.shift?.should_not be_nil
+          String.new(env.message.body).should eq "hello"
+          store.delete(env.segment_position)
+          store.close
+        end
       end
-    end
 
-    it "should be true after all messages has been shifted" do
-      with_store do |store|
-        5.times { store.push LavinMQ::Message.new("ex", "rk", "body") }
+      describe "#push" do
+        it "should not raise when rolling over on new wfile" do
+          with_store(durable: durable) do |store|
+            msg_size = LavinMQ::Config.instance.segment_size.to_u64 // 2 + 1
+            msg = LavinMQ::Message.new(
+              RoughTime.unix_ms, "e", "k",
+              AMQ::Protocol::Properties.new, msg_size, IO::Memory.new("a" * msg_size)
+            )
+            begin
+              10.times do
+                store.push msg
+              end
+            rescue ex
+              fail ex.inspect_with_backtrace, line: (__LINE__ - 3) # -3 to get the push line
+            end
+          end
+        end
+      end
 
-        while store.shift?
+      describe "#empty?" do
+        it "should be true for a fresh store" do
+          with_store(durable: durable) do |store|
+            store.empty?.should be_true
+          end
         end
 
-        store.empty?.should be_true
-      end
-    end
-  end
-
-  describe "#empty" do
-    it "should be true for a new store" do
-      with_store do |store|
-        store.empty.should be_true
-      end
-    end
-
-    it "should be true after all messages has been shifted" do
-      with_store do |store|
-        5.times { store.push LavinMQ::Message.new("ex", "rk", "body") }
-
-        while store.shift?
+        it "should be false after push" do
+          with_store(durable: durable) do |store|
+            store.push LavinMQ::Message.new("ex", "rk", "body")
+            store.empty?.should be_false
+          end
         end
 
-        store.empty.should be_true
+        it "should be true after all messages has been shifted" do
+          with_store(durable: durable) do |store|
+            5.times { store.push LavinMQ::Message.new("ex", "rk", "body") }
+
+            while store.shift?
+            end
+
+            store.empty?.should be_true
+          end
+        end
       end
-    end
 
-    it "should be false on after a push to a new store" do
-      with_store do |store|
-        store.push LavinMQ::Message.new("ex", "rk", "body")
-
-        store.empty.should be_false
-      end
-    end
-
-    it "should be false after push to an empty store" do
-      with_store do |store|
-        5.times { store.push LavinMQ::Message.new("ex", "rk", "body") }
-
-        while store.shift?
+      describe "#empty" do
+        it "should be true for a new store" do
+          with_store(durable: durable) do |store|
+            store.empty.should be_true
+          end
         end
 
-        5.times { store.push LavinMQ::Message.new("ex", "rk", "body") }
+        it "should be true after all messages has been shifted" do
+          with_store(durable: durable) do |store|
+            5.times { store.push LavinMQ::Message.new("ex", "rk", "body") }
 
-        store.empty.should be_false
-      end
-    end
+            while store.shift?
+            end
 
-    it "should be false after requeue" do
-      with_store do |store|
-        store.push LavinMQ::Message.new("ex", "rk", "body")
-        env = store.shift?.should_not be_nil
-        store.empty.should be_true
-        store.requeue env.segment_position
-        store.empty.should be_false
-      end
-    end
+            store.empty.should be_true
+          end
+        end
 
-    it "should be true after last message has been purged" do
-      with_store do |store|
-        5.times { store.push LavinMQ::Message.new("ex", "rk", "body") }
+        it "should be false on after a push to a new store" do
+          with_store(durable: durable) do |store|
+            store.push LavinMQ::Message.new("ex", "rk", "body")
 
-        store.empty.should be_false
-        store.purge(10)
+            store.empty.should be_false
+          end
+        end
 
-        store.empty.should be_true
-      end
-    end
+        it "should be false after push to an empty store" do
+          with_store(durable: durable) do |store|
+            5.times { store.push LavinMQ::Message.new("ex", "rk", "body") }
 
-    it "should be true after purge_all" do
-      with_store do |store|
-        5.times { store.push LavinMQ::Message.new("ex", "rk", "body") }
+            while store.shift?
+            end
 
-        store.empty.should be_false
-        store.purge_all
+            5.times { store.push LavinMQ::Message.new("ex", "rk", "body") }
 
-        store.empty.should be_true
+            store.empty.should be_false
+          end
+        end
+
+        it "should be false after requeue" do
+          with_store(durable: durable) do |store|
+            store.push LavinMQ::Message.new("ex", "rk", "body")
+            env = store.shift?.should_not be_nil
+            store.empty.should be_true
+            store.requeue env.segment_position
+            store.empty.should be_false
+          end
+        end
+
+        it "should be true after last message has been purged" do
+          with_store(durable: durable) do |store|
+            5.times { store.push LavinMQ::Message.new("ex", "rk", "body") }
+
+            store.empty.should be_false
+            store.purge(10)
+
+            store.empty.should be_true
+          end
+        end
+
+        it "should be true after purge_all" do
+          with_store(durable: durable) do |store|
+            5.times { store.push LavinMQ::Message.new("ex", "rk", "body") }
+
+            store.empty.should be_false
+            store.purge_all
+
+            store.empty.should be_true
+          end
+        end
       end
     end
   end
