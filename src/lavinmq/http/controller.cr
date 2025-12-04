59d70f59276d4716868fc66ce700f62de5e58c17
Christina <christina@84codes.com>
2025-12-04 13:32:24 +0100

    let general user be used for http controller

--- src/lavinmq/http/controller.cr
+++ src/lavinmq/http/controller.cr
@@ -206,7 +206,7 @@ module LavinMQ
         context
       end
 
-      private def user(context) : Auth::LocalUser
+      private def user(context) : Auth::User
         user = nil
         if username = context.authenticated_username?
           user = @amqp_server.users[username]?
@@ -218,7 +218,7 @@ module LavinMQ
         user
       end
 
-      def vhosts(user : Auth::LocalUser)
+      def vhosts(user : Auth::User)
         @amqp_server.vhosts.each_value.select do |v|
           full_view_vhosts_access = user.tags.any? { |t| t.administrator? || t.monitoring? }
           amqp_access = user.permissions.has_key?(v.name)
