607f7ed0e5b9e6498391f4b88b2a2ca966eb569e
Carl Hörberg <carl@84codes.com>
2025-12-08 13:42:26 +0100

    Make reconnect_delay a Time::Span

--- src/lavinmq/federation/constants.cr
+++ src/lavinmq/federation/constants.cr
@@ -1,7 +1,7 @@
 module LavinMQ
   module Federation
     DEFAULT_PREFETCH        = 1000_u16
-    DEFAULT_RECONNECT_DELAY =        1
+    DEFAULT_RECONNECT_DELAY = 1.second
     DEFAULT_ACK_MODE        = AckMode::OnConfirm
     DEFAULT_MAX_HOPS        = 1_i64
     DEFAULT_EXPIRES         = nil
