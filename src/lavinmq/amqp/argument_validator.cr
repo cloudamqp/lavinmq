6c46fc11888ea504cbaf73837354f7b0e2de2916
Jon Börjesson <jon@spuun.se>
2025-12-09 14:14:07 +0100

    refactor validators to prepare for breakout out dead lettering

--- src/lavinmq/amqp/argument_validator.cr
+++ src/lavinmq/amqp/argument_validator.cr
@@ -1,7 +1,13 @@
 module LavinMQ
   module AMQP
+    # Includer should override one of the validate! methods
     module ArgumentValidator
-      abstract def validate!(header : String, value : AMQP::Field) : Nil
+      def validate!(header : String, value : AMQP::Field) : Nil
+      end
+
+      def validate!(header : String, value : AMQP::Field, arguments : AMQP::Table) : Nil
+        validate!(header, value)
+      end
 
       def raise_invalid!(msg)
         raise LavinMQ::Error::PreconditionFailed.new(msg)
