375458c336245a3e0eb2bc6b3b6206ea9d8eefab
Viktor Erlingsson <viktor@84codes.com>
2025-12-17 14:21:46 +0100

    no lock needed for checksums

--- src/lavinmq/clustering/checksums.cr
+++ src/lavinmq/clustering/checksums.cr
@@ -3,7 +3,6 @@ module LavinMQ
     class Checksums
       Log = LavinMQ::Log.for "clustering.checksums"
       @checksums = Hash(String, Bytes).new
-      @lock = Mutex.new
 
       def initialize(@data_dir : String)
       end
@@ -11,10 +10,8 @@ module LavinMQ
       def store : Nil
         Dir.mkdir_p(@data_dir)
         File.open(File.join(@data_dir, "checksums.sha1"), "w") do |f|
-          @lock.synchronize do
-            @checksums.each do |path, hash|
-              f.puts "#{hash.hexstring} *#{path}"
-            end
+          @checksums.each do |path, hash|
+            f.puts "#{hash.hexstring} *#{path}"
           end
         end
         Log.info { "Wrote #{self.size} checksums to disk" }
@@ -22,15 +19,13 @@ module LavinMQ
 
       def restore : Nil
         File.open(File.join(@data_dir, "checksums.sha1")) do |f|
-          @lock.synchronize do
-            loop do
-              hash = f.read_string(40).hexbytes
-              f.skip(2) # " *"
-              path = f.read_line
-              @checksums[path] = hash
-            rescue IO::EOFError
-              break
-            end
+          loop do
+            hash = f.read_string(40).hexbytes
+            f.skip(2) # " *"
+            path = f.read_line
+            @checksums[path] = hash
+          rescue IO::EOFError
+            break
           end
           f.delete # prevent out-of-date hashes to be restored in the event of a crash
           Log.info { "Restored #{self.size} checksums from disk" }
@@ -40,23 +35,23 @@ module LavinMQ
       end
 
       def []?(path)
-        @lock.synchronize { @checksums[path]? }
+        @checksums[path]?
       end
 
       def []=(path, value)
-        @lock.synchronize { @checksums[path] = value }
+        @checksums[path] = value
       end
 
       def delete(path)
-        @lock.synchronize { @checksums.delete(path) }
+        @checksums.delete(path)
       end
 
       def clear
-        @lock.synchronize { @checksums.clear }
+        @checksums.clear
       end
 
       def size
-        @lock.synchronize { @checksums.size }
+        @checksums.size
       end
     end
   end
