fd76ad3af83a2db4bc822f27df7307486c9971f6
Jon Börjesson <jon@84codes.com>
2025-12-19 09:51:41 +0100

    Bugfix and refactor dead lettering (#1552)
    The rabbitmq-java-client tests are failing because of a bug in dead
    lettering, causing an infinite dead lettering loop which makes lavinmq
    crash.
    
    Fixed/added/changed:
    - Dead letter code has been extracted from Queue to its own file to
    reduce the size of Queue. This part also prepares some for more code to
    be extracted, hence the `ArgumentTarget` and `add_argument_validator`.
    - Death cycle detection. Instead of publishing to an exchange the queue
    itself will handling the routing, meaning that it can route the message
    to all destination except itself.
    - Messages are routed to BCC and CC if no dead lettering routing key is
    set
    - More dead lettering specs

---
+++ src/lavinmq/amqp/argument_target.cr
@@ -0,0 +1,30 @@
+require "./argument_validator"
+
+module LavinMQ
+  module AMQP
+    module ArgumentTarget
+      # By having this as a macro we can make some compile-time validations to
+      # prevent bugs like adding the same key twice
+      macro add_argument_validator(key, validator)
+         {%
+           raise "#{key} already added" if ARGUMENT_VALIDATORS.has_key?(key)
+           ARGUMENT_VALIDATORS[key] = validator
+         %}
+       end
+
+      macro included
+        # __nil is a workaround for not being able to declare an empty namedtuple
+        # and populate it from macro
+        ARGUMENT_VALIDATORS = {__nil: nil}
+
+        def self.validate_arguments!(arguments : AMQP::Table)
+          arguments.each do |k, v|
+            if validator = ARGUMENT_VALIDATORS[k]?
+              validator.validate!(k, v, arguments)
+            end
+          end
+        end
+      end
+    end
+  end
+end
