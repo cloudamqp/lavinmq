e947e7c9af64619bc09d3ffc788a5d82e585f3e2
Magnus Landerblom <magnus@landerblom.se>
2026-01-13 13:16:47 +0100

    remove commented out code

--- src/lavinmq/shovel/shovel.cr
+++ src/lavinmq/shovel/shovel.cr
@@ -4,9 +4,3 @@ require "amqp-client"
 require "http/client"
 require "wait_group"
 require "./constants"
-
-# module LavinMQ
-#   module Shovel
-#     Log = LavinMQ::Log.for "shovel"
-#   end
-# end
