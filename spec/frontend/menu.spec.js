8e665fa0f865ed3cb8c8fd80a514eeae33022807
Jon Börjesson <jon@spuun.se>
2026-01-16 17:36:27 +0100

    Add specs for collapsible menu

--- spec/frontend/menu.spec.js
+++ spec/frontend/menu.spec.js
@@ -113,4 +113,43 @@ test.describe('menu', _ => {
     await expect(activeItems).toHaveCount(1)
     await expect(activeItem).toHaveText('Users')
   })
+
+  test.describe("collapsible", _ => {
+    test('hover menu toggle button shows tooltip', async ({ page }) => {
+      await page.goto('/')
+      const toggleButton = await page.getByRole('button', { name: 'Collapse sidebar' })
+      await toggleButton.hover()
+      await expect(toggleButton.getByText('Collapse sidebar')).toBeVisible()
+    })
+
+    test('click menu toggle button collapsed menu', async ({ page }) => {
+      await page.goto('/')
+      const toggleButton = await page.getByRole('button', { name: 'Collapse sidebar' })
+      await toggleButton.click()
+      const menu = await page.locator('#menu-content')
+      const clusterGroup = await menu.getByText('Cluster')
+      const overviewMenuItem = await menu.getByRole('listitem', { name: 'Overview' })
+      await expect(page.locator('html')).toHaveClass('menu-collapsed')
+      await expect(clusterGroup).toBeHidden()
+      await expect(overviewMenuItem).toBeHidden()
+      await expect(page.getByRole('button', { name: 'Expand sidebar' })).toHaveCount(1)
+    })
+
+    test('hover collapsed menu item show tooltip', async ({ page }) => {
+      await page.goto('/')
+      const toggleButton = await page.getByRole('button', { name: 'Collapse sidebar' })
+      const overviewItem = await page.getByRole('link', { name: 'Overview' })
+      await toggleButton.click()
+      await overviewItem.hover()
+      await expect(await overviewItem.locator('.menu-item-label')).toBeVisible()
+    })
+
+    test('state is stored', async ({ page }) => {
+      await page.goto('/')
+      const toggleButton = await page.getByRole('button', { name: 'Collapse sidebar' })
+      await toggleButton.click()
+      await page.goto('/nodes')
+      await expect(page.locator('html')).toHaveClass('menu-collapsed')
+    })
+  })
 })
