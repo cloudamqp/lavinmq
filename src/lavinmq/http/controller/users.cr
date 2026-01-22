dc2e98897d868c57d9e076e5adff99565dfe5603
Christina <christina@84codes.com>
2026-01-22 15:57:12 +0100

    undo wrong changes

--- src/lavinmq/http/controller/users.cr
+++ src/lavinmq/http/controller/users.cr
@@ -36,8 +36,8 @@ module LavinMQ
       private def register_routes # ameba:disable Metrics/CyclomaticComplexity
         get "/api/users" do |context, _params|
           refuse_unless_administrator(context, user(context))
-          users = @amqp_server.users.each_value.reject(&.hidden?).map { |u| UserView.new(u) }
-          page(context, users)
+          page(context, @amqp_server.users.each_value.reject(&.hidden?)
+            .map { |u| UserView.new(u) })
         end
 
         get "/api/users/without-permissions" do |context, _params|
