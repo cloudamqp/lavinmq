72e9a55397176f723ba5dd71e15d4f4e6e0086a3
Jon Börjesson <jon@spuun.se>
2026-02-11 14:35:15 +0100

    js lint

--- static/js/inline.js
+++ static/js/inline.js
@@ -1,6 +1,6 @@
 (function () {
   try {
-    var collapsed = window.localStorage.getItem("menuCollapsed") === "true";
-    document.documentElement.classList.toggle("menu-collapsed", collapsed);
+    const collapsed = window.localStorage.getItem('menuCollapsed') === 'true'
+    document.documentElement.classList.toggle('menu-collapsed', collapsed)
   } catch (e) {}
-})();
+})()
