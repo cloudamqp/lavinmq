069c8cd036b0a9b0d61e349615d365d69ba95369
Jon Börjesson <jon@spuun.se>
2026-05-08 11:28:45 +0200

    Monkey patch compact iterator bugfix

--- src/stdlib/iterator.cr
+++ src/stdlib/iterator.cr
@@ -1,7 +1,19 @@
 module Iterator(T)
   # Bugfix for crystal-lang/crystal/issues/16922
-  def compact_map(&func : T -> U?) forall U
+  # Probably not 100%, but as long as it compiles...
+  def compact_map(&func : T -> U) forall U
     x = uninitialized T
+    # ameba:disable Lint/NotNil
     CompactMapIterator(typeof(self), T, typeof(func.call(x).not_nil!)).new(self, func)
+  end
+
+  private struct CompactMapIterator(I, T, U)
+    def self.new(iterator, func : T -> U)
+      MapIterator(I, T, U).new(iterator, func)
+    end
+
+    # We must redeclare constructor to make it come AFTER self.new
+    def initialize(@iterator : I, @func : T -> U?)
+    end
   end
 end
