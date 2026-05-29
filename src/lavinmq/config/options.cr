68dab9639bc3f053ba0c1d48853efcc6afa3f95f
Anton Dalgren <anton@84codes.com>
2026-05-29 14:02:30 +0200

    Add clustering_backend config option (etcd|raft, default etcd)

--- src/lavinmq/config/options.cr
+++ src/lavinmq/config/options.cr
@@ -326,6 +326,11 @@ module LavinMQ
       @[EnvOpt("LAVINMQ_CLUSTERING_RAFT_PORT")]
       property clustering_raft_port = 5680
 
+      @[CliOpt("", "--clustering-backend=BACKEND", "Clustering coordination backend (etcd|raft, default: etcd)", section: "clustering")]
+      @[IniOpt(ini_name: backend, section: "clustering")]
+      @[EnvOpt("LAVINMQ_CLUSTERING_BACKEND")]
+      property clustering_backend = "etcd"
+
       @[IniOpt(section: "amqp")]
       property max_consumers_per_channel = 0
 
