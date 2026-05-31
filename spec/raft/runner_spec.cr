838286d3bd562a0f905c777113c371a2e7ce678b
Anton Dalgren <anton@84codes.com>
2026-05-31 11:35:22 +0200

    Auto-bootstrap fresh raft node in Runner

--- spec/raft/runner_spec.cr
+++ spec/raft/runner_spec.cr
@@ -26,4 +26,29 @@ describe LavinMQ::Raft::Runner do
       FileUtils.rm_rf(dir)
     end
   end
+
+  it "auto-bootstraps a fresh node with no peers and no .join_target" do
+    dir = tmp_data_dir
+    begin
+      config = LavinMQ::Config.new
+      config.data_dir = dir
+      config.clustering_bind = "127.0.0.1"
+      config.clustering_raft_port = 0
+      config.clustering_port = 5679
+      config.clustering_advertised_uri = "tcp://127.0.0.1:5679"
+      runner = LavinMQ::Raft::Runner.new(config)
+      spawn(name: "runner-test") do
+        runner.run { Fiber.yield }
+      end
+      select
+      when runner.server.is_leader.when_true.receive
+        runner.server.is_leader.value.should be_true
+      when timeout(3.seconds)
+        fail "single-node runner did not auto-bootstrap into leadership"
+      end
+      runner.stop
+    ensure
+      FileUtils.rm_rf(dir)
+    end
+  end
 end
