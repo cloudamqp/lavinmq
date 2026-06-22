237dc088d8b6e614d4a63e9410e482d730dc963b
Jon Börjesson <jon@spuun.se>
2026-06-22 17:15:33 +0200

    Split LavinMQ::BindingKey into AMQP::BindingKey and MQTT::SubscriptionKey

---
+++ src/lavinmq/amqp/binding_key.cr
@@ -0,0 +1,27 @@
+require "../amqp"
+
+module LavinMQ
+  module AMQP
+    struct BindingKey
+      getter routing_key : String
+      getter arguments : AMQP::Table? = nil
+
+      def initialize(@routing_key : String, @arguments : AMQP::Table? = nil)
+      end
+
+      def properties_key
+        if (args = arguments) && !args.empty?
+          @hsh ||= begin
+            hsh = args.to_h
+            Base64.urlsafe_encode(hsh.keys.sort!.map { |k| "#{k}:#{hsh[k]}" }.join(","))
+          end
+          return "#{routing_key}~#{@hsh}"
+        end
+        return "~" if routing_key.empty?
+        routing_key
+      end
+
+      def_hash properties_key
+    end
+  end
+end
