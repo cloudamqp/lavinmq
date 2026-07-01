d64ae56e674559677194a758364cc649fc6f57d1
Christina Dahlén <85930202+kickster97@users.noreply.github.com>
2026-07-01 11:16:50 +0200

    Add policymaker role checks to ShovelsController endpoints (#2133)
    The shovel endpoints didn't enforce role checks, unlike the parameters
    and policies controllers which already require policymaker access.
    
    This aligns ShovelsController with those controllers: listing, viewing,
    pausing, and resuming shovels now go through
    `refuse_unless_policymaker`,
    consistent with how shovels (dynamic parameters) are managed elsewhere.
    
    Specs added for the role checks on each endpoint.

--- spec/api/shovels_spec.cr
+++ spec/api/shovels_spec.cr
@@ -22,6 +22,71 @@ def create_shovel(server, name = "spec-shovel", config = NamedTuple.new, paused 
 end
 
 describe LavinMQ::HTTP::ShovelsController do
+  describe "access control" do
+    it "should refuse management users from listing all shovels" do
+      with_http_server do |http, s|
+        s.users.create("arnold", "pw", [LavinMQ::Tag::Management])
+        hdrs = ::HTTP::Headers{"Authorization" => "Basic YXJub2xkOnB3"}
+        response = http.get("/api/shovels", headers: hdrs)
+        response.status_code.should eq 403
+      end
+    end
+
+    it "should allow policymaker to list shovels in a vhost" do
+      with_http_server do |http, s|
+        s.users.create("arnold", "pw", [LavinMQ::Tag::PolicyMaker])
+        s.users.add_permission("arnold", "/", /.*/, /.*/, /.*/)
+        hdrs = ::HTTP::Headers{"Authorization" => "Basic YXJub2xkOnB3"}
+        vhost_url_encoded = URI.encode_path_segment("/")
+        response = http.get("/api/shovels/#{vhost_url_encoded}", headers: hdrs)
+        response.status_code.should eq 200
+      end
+    end
+
+    it "should refuse management and monitoring users from listing shovels" do
+      with_http_server do |http, s|
+        s.users.create("arnold", "pw", [LavinMQ::Tag::Management, LavinMQ::Tag::Monitoring])
+        hdrs = ::HTTP::Headers{"Authorization" => "Basic YXJub2xkOnB3"}
+        vhost_url_encoded = URI.encode_path_segment("/")
+        response = http.get("/api/shovels/#{vhost_url_encoded}", headers: hdrs)
+        response.status_code.should eq 403
+      end
+    end
+
+    it "should refuse management users from getting a shovel" do
+      with_http_server do |http, s|
+        create_shovel(s)
+        s.users.create("arnold", "pw", [LavinMQ::Tag::Management])
+        hdrs = ::HTTP::Headers{"Authorization" => "Basic YXJub2xkOnB3"}
+        vhost_url_encoded = URI.encode_path_segment("/")
+        response = http.get("/api/shovels/#{vhost_url_encoded}/spec-shovel", headers: hdrs)
+        response.status_code.should eq 403
+      end
+    end
+
+    it "should refuse management users from pausing a shovel" do
+      with_http_server do |http, s|
+        create_shovel(s)
+        s.users.create("arnold", "pw", [LavinMQ::Tag::Management])
+        hdrs = ::HTTP::Headers{"Authorization" => "Basic YXJub2xkOnB3"}
+        vhost_url_encoded = URI.encode_path_segment("/")
+        response = http.put("/api/shovels/#{vhost_url_encoded}/spec-shovel/pause", headers: hdrs)
+        response.status_code.should eq 403
+      end
+    end
+
+    it "should refuse management users from resuming a shovel" do
+      with_http_server do |http, s|
+        create_shovel(s, paused: true)
+        s.users.create("arnold", "pw", [LavinMQ::Tag::Management])
+        hdrs = ::HTTP::Headers{"Authorization" => "Basic YXJub2xkOnB3"}
+        vhost_url_encoded = URI.encode_path_segment("/")
+        response = http.put("/api/shovels/#{vhost_url_encoded}/spec-shovel/resume", headers: hdrs)
+        response.status_code.should eq 403
+      end
+    end
+  end
+
   describe "PUT api/shovels/:vhost/:name/pause" do
     it "should return 404 for non-existing shovel" do
       with_http_server do |http, _s|
